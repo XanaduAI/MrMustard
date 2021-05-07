7aa5f40d68aa698e8d504e808ccec00aba8c6c82
ziofil <miatto@gmail.com>
2021-05-07 10:38:02 -0400

    tmp commit

--- mrmustard/_gates.py
+++ mrmustard/_gates.py
@@ -1,15 +1,16 @@
 from abc import ABC, abstractmethod
 from typing import List, Tuple, Optional, Sequence, Union
 from numpy.typing import ArrayLike
+from dataclasses import dataclass
 
 from mrmustard._circuit import GateInterface
 from mrmustard._states import State
+from mrmustard._backends import MathBackendInterface
+
+
 
 # NOTE Gates IMPLEMENT the GateInterface, and USE the GateBackendInterface
 
-
-# TODO [improvement]: we may want to apply gates to states that live in a subset of modes
-# rather than applying gates on states spanning all the modes
 
 class GateBackendInterface(ABC):
     @abstractmethod
@@ -24,47 +25,35 @@ class GateBackendInterface(ABC):
     @abstractmethod
     def _beam_splitter_symplectic(self, theta, varphi) -> ArrayLike: pass
 
-    @abstractmethod
-    def _expand(self, matrix: ArrayLike, modes: List[int], total_modes:int): pass
 
-    @abstractmethod
-    def _sandwich(self, bread:ArrayLike, filling:ArrayLike) -> ArrayLike: pass
+@dataclass
+class ParameterInfo:
+    init_value: Optional[float] = None
+    trainable: bool = True
+    bounds: Tuple[Optional[float], Optional[float]] = (None,None)
+    shape:Optional[Tuple[int,...]] = None
+    name: str = ''
+    
 
-    @abstractmethod
-    def _matvec(self, mat:ArrayLike, vec:ArrayLike) -> ArrayLike: pass
-
-    @abstractmethod
-    def _add_at_index(self, array:ArrayLike, value:ArrayLike, index:Sequence[int]) -> ArrayLike: pass
-
-    @abstractmethod
-    def _new_real_variable(self, shape:Optional[Sequence[int]]=None, clip_min:Optional[float]=None,
-                            clip_max:Optional[float]=None, name:str='') -> ArrayLike: pass
-
-    @abstractmethod
-    def _new_symplectic_variable(self, num_modes:int, name:str) -> ArrayLike: pass
-
-    @abstractmethod
-    def _constant(self, value:Optional[float]) -> Optional[ArrayLike]: pass
-
-
-
-
-class BaseBSgate(GateInterface, GateBackendInterface):
-    def __init__(self, modes:List[int], theta:Optional[float]=None, phi:Optional[float]=None):
+class BaseBSgate(GateInterface, GateBackendInterface, MathBackendInterface):
+    def __init__(self, modes:List[int],
+                       theta:Optional[float]=None,
+                       theta_bounds:Tuple[Optional[float], Optional[float]]=(None,None),
+                       theta_trainable:bool=True,
+                       phi:Optional[float]=None,
+                       phi_bounds:Tuple[Optional[float], Optional[float]]=(None,None),
+                       phi_trainable:bool=True):
         self.modes = modes
         self.mixing = False
-        self._parameters = [self._constant(theta), self._constant(phi)]
-        if theta is None:
-            self._parameters[0] = self._new_real_variable(name='theta')
-        if phi is None:
-            self._parameters[1] = self._new_real_variable(name='varphi')
+        _theta = ParameterInfo(theta, theta_trainable, theta_bounds, None, 'theta')
+        _phi = ParameterInfo(phi, phi_trainable, phi_bounds, None, 'phi')
+        self._parameters = [self._make_parameter(_theta), self._make_parameter(_phi)]
 
     def __call__(self, state:State) -> State:
-        theta, phi = self._parameters
-        BS = self._expand(self._beam_splitter_symplectic(theta, phi), self.modes, state.num_modes)
+        BS = self._beam_splitter_symplectic(*self._parameters) # (4x4)
         output = State(state.num_modes)
-        output.cov = self._sandwich(bread=BS, filling=state.cov)
-        output.means = self._matvec(mat=BS, vec=state.means)
+        output.cov = self._sandwich(bread=BS, filling=state.cov, modes=self.modes)
+        output.means = self._matvec(mat=BS, vec=state.means, modes=self.modes)
         return output
 
     def __repr__(self):
@@ -77,14 +66,18 @@ class BaseBSgate(GateInterface, GateBackendInterface):
 
 
 class BaseDgate(GateInterface, GateBackendInterface):
-    def __init__(self, modes:List[int], x:Optional[float]=None, y:Optional[float]=None):
+    def __init__(self, modes:List[int],
+                       x:Optional[float]=None,
+                       x_bounds:Tuple[Optional[float], Optional[float]]=(None,None),
+                       x_trainable:bool=True,
+                       y:Optional[float]=None,
+                       y_bounds:Tuple[Optional[float], Optional[float]]=(None,None),
+                       y_trainable:bool=True):
         self.modes = modes
         self.mixing = False
-        self._parameters = [self._constant(x), self._constant(y)]
-        if x is None:
-            self._parameters[0] = self._new_real_variable(name='x')
-        if y is None:
-            self._parameters[1] = self._new_real_variable(name='y')
+        _x = ParameterInfo(x, x_trainable, x_bounds, None, 'x')
+        _y = ParameterInfo(y, y_trainable, y_bounds, None, 'y')
+        self._parameters = [self._make_parameter(_x), self._make_parameter(_y)]
             
     def __call__(self, state:State) -> State:
         x,y = self._parameters
@@ -104,21 +97,25 @@ class BaseDgate(GateInterface, GateBackendInterface):
 
 
 class BaseSgate(GateInterface, GateBackendInterface):
-    def __init__(self, modes:List[int], r:Optional[float]=None, phi:Optional[float]=None):
+    def __init__(self, modes:List[int],
+                       r:Optional[float]=None,
+                       r_bounds:Tuple[Optional[float], Optional[float]]=(0.0,None),
+                       r_trainable:bool=True,
+                       phi:Optional[float]=None,
+                       phi_bounds:Tuple[Optional[float], Optional[float]]=(None,None),
+                       phi_trainable:bool=True):
         self.modes = modes
         self.mixing = False
-        self._parameters = [self._constant(r), self._constant(phi)]
-        if r is None:
-            self._parameters[0] = self._new_real_variable(clip_min=0.0, name='r')
-        if phi is None:
-            self._parameters[1] = self._new_real_variable(name='phi')
+        _r = ParameterInfo(r, r_trainable, r_bounds, None, 'r')
+        _phi = ParameterInfo(phi, phi_trainable, phi_bounds, None, 'phi')
+        self._parameters = [self._make_parameter(_r), self._make_parameter(_phi)]
 
     def __call__(self, state:State) -> State:
         r, phi = self._parameters
         S = self._expand(self._squeezing_symplectic(r, phi), self.modes, state.num_modes)
         output = State(state.num_modes)
-        output.cov = self._sandwich(bread=S, filling=state.cov)
-        output.means = self._matvec(mat=S, vec=state.means)
+        output.cov = self._sandwich(bread=S, filling=state.cov, modes=self.modes)
+        output.means = self._matvec(mat=S, vec=state.means, modes=self.modes)
         return output
 
     def __repr__(self):
@@ -131,22 +128,22 @@ class BaseSgate(GateInterface, GateBackendInterface):
 
 
 class BaseGgate(GateInterface, GateBackendInterface):
-    def __init__(self, modes:List[int], cov:Optional[ArrayLike]=None, means:Optional[ArrayLike]=None):
+    def __init__(self, modes:List[int], with_displacement:bool=False):
         self.modes = modes
         self.mixing = False
-        self._parameters = [self._constant(cov), self._constant(means)]
-        if cov is None:
-            # import pdb
-            # pdb.set_trace()
-            self._parameters[0] = self._new_symplectic_variable(num_modes=len(self.modes), name='CovMatrix')
-        if means is None:
-            self._parameters[1] = self._new_real_variable(shape=[2*len(self.modes)], name='MeansVector')
+        self._parameters = []
+        self._parameters[0] = self._new_symplectic_variable(num_modes=len(self.modes), name='CovMatrix')
+        if with_displacement:
+            _means_vector = ParameterInfo(trainable=True, shape=(2*len(modes),), name='MeansVector')
+        else:
+            _means_vector = ParameterInfo(init_value= np.zeros(2*len(modes)), trainable=False, name='MeansVector')
+        self._parameters[1] = self._make_parameter(_means_vector)
 
     def __call__(self, state:State) -> State:
         cov, means = self._parameters
         output = State(state.num_modes)
-        output.cov = self._sandwich(bread=cov, filling=state.cov)
-        output.means = state.means + means
+        output.cov = self._sandwich(bread=cov, filling=state.cov, modes=self.modes)
+        output.means = self._matvec(mat=S, vec=state.means) + means # TODO fix
         return output
 
     # def __repr__(self):
@@ -168,19 +165,21 @@ class BaseGgate(GateInterface, GateBackendInterface):
 
 
 class BaseRgate(GateInterface, GateBackendInterface):
-    def __init__(self, modes:List[int], angle:Optional[float]=None):
+    def __init__(self, modes:List[int],
+                       angle:Optional[float]=None,
+                       angle_bounds:Tuple[Optional[float], Optional[float]]=(None,None),
+                       angle_trainable:bool=True):
         self.modes = modes
         self.mixing = False
-        self._parameters = [self._constant(angle)]
-        if angle is None:
-            self._parameters[0] = self._new_real_variable(name='angle')
+        _angle = ParameterInfo(angle, angle_trainable, angle_bounds, None, 'angle')
+        self._parameters = [self._make_parameter(_angle)]
 
     def __call__(self, state:State) -> State:
         angle, = self._parameters
         R = self._expand(self._rotation_symplectic(angle), self.modes, state.num_modes)
         output = State(state.num_modes)
-        output.cov = self._sandwich(bread=R, filling=state.cov)
-        output.means = self._matvec(mat=R, vec=state.means)
+        output.cov = self._sandwich(bread=R, filling=state.cov, modes=self.modes)
+        output.means = self._matvec(mat=R, vec=state.means, modes=self.modes)
         return output
 
     def __repr__(self):
@@ -193,15 +192,14 @@ class BaseRgate(GateInterface, GateBackendInterface):
 
 
 class BaseLoss(GateInterface, GateBackendInterface):
-    def __init__(self, modes:Sequence[int], transmissivity:Union[float,Sequence[float]]=None):
+    def __init__(self, modes:List[int],
+                       transmissivity:Optional[float]=None,
+                       transmissivity_bounds:Tuple[Optional[float], Optional[float]]=(0.0,1.0),
+                       transmissivity_trainable:bool=True):
         self.modes = modes
-        self.mixing = True
-        if transmissivity is None:
-            self._parameters = [self._new_real_variable(clip_min=0.0, clip_max=1.0, name=f'Transmissivity_{i}') for i in modes]
-        if not isinstance(transmissivity, Sequence):
-            self._parameters = [self._constant(transmissivity) for _ in modes]
-        if isinstance(transmissivity, Sequence):
-            self._parameters = [self._constant(t) for t in transmissivity]
+        self.mixing = False
+        _transmissivity = ParameterInfo(transmissivity, transmissivity_trainable, transmissivity_bounds, None, 'transmissivity')
+        self._parameters = [self._make_parameter(_transmissivity)]
             
     def __call__(self, state:State) -> State:
         transmissivity = self._parameters
