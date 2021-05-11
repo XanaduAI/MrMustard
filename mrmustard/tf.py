b626531a7ca75db947a2f27b80747db225c00c44
ziofil <miatto@gmail.com>
2021-05-11 11:39:45 -0400

    injecting backend via class attribute

--- mrmustard/tf.py
+++ mrmustard/tf.py
@@ -1,19 +1,11 @@
-from mrmustard._circuit import BaseCircuit
-from mrmustard._opt import BaseOptimizer
-from mrmustard._gates import BaseGate, BaseBSgate, BaseDgate, BaseSgate, BaseRgate, BaseGgate, BaseLoss, BaseS2gate
-from mrmustard._backends.tfbackend import TFCircuitBackend, TFGateBackend, TFOptimizerBackend, TFMathbackend
+if __name__ == 'mrmustard.tf':
+    from mrmustard._circuit import BaseCircuit
+    from mrmustard._opt import BaseOptimizer
+    from mrmustard._gates import Gate, BSgate, Sgate, Rgate, Dgate, Ggate, LossChannel, S2gate
+    from mrmustard._backends.tfbackend import TFCircuitBackend, TFGateBackend, TFOptimizerBackend, TFMathbackend
 
-BaseGate._backend = TFMathbackend() # injecting tf math backend into all the gates
+    Gate._math_backend = TFMathbackend() # injecting tf math backend into all the gates
+    Gate._gate_backend = TFGateBackend() # injecting tf gate backend into all the gates
 
-class Circuit(BaseCircuit,TFCircuitBackend): pass
-class Optimizer(TFOptimizerBackend, BaseOptimizer): pass
-
-class Sgate(TFGateBackend, BaseSgate): pass
-class Dgate(TFGateBackend, BaseDgate): pass
-class Rgate(TFGateBackend, BaseRgate): pass
-class Ggate(TFGateBackend, BaseGgate): pass
-class BSgate(TFGateBackend, BaseBSgate): pass
-class LossChannel(TFGateBackend, BaseLoss): pass
-class S2gate(TFGateBackend, BaseS2gate): pass
-
-__all__ = ['Circuit', 'Optimizer', 'Sgate', 'Dgate', 'Ggate', 'BSgate', 'Rgate', 'LossChannel', 'S2gate']
+    class Circuit(BaseCircuit,TFCircuitBackend): pass
+    class Optimizer(TFOptimizerBackend, BaseOptimizer): pass
