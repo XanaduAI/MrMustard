5d2b1cc07a0feb06fd2ceabc266db24ee5f8c3d8
Nicolas Quesada <zeitus@gmail.com>
2021-05-12 13:30:34 -0400

    passes black

--- tests/test_opt.py
+++ tests/test_opt.py
@@ -20,3 +20,20 @@ def test_S2gate_coincidence_prob(n):
     prob = np.abs(circ.fock_output(cutoffs=[n + 1, n + 1]))[n, n] ** 2
     expected = 1 / (n + 1) * (n / (n + 1)) ** n
     assert np.allclose(prob, expected, atol=1e-4)
+
+
+def test_hong_ou_mandel_optimizer():
+    """Finding the optimal beamsplitter transmission to get Hong-Ou-Mandel dip"""
+    tf.random.set_seed(137)
+    circ = Circuit(num_modes=4)  # emtpy circuit with vacuum input state
+    r = np.arcsinh(1.0)
+    circ.add_gate(S2gate(modes=[0, 1], r=r, r_trainable=False))
+    circ.add_gate(S2gate(modes=[2, 3], r=r, r_trainable=False))
+    circ.add_gate(BSgate(modes=[1, 2]))
+
+    def loss_fn():
+        return tf.abs(circ.fock_output(cutoffs=[2, 2, 2, 2])[1, 1, 1, 1]) ** 2
+
+    opt = Optimizer(euclidean_lr=0.005)
+    circ = opt.minimize(circ, loss_fn, max_steps=0)
+    assert np.allclose(np.cos(circ.euclidean_parameters[2]) ** 2, 0.5, atol=2e-3)
