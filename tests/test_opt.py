8cbb0fb809c3dcf15ab8090243b5aa2b1a770e49
Nicolas Quesada <zeitus@gmail.com>
2021-05-12 15:39:14 -0400

    Adds tests

--- tests/test_opt.py
+++ tests/test_opt.py
@@ -22,7 +22,7 @@ def test_S2gate_coincidence_prob(n):
     assert np.allclose(prob, expected, atol=1e-4)
 
 
-def test_hong_ou_mandel_optimizer_S2():
+def test_hong_ou_mandel_optimizer():
     """Finding the optimal beamsplitter transmission to get Hong-Ou-Mandel dip"""
     tf.random.set_seed(137)
     circ = Circuit(num_modes=4)  # emtpy circuit with vacuum input state
@@ -40,7 +40,7 @@ def test_hong_ou_mandel_optimizer_S2():
 
 
 def test_learning_two_mode_squeezing():
-    """Finding the optimal beamsplitter transmission to get Hong-Ou-Mandel dip"""
+    """Finding the optimal beamsplitter transmission to make a pair of single photons"""
     tf.random.set_seed(137)
     circ = Circuit(num_modes=2)
     circ.add_gate(Sgate(modes=[0]))
@@ -53,6 +53,22 @@ def test_learning_two_mode_squeezing():
         return -tf.abs(amps[1,1])**2 + tf.abs(amps[0,1])**2
 
     opt = Optimizer(euclidean_lr=0.05)
+
+    circ = opt.minimize(circ, loss_fn, max_steps=1000)
+    assert np.allclose(-loss_fn(), 0.25, atol=2e-3)
+
+def test_learning_two_mode_Ggate():
+    """Finding the optimal Ggate to make a pair of single photons"""
+
+    circ = Circuit(num_modes=2) # emtpy circuit with vacuum input state
+    circ.add_gate(Ggate(modes = [0,1], displacement_trainable=False, displacement=[0,0,0,0]))
+    tf.random.set_seed(20)
+
+    def loss_fn():
+        amps = circ.fock_output(cutoffs=[2,2])
+        return -tf.abs(amps[1,1])**2 + tf.abs(amps[0,1])**2
+
+    opt = Optimizer(symplectic_lr=0.1)
 
     circ = opt.minimize(circ, loss_fn, max_steps=1000)
     assert np.allclose(-loss_fn(), 0.25, atol=2e-3)
