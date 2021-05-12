06840d33153db95eb75a6fd06f2972f75833badf
ziofil <miatto@gmail.com>
2021-05-12 09:28:27 -0400

    can instantiate State

--- mrmustard/_states.py
+++ mrmustard/_states.py
@@ -18,6 +18,9 @@ class State:
     _state_backend: StateBackendInterface
     cov: ArrayLike
     means: ArrayLike
+
+    def __init__(self, num_modes:int):
+        self.num_modes = num_modes
     
     def photon_number_mean(self, hbar:float=2) -> ArrayLike:
         return self._state_backend.photon_number_mean(self.cov, self.means, hbar)
@@ -28,7 +31,7 @@ class State:
 
 class Vacuum(State):
     def __init__(self, num_modes:int):
-        self.num_modes = num_modes
+        super().__init__(num_modes)
         self.cov = np.identity(2*self.num_modes)
         self.means = np.zeros(2*self.num_modes)
     def __repr__(self)->str:
@@ -36,18 +39,18 @@ class Vacuum(State):
 
 class SqueezedVacuum(State):
     def __init__(self, num_modes:int):
-        self.num_modes = num_modes
+        super().__init__(num_modes)
         self.cov = np.identity(2*self.num_modes) # TODO
         self.means = np.zeros(2*self.num_modes)  # TODO
 
 class Coherent(State):
     def __init__(self, num_modes:int):
-        self.num_modes = num_modes
+        super().__init__(num_modes)
         self.cov = np.identity(2*self.num_modes) # TODO
         self.means = np.zeros(2*self.num_modes)  # TODO
 
 class Thermal(State):
     def __init__(self, num_modes:int):
-        self.num_modes = num_modes
+        super().__init__(num_modes)
         self.cov = np.identity(2*self.num_modes) # TODO
         self.means = np.zeros(2*self.num_modes)  # TODO
