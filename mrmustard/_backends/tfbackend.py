fixed photon number covariance

mrmustard/_backends/tfbackend.py
@@ -167,14 +167,6 @@ class TFOptimizerBackend(OptimizerBackendInterface):
 
 class TFStateBackend(StateBackendInterface):
 
-    def AB(self, cov:tf.Tensor) -> tf.Tensor:
-        N = cov.shape[-1]//2
-        V1 = cov[:N,:N]
-        V2 = cov[:N,N:]
-        V2T = cov[N:,:N]
-        V3 = cov[N:,N:]
-        return 0.5*tf.complex(V1 + V3, V2T - V2), 0.5*tf.complex(V1 - V3, V2T + V2)
-
     def photon_number_mean(self, cov:tf.Tensor, means:tf.Tensor, hbar:float) -> tf.Tensor:
         N = means.shape[-1] // 2
         return (means[:N] ** 2
@@ -185,14 +177,11 @@ class TFStateBackend(StateBackendInterface):
             ) / (2 * hbar)
 
     def photon_number_covariance(self, cov, means, hbar)->tf.Tensor:
-        A, B = self.AB(cov)
         N = means.shape[-1] // 2
-        ac = tf.complex(means[:N], -means[:N]) # alpha conj
-        return (tf.abs(A * tf.math.conj(A))
-                + tf.abs(B * tf.math.conj(B))
-                - 0.25*tf.eye(len(A), dtype=tf.float64)
-                + 2*tf.math.real(ac[None,:]*tf.math.conj(ac)[:,None]*A + ac[None,:]*ac[:,None]*B)
-                )
+        mCm = cov * means[:,None] * means[None,:]
+        dd = tf.linalg.diag(tf.linalg.diag_part(mCm[:N,:N]+mCm[N:,N:]+mCm[:N,N:]+mCm[N:,:N]))/(2*hbar**2)
+        CC = (cov**2 + mCm)/(2*hbar**2)
+        return CC[:N,:N] + CC[N:,N:] + CC[:N,N:] + CC[N:,:N] + dd - 0.25*np.identity(N)
 
 
 

tests/test_fock.py
@@ -9,15 +9,15 @@ import pytest
 @pytest.mark.parametrize("n_mean", [0, 1, 2, 3])
 @pytest.mark.parametrize("phi", 2 * np.pi * np.random.rand(4))
 def test_two_mode_squeezing_fock(n_mean, phi):
-    """Tests that perfect number correlations are obtained for a two-mode squeezed vacuum state"""
+    """Tests that perfect number correlations are obtained for a two-mode squeezed vacuum state
+    Note that we use an extra - sign with respect to TMSV in https://en.wikipedia.org/wiki/Squeezed_coherent_state"""
     cutoff = 4
     circ = Circuit(num_modes=2)
     r = np.arcsinh(np.sqrt(n_mean))
-    circ.add_gate(S2gate(modes=[0, 1], r=-r, phi=phi))
+    circ.add_gate(S2gate(modes=[0, 1], r=-r, phi=phi)) 
     amps = circ.fock_output(cutoffs=[cutoff, cutoff])
-    modulus = np.sqrt(1 / (1 + n_mean) * (n_mean / (1 + n_mean)) ** np.arange(cutoff))
-    phase = np.exp(-1j * phi) ** np.arange(cutoff)
-    expected = np.diag(modulus * phase)
+    diag = (1 / np.cosh(r)) * (-np.exp(1j*phi)*np.tanh(r)) ** np.arange(cutoff)
+    expected = np.diag(diag)
     assert np.allclose(amps, expected)
 
 
@@ -54,7 +54,8 @@ def test_coherent_state(realpha, imalpha):
 @pytest.mark.parametrize("r", 2 * np.random.rand(4))
 @pytest.mark.parametrize("phi", 2 * np.pi * np.random.rand(4))
 def test_squeezed_state(r, phi):
-    """Test that squeezed states have the correct photon number statistics"""
+    """Test that squeezed states have the correct photon number statistics
+    Note that we use the same sign with respect to SMSV in https://en.wikipedia.org/wiki/Squeezed_coherent_state"""
     cutoff = 10
     circ = Circuit(num_modes=1)
     circ.add_gate(Sgate(modes=[0], r=r, phi=phi))
@@ -77,7 +78,7 @@ def test_squeezed_state(r, phi):
     assert np.allclose(non_zero_amps, amp_pairs)
 
 ####
-# The following tests currently fails
+# The following tests currently fail
 ####
 @pytest.mark.parametrize("n_mean", [0, 1, 2, 3])
 @pytest.mark.parametrize("phi", 2 * np.pi * np.random.rand(4))