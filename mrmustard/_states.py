4217c71bab7d3022535f960aaa915bab2abe105c
ziofil <miatto@gmail.com>
2021-05-20 15:15:51 -0400

    states docstrings

--- mrmustard/_states.py
+++ mrmustard/_states.py
@@ -34,6 +34,11 @@ class State:
         return "covariance:\n" + repr(self.cov) + "\nmeans:\n" + repr(self.means)
 
     def ket(self, cutoffs: List[int]):
+        r"""
+        Returns the ket of the state in Fock representation.
+        Arguments:
+            cutoffs List[int]: the cutoff dimensions for each mode
+        """
         if not self.mixed:
             A, B, C = self._state_backend.ABC(
                 self.cov, self.means, mixed=self.mixed, hbar=self.hbar
@@ -41,6 +46,11 @@ class State:
             return self._state_backend.fock_state(A, B, C, cutoffs=cutoffs)
 
     def dm(self, cutoffs: List[int]):
+        r"""
+        Returns the density matrix of the state in Fock representation.
+        Arguments:
+            cutoffs List[int]: the cutoff dimensions for each mode
+        """
         A, B, C = self._state_backend.ABC(self.cov, self.means, mixed=self.mixed, hbar=self.hbar)
         fock_state = self._state_backend.fock_state(A, B, C, cutoffs=cutoffs)
         if self.mixed:
@@ -49,6 +59,13 @@ class State:
             return self._math_backend.outer(self._math_backend.conj(fock_state), fock_state)
 
     def fock_probabilities(self, cutoffs: List[int]):
+        r"""
+        Returns the probabilities in Fock representation. If the state is pure, they are 
+        the absolute value squared of the ket amplitudes. If the state is mixed they are
+        the diagonals of the density matrix.
+        Arguments:
+            cutoffs List[int]: the cutoff dimensions for each mode
+        """
         if self.mixed:
             rho = self.dm(cutoffs=cutoffs)
             return self._math_backend.all_diagonals(rho, real=True)
@@ -58,14 +75,23 @@ class State:
 
     @property
     def number_means(self):
+        r"""
+        Returns the mean photon number for each mode
+        """
         return self._state_backend.number_means(self.cov, self.means, self.hbar)
 
     @property
     def number_cov(self):
+        r"""
+        Returns the complete photon number covariance matrix
+        """
         return self._state_backend.number_cov(self.cov, self.means, self.hbar)
 
 
 class Vacuum(State):
+    r"""
+    The N-mode vacuum state.
+    """
     def __init__(self, num_modes: int, hbar: float = 2.0):
         super().__init__(num_modes, hbar, mixed=False)
         self.cov = hbar * self._math_backend.identity(2 * self.num_modes) / 2.0
