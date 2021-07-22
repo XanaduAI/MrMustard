a2df78b9a4379aa30526499726e5015eb7a7625f
ziofil <miatto@gmail.com>
2021-07-22 17:21:49 -0400

    fixed import

--- mrmustard/abstract/__init__.py
+++ mrmustard/abstract/__init__.py
@@ -1,6 +1,6 @@
 from .detector import Detector
 from .state import State
-from .gate import Op
+from .op import Op
 from .parametrized import Parametrized
 
 __all__ = ["Detector", "State", "Op", "Parametrized"]
