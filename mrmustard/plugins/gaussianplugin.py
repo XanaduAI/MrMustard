92b3050eada50f32c5edd4d99a9c5fb9b93391fd
Zhi Han <zhihuahan72@gmail.com>
2021-08-27 12:38:02 -0600

    Update gaussianplugin.py

--- mrmustard/plugins/gaussianplugin.py
+++ mrmustard/plugins/gaussianplugin.py
@@ -1,5 +1,5 @@
-from typing import Any
 from __future__ import annotations
+from typing import Any
 from mrmustard.backends import BackendInterface
 from mrmustard._typing import *
 from math import pi, sqrt
