ec6de89b6fb7b855527342a638475e356f524081
ziofil <miatto@gmail.com>
2021-09-14 10:23:40 -0400

    multiplication done

--- mrmustard/experimental/xptensor.py
+++ mrmustard/experimental/xptensor.py
@@ -29,25 +29,33 @@ class XPTensor:
                 ):
         self.additive = bool(additive) or not bool(multiplicative)  # I love python
         self.shape = None if tensor is None else [t // 2 for t in tensor.shape]
-        self.ndim = None if tensor is None else tensor.ndim
-        self.isVector = None if tensor is None else self.ndim == 1
-        self.isMatrix = None if tensor is None else self.ndim == 2
+        self.ndim = None if tensor is None else len(self.shape)
+        self.isVector = None if tensor is None else = self.ndim == 1
+        self.tensor = None if tensor is None else backend.reshape(tensor, [_ for n in tensor.shape for _ in (2, n)])
         if modes is None and tensor is not None:
-            if self.isVector:
-                modes = [m for m in range(tensor.shape[0] // 2)], []
-            if self.isMatrix:
-                modes = [m for m in range(tensor.shape[0] // 2)], [m for m in range(tensor.shape[1] // 2)]
+            modes = [list(range(s)) for s in self.shape+(0,)*self.isVector]
         assert set(modes[0]).isdisjoint(modes[1]) or set(modes[0]) == set(modes[1])  # either a coherence or a diagonal block
         self.modes = modes
-        self.tensor = None if tensor is None else backend.reshape(tensor, [k for n in tensor.shape for k in (2, n)])
 
     @property
-    def isCoherence(self):
+    def isMatrix(self) -> bool:
+        return None if self.tensor is None else not self.isVector
+
+    @property
+    def isCoherence(self) -> bool:
         return self.isMatrix and self.modes[0] != self.modes[1]
 
     @property
     def multiplicative(self) -> bool:
         return not bool(self.additive)
+
+    @property
+    def T(self) -> XPTensor:
+        if self.isVector:
+            raise ValueError("Cannot transpose a vector")
+        if self.tensor is None:
+            return XPTensor(None, [], self.additive)
+        return XPTensor(backend.reshape(backend.transpose(self.tensor, (0,3,2,1)), (2*self.modes[1], 2*self.modes[0])), (self.modes[1], self.modes[0]), self.additive)
 
     @property
     def outmodes(self) -> List[int]:
@@ -58,38 +66,50 @@ class XPTensor:
         return self.modes[1]
 
     @classmethod
-    def from_xxpp(cls, tensor: Union[Matrix, Vector], modes: Optional[Tuple[List[int],List[int]]], additive: bool = None, multiplicative: bool = None) -> XPTensor:
+    def from_tensor(cls,
+    tensor: Union[Matrix, Vector],
+    modes: Union[Tuple[List[int], List[int]], List[int]] = None,
+    additive=None, multiplicative=None) -> XPTensor:
+        xptensor = cls()
+        xptensor.additive = bool(additive) or not bool(multiplicative)  # I love python
+        xptensor.shape = tensor.shape[1::2]
+        xptensor.ndim = tensor.ndim // 2
+        xptensor.isVector = xptensor.ndim == 1
+        if modes is None:
+            modes = [list(range(s)) for s in xptensor.shape+(0,)*xptensor.isVector]
+        assert set(modes[0]).isdisjoint(modes[1]) or set(modes[0]) == set(modes[1])  # either a coherence or a diagonal block
+        xptensor.modes = modes
+        return xptensor
+
+    @classmethod
+    def from_xxpp(cls,
+    tensor: Union[Matrix, Vector],
+    modes: Optional[Tuple[List[int],List[int]]],
+    additive: bool = None,
+    multiplicative: bool = None) -> XPTensor:
         return XPTensor(tensor, modes, additive, multiplicative)
 
     @classmethod
-    def from_xpxp(cls, tensor: Union[Matrix, Vector], modes: Optional[Tuple[List[int], List[int]]], additive: bool = None, multiplicative: bool = None) -> XPTensor:
+    def from_xpxp(cls,
+    tensor: Union[Matrix, Vector],
+    modes: Optional[Tuple[List[int], List[int]]],
+    additive: bool = None,
+    multiplicative: bool = None) -> XPTensor:
         if tensor is not None:
-            tensor = backend.reshape(tensor, [k for n in matrix.shape for k in (n, 2)])
-            tensor = backend.transpose(tensor, (1, 0, 3, 2)[: 2 * tensor.ndim])
-            tensor = backend.reshape(tensor, [2 * s for s in matrix.shape])
-        return cls(tensor, modes, additive, multiplicative)
+            tensor = backend.reshape(tensor, [_ for n in matrix.shape for _ in (n, 2)])
+            tensor = backend.transpose(tensor, (1, 0, 3, 2) if self.isMatrix else (1, 0))
+        return cls.from_tensor(tensor, modes, additive, multiplicative)
 
     def to_xpxp(self) -> Optional[Matrix]:
         if self.tensor is None:
             return None
-        tensor = backend.transpose(self.tensor, (1, 0, 3, 2)[: 2 * self.ndim])
+        tensor = backend.transpose(self.tensor, (1, 0, 3, 2) if self.isMatrix else (1, 0))
         return backend.reshape(tensor, [2 * s for s in self.shape])
 
     def to_xxpp(self) -> Optional[Tensor]:
         if self.tensor is None:
             return None
-        return backend.reshape(self.tensor, [2 * s for s in matrix.shape])
-
-    def clone(self, modes: Sequence[int], times: int):  # TODO: finish this
-        r"""Artificially clones the given modes a given number of times and includes them as new modes."""
-        if self.tensor is None:
-            pass
-        if self.isCoherence:
-            raise NotImplementedError("Cloning of pure coherences is not yet implemented.")
-        if self.isVector:
-            return XPTensor(backend.concat([self.tensor] + ([to_clone] for _ in range(times)), axis=1), (self.outmodes + outmodes*times, self.inmodes), self.additive, self.multiplicative)
-        else:
-            pass
+        return backend.reshape(self.tensor, [2 * s for s in self.shape])
 
     def __array__(self):
         return self.tensor
@@ -100,10 +120,17 @@ class XPTensor:
                 raise NotImplementedError("Cannot multiply a scalar by a multiplicative null tensor yet")
             else:
                 return XPTensor(None, self.modes, self.additive, self.multiplicative)
-        return XPTensor(other * self.tensor, self.modes, self.additive, self.multiplicative)
+        self.tensor = other * self.tensor
+        return self
 
     def __mul__(self, other: Scalar) -> Optional[XPTensor]:
         return other * self if self.tensor is not None else None
+
+    def _join_modes(other: XPTensor) -> Tuple[List[int], List[int], List[int]]:
+        contracted = [i for i in self.inmodes if i in other.outmodes]
+        uncontracted_other = [o for o in other.outmodes if o not in contracted]
+        uncontracted_self = [i for i in self.inmodes if i not in contracted]
+        return contracted, uncontracted_other, uncontracted_self
 
     def __matmul__(self, other: XPTensor) -> Optional[XPTensor]:
         # both None
@@ -115,113 +142,98 @@ class XPTensor:
         if other.tensor is None:
             return other if other.additive else self
         # Now neither self nor other is None
-        if self.isMatrix and other.isVector:
-            xxpp, modes = self.mode_aware_matvec(other)
+        if self.isMatrix:
+            tensor, modes = self._mode_aware_matmul(other)
         elif self.isVector and other.isMatrix:
-            xxpp, modes = other.T.mode_aware_matvec(self)
-        elif self.isMatrix and other.isMatrix:
-            xxpp, modes = self.mode_aware_matmul(other)
+            tensor, modes = other.T.mode_aware_matmul(self)
         else: # self.isVector and other.isVector:
-            xxpp, modes = self.mode_aware_vecvec(other)
-        return XPTensor(xxpp, modes, multiplicative=self.multiplicative and other.multiplicative)
+            tensor, modes = self._mode_aware_vecvec(other)
+        return XPTensor.from_tensor(tensor, modes, multiplicative=self.multiplicative and other.multiplicative)
 
-    def _join_modes_prod_right(other: XPTensor) -> Tuple[List[int], List[int], List[int]]:
-        contracted = [i for i in self.inmodes if i in other.outmodes]
-        uncontracted_other = [o for o in other.outmodes if o not in contracted]
-        if set(self.outmodes).intersection(uncontracted_other) is not None:
-            raise ValueError("outmodes are not disjoint")
-        uncontracted_self = [i for i in self.inmodes if i not in contracted]
-        if set(other.inmodes).intersection(uncontracted_self) is not None:
-            raise ValueError("inmodes are not disjoint")
-        return uncontracted_self, contracted, uncontracted_other
-
-    def _mode_aware_matmul(self, other:XPTensor) -> Tuple[Matrix, Tuple[List[int], List[int]]]:
+    def _mode_aware_matmul(self, other:XPTensor) -> Tuple[Tensor, Tuple[List[int], List[int]]]:
+        r"""Performs matrix multiplication only on the necessary modes and
+        takes care of keeping only the modes that are needed, in case of mismatch.
+        """
         if self.inmodes == other.outmodes:
             return backend.tensordot(self.tensor, other.tensor, ((2,3),(0,1))), (self.outmodes, other.inmodes)
-        # otherwise, there's a mismatch
-        uncontracted_self, contracted, uncontracted_other = self._join_modes_prod_right(other)
-        if set(self.inmodes).issubset(other.outmodes): # other is bigger than us and we plug entirely into it
-            if self.additive: # we trow away lots of stuff from other
-                return backend.tensordot(self.tensor[:,:,:,contracted], other.tensor[:,contracted], ((2,3),(0,1))), (modes_out, modes_in)
-        
-        product = backend.tensordot(self.tensor[:,:,:,contracted], other[:,contracted,:,:], ((2,3),(0,1))) # result without the uncontracted modes
-        if self.additive:
-            if other.additive:
-                return product, (modes_out, modes_in)
-            else:
-                other.tensor[:,modes_out] = product
-                return other, (modes_out, modes_in)
-            self.tensor[:,modes_out,:,contracted] = product[:,:,:,contracted]
-        return self.to_xxpp(), (modes_out, modes_in)
+        contracted, uncontracted_other, uncontracted_self = self._join_modes(other)
+        outmodes = self.outmodes + uncontracted_other if self.multiplicative else self.outmodes
+        inmodes = other.inmodes + uncontracted_self if other.multiplicative else other.inmodes
+        if list(set(outmodes)) != list(outmodes) or list(set(inmodes)) != list(inmodes):
+            raise ValueError("modes are not disjoint")
+        blue = backend.tensordot(backend.gather(self.tensor, contracted, axis=3), backend.gather(other.tensor, contracted, axis=1), ((2,3),(0,1)))
+        green = backend.gather(other, uncontracted_other, axis=1) if self.multiplicative else None
+        purple = backend.gather(self, uncontracted_self, axis=3) if other.multiplicative else None
+        white = backend.zeros((2, green.shape[0], 2, purple.shape[1]), dtype=blue.dtype) if self.multiplicative and other.multiplicative else None
+        if green is not None and purple is not None:
+            final = backend.concatenate((backend.concatenate((blue, green), axis=1), backend.concatenate((purple, white), axis=1)), axis=3)
+        elif green is not None and purple is None:
+            final = backend.concatenate((blue, green), axis=1)
+        elif green is None and purple is not None:
+            final = backend.concatenate((blue, purple), axis=3)
         else:
-            self.tensor[:,contracted,:,:] = backend.reshape(product, (2, len(contracted), 2, len(contracted)))
-        return other.to_xxpp(), (modes_out, modes_in)
+            final = blue
+        tr_out = [outmodes.index(o) for o in sorted(outmodes)]
+        tr_in = [inmodes.index(i) for i in sorted(inmodes)]
+        return backend.gather(backend.gather(final, tr_out, axis=1), tr_in, axis=3), (list(sorted(outmodes)), list(sorted(inmodes)))
 
-    def mode_aware_matvec(self, xxpp_a: Matrix, xxpp_b: Vector, modes_a: List[int], modes_b: List[int]) -> Vector:
-        if modes_a == modes_b:
-            return backend.matvec(xxpp_a, xxpp_b)
-        modes = set(modes_a) | set(modes_b)
-        out = backend.zeros(2 * len(modes), dtype=xxpp_b.dtype)
-        out = backend.add_at_modes(xxpp_b, out, modes_b)
-        out = backend.matvec_at_modes(xxpp_a, out, modes_a)
-        return out, modes
-
-    def mode_aware_vecvec(self, xxpp_a: Vector, xxpp_b: Vector, modes_a: List[int], modes_b: List[int]) -> Scalar:
-        if modes_a == modes_b:
-            return backend.sum(xxpp_a * xxpp_b)
-        modes = set(modes_a) & set(modes_b)  # only the common modes
-        out = backend.vecvec_at_modes(xxpp_a, out, modes)
-        return out, modes
+    def _mode_aware_vecvec(self, other: XPTensor) -> Scalar:
+        if self.outmodes == other.outmodes:
+            return backend.sum(self.tensor * other.tensor)
+        contracted = list(set(self.outmodes) & set(other.outmodes))  # only the common modes
+        return backend.sum(backend.gather(self.tensor, contracted, axis=1) * backend.gather(other.tensor, contracted, axis=1))
 
     def __add__(self, other: XPTensor) -> Optional[XPTensor]:
-        if self._tensor is None and other._tensor is None:
-            return XPTensor(None, [], self.additive and other.additive)  # NOTE: this says 1+1 = 1
-        if self._tensor is None:  # only self is None
+        if self.tensor is None and other.tensor is None: # both are none
+            if self.multiplicative and other.multiplicative:
+                raise ValueError("Cannot add two multiplicative null tensors yet")
+            return XPTensor(additive = self.additive and other.additive) # 0+0 = 0
+        if self.tensor is None:  # only self is None
             if self.additive:
                 return other
-            return ValueError("0+1 not implemented 🥸")
-        if other._tensor is None:  # only other is None
+            return ValueError("1+other not implemented 🥸")
+        if other.tensor is None:  # only other is None
             if other.additive:
                 return self
-            return ValueError("1+0 not implemented 🥸")
-        xxpp = self.add_at_modes(self.to_xxpp(), other.to_xxpp(), modes_a=self.modes, modes_b=other.modes)
-        return XPTensor.from_xxpp(xxpp, sorted(list(set(self.modes) | set(other.modes))), self.additive and other.additive)
+            return ValueError("self+1 not implemented 🥸")
+        if self.isVector == other.isVector:
+            tensor, modes = self._mode_aware_add(other)
+            raise ValueError("Cannot add a matrix and a vector")
+        return XPTensor.from_tensor(tensor, modes, additive = self.additive and other.additive)
 
-    def add_at_modes(self, xxpp_a: Matrix, xxpp_b: Matrix, modes_a: List[int], modes_b: List[int]) -> Matrix:
-        if modes_a == modes_b:
-            return xxpp_a + xxpp_b
-        modes = set(modes_a) | set(modes_b)
-        out = backend.zeros((2 * len(modes), 2 * len(modes)), dtype=xxpp_a.dtype)
-        out = backend.add_at_modes(out, xxpp_a, modes_a)
-        out = backend.add_at_modes(out, xxpp_b, modes_b)
-        return out
+    def _mode_aware_add(self, other: XPTensor) -> Tuple[Matrix, Tuple[List[int], List[int]]]:
+        if self.modes == other.modes:
+            return self.tensor + other.tensor, self.modes
+        raise NotImplementedError("Cannot add two xptensors with different modes yet")
 
     def __sub__(self, other: XPTensor) -> Optional[XPTensor]:
         return self + (-1) * other
 
     def __repr__(self) -> str:
-        return f"XPTensor(modes={self.modes}, additive={self.additive}, _tensor={self._tensor})"
+        return f"XPTensor(modes={self.modes}, additive={self.additive}, tensor_xpxp={self.tensor.to_xpxp()})"
 
-    def __getitem__(self, item: Union[int, slice, List[int]]) -> XPTensor:
+    def __getitem__(self, item: Union[int, slice, List[int]]) -> Tensor:
         r"""
-        Returns modes or subsets of modes from the XPTensor, or coherences between modes.
+        Returns modes or subsets of modes from the XPTensor, or coherences between modes using an intuitive notation
         Examples:
-        >>> T[0]  # returns obly outmode 0 (result has only inmodes, i.e. it's a phase space covector)
-        >>> T[:, 0]  # returns only inmode 0 (result has only outmodes, i.e. it's a phase space vector)
-        >>> T[0, 1]  # returns coherence between modes 0 and 1
-        >>> T[:, 0:3]  # returns block with all outmodes and inmodes 0, 1, 2
-        >>> T[[0, 2, 12]]  # returns block with outmodes 0, 2, 12 and all inmodes
-        >>> T[0:3, [0, 10]]  # returns block with outmodes 0, 1, 2 and inmodes 0, 10
-        
+            T[M] = self.tensor[:,M,...]
+            T[M,N] = self.tensor[:,M,:,N]
+            T[:,N] = self.tensor[:,:,:,N]
+            T[[1,2,3],:] = self.tensor[:,[1,2,3],:,N]
+            T[[1,2,3],[4,5]] = self.tensor[:,[1,2,3],:,[4,5]]  # i.e. the rows [1,2,3] and columns [4,5]
         """
-        if self._tensor is None:
-            return XPTensor(None, self.__getitem_list(item), self.additive)
+
+        _all = slice(None)
+        if isinstance(item, int):
+            return XPTensor.from_tensor(backend.expand_dims(self.tensor[:,item,...], axis=1), modes=(self.modes[0][item], self.modes[1]), additive=self.additive)
+        if self.tensor is None:
+            return XPTensor(additive=self.additive)
         if isinstance(item, tuple) and len(item) == 2:
             if self.isVector:
                 raise ValueError("Cannot index a vector with 2 indices")
-            return XPTensor(backend.getitem(self.tensor, (slice(None), item[0], slice(None), item[1])), self.modes, self.additive)
+            return XPTensor(backend.getitem(self.tensor, (_all, item[0], _all, item[1])), self.modes, self.additive)
         if isinstance(item, int):
-            return XPTensor(backend.getitem(self.tensor, (slice(None), item, slice(None))[:2+self.isMatrix]), self.modes, self.additive)
+            return XPTensor(backend.getitem(self.tensor, (_all, item)[:2+self.isMatrix]), self.modes, self.additive)
         if isinstance(item, slice):
             return XPTensor(backend.getitem(self.tensor, (item, slice(
         # the right indices (don't exceed 2 or 1 index)
@@ -244,7 +256,7 @@ class XPTensor:
         else:
             self._tensor = backend.setitem(self.tensor, (slice(), key), value.tensor[:, key])
 
-    def __getitem_list(self, item=None):
+    def __getitem_tuple(self, item=None):
         if isinstance(item, int):
             lst = [item]
         elif isinstance(item, slice):
@@ -257,10 +269,4 @@ class XPTensor:
             raise ValueError(f"Invalid item: {item}")
         return lst
 
-    @property
-    def T(self) -> XPTensor:
-        if self.isVector:
-            raise ValueError("Cannot transpose a vector")
-        if self._tensor is None:
-            return XPTensor(None, [], self.additive)
-        return XPTensor(backend.transpose(self._tensor), self.modes)
+
