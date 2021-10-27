18d1d00fe31e61fc7ec45a07347a62d348f39412
ziofil <miatto@gmail.com>
2021-10-27 14:50:43 -0400

    added blurb at beginning of files

--- mrmustard/_typing.py
+++ mrmustard/_typing.py
@@ -1,13 +1,28 @@
+# Copyright 2021 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import *
 
-# NOTE: when type-annotating with typevars, objects with the same typevars must have the same type
+# NOTE: when type-annotating with typevars, objects in a function signature with the same typevars must have the same type
 # E.g. in `def f(x: Vector, y: Vector) -> Tensor: ...`
 # the type of `x` and the type of `y` are assumed to be the same, even though "Vector" can mean different things.
-Scalar = TypeVar("Scalar")
-Vector = TypeVar("Vector")
-Matrix = TypeVar("Matrix")
-Tensor = TypeVar("Tensor")
-Array = TypeVar("Array")  # TODO: let mypy know that this is Vector, Matrix, or Tensor
+
+Scalar = TypeVar("Scalar")  # rank 0
+Vector = TypeVar("Vector")  # rank 1
+Matrix = TypeVar("Matrix")  # rank 2
+Tensor = TypeVar("Tensor")  # rank > 2
+Array = TypeVar("Array")  # TODO: let mypy know that this is Vector, Matrix or Tensor
 Trainable = TypeVar("Trainable")
 
 Numeric = Union[Scalar, Vector, Matrix, Tensor]
--- mrmustard/tests/test_plugins/test_gaussian/test_gaussian_utils.py
+++ mrmustard/tests/test_plugins/test_gaussian/test_gaussian_utils.py
@@ -1,3 +1,17 @@
+# Copyright 2021 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import numpy as np
 from hypothesis import given, strategies as st
 from mrmustard import *
@@ -38,11 +52,3 @@ def test_partition_cov_2modes():
 
 def test_partition_cov_3modes():
     pass  # TODO
-
-    # arr = np.array([[1,2,3,4,5,6],
-    #                 [7,8,9,10,11,12],
-    #                 [13,14,15,16,17,18],
-    #                 [19,20,21,22,23,24],
-    #                 [25,26,27,28,29,30],
-    #                 [31,32,33,34,35,36]])
-    # A,B,AB = gp.partition_cov(gp.backend.astensor(arr), Amodes=[0,2])
