514f4f2ae6c97ab14092f7db7e05386874777059
Nicolas Quesada <zeitus@gmail.com>
2021-11-04 21:39:27 -0400

    Passes black

--- mrmustard/tests/test_opt.py
+++ mrmustard/tests/test_opt.py
@@ -28,7 +28,13 @@ from mrmustard.lab.states import Vacuum
 def test_S2gate_coincidence_prob(n):
     """Testing the optimal probability of obtaining |n,n> from a two mode squeezed vacuum"""
     tf.random.set_seed(137)
-    S = S2gate(modes=[0, 1], r=abs(np.random.normal()), phi=np.random.normal(), r_trainable=True, phi_trainable=True)
+    S = S2gate(
+        modes=[0, 1],
+        r=abs(np.random.normal()),
+        phi=np.random.normal(),
+        r_trainable=True,
+        phi_trainable=True,
+    )
 
     def cost_fn():
         return -tf.abs(S(Vacuum(2)).ket(cutoffs=[n + 1, n + 1])[n, n]) ** 2
@@ -47,7 +53,15 @@ def test_hong_ou_mandel_optimizer():
     r = np.arcsinh(1.0)
     circ.append(S2gate(modes=[0, 1], r=r, phi=0.0, phi_trainable=True))
     circ.append(S2gate(modes=[2, 3], r=r, phi=0.0, phi_trainable=True))
-    circ.append(BSgate(modes=[1, 2], theta=np.random.normal(), phi=np.random.normal(), theta_trainable=True, phi_trainable=True))
+    circ.append(
+        BSgate(
+            modes=[1, 2],
+            theta=np.random.normal(),
+            phi=np.random.normal(),
+            theta_trainable=True,
+            phi_trainable=True,
+        )
+    )
     state_in = Vacuum(num_modes=4)
 
     def cost_fn():
@@ -56,6 +70,7 @@ def test_hong_ou_mandel_optimizer():
     opt = Optimizer(euclidean_lr=0.01)
     opt.minimize(cost_fn, by_optimizing=[circ], max_steps=300)
     assert np.allclose(np.cos(circ.trainable_parameters["euclidean"][2]) ** 2, 0.5, atol=1e-2)
+
 
 def test_squeezing_hong_ou_mandel_optimizer():
     """Finding the optimal squeezing parameter to get Hong-Ou-Mandel dip in time
@@ -66,7 +81,9 @@ def test_squeezing_hong_ou_mandel_optimizer():
     r = np.arcsinh(1.0)
     circ.append(S2gate(modes=[0, 1], r=r, phi=0.0, phi_trainable=True))
     circ.append(S2gate(modes=[2, 3], r=r, phi=0.0, phi_trainable=True))
-    circ.append(S2gate(modes=[1, 2], r=1.0, phi=np.random.normal(), r_trainable=True, phi_trainable=True))
+    circ.append(
+        S2gate(modes=[1, 2], r=1.0, phi=np.random.normal(), r_trainable=True, phi_trainable=True)
+    )
     state_in = Vacuum(num_modes=4)
 
     def cost_fn():
@@ -77,15 +94,28 @@ def test_squeezing_hong_ou_mandel_optimizer():
     assert np.allclose(np.sinh(circ.trainable_parameters["euclidean"][2]) ** 2, 1, atol=1e-2)
 
 
-
 def test_learning_two_mode_squeezing():
     """Finding the optimal beamsplitter transmission to make a pair of single photons"""
     tf.random.set_seed(137)
     circ = Circuit()
     circ.append(
-        Sgate(modes=[0, 1], r=abs(np.random.normal(size=(2))), phi=np.random.normal(size=(2)), r_trainable=True, phi_trainable=True)
+        Sgate(
+            modes=[0, 1],
+            r=abs(np.random.normal(size=(2))),
+            phi=np.random.normal(size=(2)),
+            r_trainable=True,
+            phi_trainable=True,
+        )
     )
-    circ.append(BSgate(modes=[0, 1], theta=np.random.normal(), phi=np.random.normal(), theta_trainable=True, phi_trainable=True))
+    circ.append(
+        BSgate(
+            modes=[0, 1],
+            theta=np.random.normal(),
+            phi=np.random.normal(),
+            theta_trainable=True,
+            phi_trainable=True,
+        )
+    )
     tf.random.set_seed(20)
     state_in = Vacuum(num_modes=2)
 
@@ -120,7 +150,13 @@ def test_learning_two_mode_Interferometer():
     np.random.seed(11)
     circ = Circuit()  # emtpy circuit with vacuum input state
     circ.append(
-        Sgate(modes=[0, 1], r=np.random.normal(size=(2)) ** 2, phi=np.random.normal(size=(2)), r_trainable=True, phi_trainable=True)
+        Sgate(
+            modes=[0, 1],
+            r=np.random.normal(size=(2)) ** 2,
+            phi=np.random.normal(size=(2)),
+            r_trainable=True,
+            phi_trainable=True,
+        )
     )
     circ.append(Interferometer(num_modes=2, orthogonal_trainable=True))
     state_in = Vacuum(num_modes=2)
@@ -139,19 +175,35 @@ def test_learning_four_mode_Interferometer():
     """Finding the optimal Interferometer to make a NOON state with N=2"""
     np.random.seed(11)
     circ = Circuit()
-    circ.append(Sgate(r=np.random.uniform(size=4), phi=np.random.normal(size=4), r_trainable=True, phi_trainable=True))
+    circ.append(
+        Sgate(
+            r=np.random.uniform(size=4),
+            phi=np.random.normal(size=4),
+            r_trainable=True,
+            phi_trainable=True,
+        )
+    )
     circ.append(Interferometer(num_modes=4, orthogonal_trainable=True))
     state_in = Vacuum(num_modes=4)
 
     def cost_fn():
         amps = circ(state_in).ket(cutoffs=[3, 3, 3, 3])
-        return -tf.abs(tf.reduce_sum(amps[1, 1] * np.array([[0, 0, 1 / np.sqrt(2)], [0, 0, 0], [1 / np.sqrt(2), 0, 0]]))) ** 2
+        return (
+            -tf.abs(
+                tf.reduce_sum(
+                    amps[1, 1]
+                    * np.array([[0, 0, 1 / np.sqrt(2)], [0, 0, 0], [1 / np.sqrt(2), 0, 0]])
+                )
+            )
+            ** 2
+        )
 
     opt = Optimizer(symplectic_lr=0.5, euclidean_lr=0.01)
 
     opt.minimize(cost_fn, by_optimizing=[circ], max_steps=1000)
     assert np.allclose(-cost_fn(), 0.0625, atol=1e-3)
-    
+
+
 def test_squeezing_hong_ou_mandel_optimizer():
     """Finding the optimal squeezing parameter to get Hong-Ou-Mandel dip in time
     see https://www.pnas.org/content/117/52/33107/tab-article-info
@@ -161,7 +213,9 @@ def test_squeezing_hong_ou_mandel_optimizer():
     r = np.arcsinh(1.0)
     circ.append(S2gate(modes=[0, 1], r=r, phi=0.0, phi_trainable=True))
     circ.append(S2gate(modes=[2, 3], r=r, phi=0.0, phi_trainable=True))
-    circ.append(S2gate(modes=[1, 2], r=1.0, phi=np.random.normal(), r_trainable=True, phi_trainable=True))
+    circ.append(
+        S2gate(modes=[1, 2], r=1.0, phi=np.random.normal(), r_trainable=True, phi_trainable=True)
+    )
     state_in = Vacuum(num_modes=4)
 
     def cost_fn():
