5c22ee86093cf6c87de73708961033c1bd56970c
ziofil <miatto@gmail.com>
2021-11-24 16:00:09 -0500

    more settings

--- mrmustard/__init__.py
+++ mrmustard/__init__.py
@@ -14,9 +14,10 @@ class Settings:
         self.HBAR = 2.0
         self.CHOI_R = 0.881373587019543  # np.arcsinh(1.0)
         self.DEBUG = False
-        # 7x the sqrt of the photon number variance when auto-detecting the Fock cutoff for a mode
-        self.AUTOCUTOFF_SIGMA_FACTOR = 7
+        # mean + 7*std when auto-detecting the Fock cutoff for a mode
+        self.AUTOCUTOFF_STDEV_FACTOR = 7
         self.AUTOCUTOFF_MAX_CUTOFF = 100
+        self.AUTOCUTOFF_MIN_CUTOFF = 1
         # using cutoff=5 for each mode when determining if two transformations in fock repr are equal
         self.EQ_TRANSFORMATION_CUTOFF = 5
         self.EQ_TRANSFORMATION_RTOL_FOCK = 1e-3
