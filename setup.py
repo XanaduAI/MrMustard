0e4021b86ac430ae9ed6768e5d8ec39c3cc29e1f
Theodor Isacsson <theodor@xanadu.ai>
2021-11-30 18:21:23 -0500

    update setup

--- setup.py
+++ setup.py
@@ -11,21 +11,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-r"""Basic setup module"""
 from setuptools import setup, find_packages
 
+with open("mrmustard/_version.py") as f:
+    version = f.readlines()[-1].split()[-1].strip("\"'")
 
 requirements = [
-    "numpy==1.19.2",
-    "scipy",
-    "numba",
-    "repoze.lru",
-    "tensorflow==2.4.1",
-    "rich",
-    "tqdm",
+    "numpy"
+    "scipy"
+    "numba"
+    "thewalrus>=0.17.0"
+    "tensorflow>=2.4.0"
+    "rich"
+    "tqdm"
+    "matplotlib"
+    "strawberryfields>=0.20.0"
 ]
 
+info = {
+    "name": "mrmustard",
+    "version": version,
+    "description": "Differentiable quantum Gaussian circuits",
+    "url": "https://github.com/XanaduAI/mrmustard",
+    "author": "Xanadu",
+    "author_email": "filippo@xanadu.ai",
+    "license": "Apache License 2.0",
+    "packages": find_packages(where="."),
+    "install_requires": requirements,
+    "long_description": open("README.md", encoding="utf-8").read(),
+    "long_description_content_type": "text/markdown",
+}
 
 classifiers = [
     "Development Status :: 4 - Beta",
@@ -41,22 +56,9 @@ classifiers = [
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3 :: Only",
     "Topic :: Scientific/Engineering :: Physics",
 ]
 
-
-setup(
-    name="Mr. Mustard",
-    version="0.0.1",
-    description="Differentiable quantum Gaussian circuits",
-    url="https://github.com/XanaduAI/mrmustard",
-    author="Xanadu",
-    author_email="filippo@xanadu.ai",
-    license="Apache License 2.0",
-    packages=find_packages(where="."),
-    install_requires=requirements,
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    classifiers=classifiers,
-)
+setup(classifiers=classifiers, **(info))
