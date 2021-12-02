c25ee09fd1780827a811919b41dec26aa4b2c519
Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>
2021-12-02 09:50:33 -0500

    Include suggestion for mrmustard/physics/__init__.py docstring
    Co-authored-by: Theodor <theodor@xanadu.ai>

--- mrmustard/physics/__init__.py
+++ mrmustard/physics/__init__.py
@@ -14,8 +14,7 @@
 
 r"""
 The physics module contains high-level functions for performing calculations on quantum objects.
-It allows for objects in different representation to be used seamlessly in the same calculation.
-E.g. :code:`fidelity(A, B)` works whether A and B are in Fock or Gaussian representation or a mix of both.
+It allows for objects with different representations to be used seamlessly in the same calculation, e.g. :code:`fidelity(A, B)` works whether A and B are in the Fock or Gaussian representation or a mix of both.
 
 All the functions are automatically differentiated and can be used in conjunction with an
 optimiization routine.
