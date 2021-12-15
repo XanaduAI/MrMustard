486f773a8fdd9573c2e933a2ef26bc69b529ca13
ziofil <ziofil@users.noreply.github.com>
2021-12-15 16:23:56 -0500

    Update mrmustard/physics/fock.py
    Co-authored-by: Theodor <theodor@xanadu.ai>

--- mrmustard/physics/fock.py
+++ mrmustard/physics/fock.py
@@ -377,7 +377,7 @@ def norm(state: Tensor, is_dm: bool):
     Returns the norm of a ket or the trace of the density matrix.
     Note that the "norm" is intended as that float number that is used to normalize the state,
     which depends on the representation. Hence different numbers for different representations
-    of the same state (|amp| for ket and |amp|^2 for dm).
+    of the same state (:math:`|amp|` for ``ket`` and :math:`|amp|^2` for ``dm``).
     """
     if is_dm:
         return math.sum(math.all_diagonals(state, real=True))
