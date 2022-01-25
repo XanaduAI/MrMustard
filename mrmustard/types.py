b2e48c24f2e0da0d5ae6a98641ec0a1f922cab49
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2022-01-25 09:26:58 -0500

    refactor - types

--- mrmustard/types.py
+++ mrmustard/types.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# pylint: disable=unused-wildcard-import
+
 from typing import *
 
 # NOTE: when type-annotating with typevars, objects with the same typevars must have the same type
