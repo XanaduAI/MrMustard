e1db50d83ecc1b31057c187898e21ced1250efe7
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2022-01-26 15:32:56 -0500

    refactor [utils] - parametrized

--- mrmustard/utils/parametrized.py
+++ mrmustard/utils/parametrized.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""This module contains the :class:`.Parametrized` class which acts as
+an abstract base class for all parametrized objects
+"""
 
 from mrmustard.types import Tensor, Dict, List, Trainable
 from mrmustard.math import Math
