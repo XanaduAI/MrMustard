86022affa64bed7706edea6617b619c90be5f084
Theodor <theodor@xanadu.ai>
2022-02-01 11:51:21 -0500

    Run latest Black version (#113)
    * run latest black
    
    * add black to requirements-dev

--- tests/test_physics/test_fock/test_fock.py
+++ tests/test_physics/test_fock/test_fock.py
@@ -58,7 +58,7 @@ def test_coherent_state(alpha):
     cutoff = 10
     amps = Coherent(x=alpha.real, y=alpha.imag).ket(cutoffs=[cutoff])
     expected = np.exp(-0.5 * np.abs(alpha) ** 2) * np.array(
-        [alpha ** n / np.sqrt(factorial(n)) for n in range(cutoff)]
+        [alpha**n / np.sqrt(factorial(n)) for n in range(cutoff)]
     )
     assert np.allclose(amps, expected, atol=1e-6)
 
@@ -79,7 +79,7 @@ def test_squeezed_state(r, phi):
             [
                 (-np.exp(1j * phi) * np.tanh(r)) ** n
                 * np.sqrt(factorial(2 * n))
-                / (2 ** n * factorial(n))
+                / (2**n * factorial(n))
                 for n in range(len_non_zero)
             ]
         )
