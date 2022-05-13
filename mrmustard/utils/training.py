a7c419a2c28411d485ac02d014130905283b5dd2
ziofil <miatto@gmail.com>
2022-05-13 17:27:27 -0400

    removes commented code and uses logging flag

--- mrmustard/utils/training.py
+++ mrmustard/utils/training.py
@@ -103,7 +103,8 @@ class Optimizer:
                 sum(abs(self.opt_history[-i - 1] - self.opt_history[-i]) for i in range(1, 20))
                 < 1e-6
             ):
-                self.log.info("Loss looks stable, stopping here.")
+                if settings.LOGGING:
+                    self.log.info("Loss looks stable, stopping here.")
                 return True
         return False
 
@@ -118,54 +119,6 @@ class Optimizer:
 # Static functions
 # ~~~~~~~~~~~~~~~~~
 
-
-# def new_variable(
-#     value, bounds: Tuple[Optional[float], Optional[float]], name: str, dtype=math.float64
-# ) -> Trainable:
-#     r"""Returns a new trainable variable from the current math backend
-#     with initial value set by `value` and bounds set by `bounds`.
-#
-#     Args:
-#         value (float): The initial value of the variable
-#         bounds (Tuple[float, float]): The bounds of the variable
-#         name (str): The name of the variable
-#         dtype: The dtype of the variable
-#
-#     Returns:
-#         variable (Trainable): The new variable
-#     """
-#     return math.new_variable(value, bounds, name, dtype)
-
-
-# def new_constant(value, name: str, dtype=math.float64) -> Tensor:
-#     r"""Returns a new constant (non-trainable) tensor from the current math backend
-#     with initial value set by `value`.
-#     Args:
-#         value (numeric): The initial value of the tensor
-#         name (str): The name of the constant
-#         dtype: The dtype of the constant
-#
-#     Returns:
-#         tensor (Tensor): The new constant tensor
-#     """
-#     return math.new_constant(value, name, dtype)
-
-
-def new_symplectic(num_modes: int) -> Tensor:
-    r"""Returns a new symplectic matrix from the current math backend with ``num_modes`` modes.
-
-    Args:
-        num_modes (int): the number of modes in the symplectic matrix
-
-    Returns:
-        Tensor: the new symplectic matrix
-    """
-    return math.random_symplectic(num_modes)
-
-
-def new_orthogonal(num_modes: int) -> Tensor:
-    """Returns a random orthogonal matrix in :math:`O(2*num_modes)`."""
-    return math.random_orthogonal(num_modes)
 
 
 def update_symplectic(
