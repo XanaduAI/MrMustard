deee04cfa48d5099e7541e5ea183fc449f063bf2
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2022-05-18 14:50:56 -0500

    refactor [parametrized] - use parameter classes

--- mrmustard/utils/parametrized.py
+++ mrmustard/utils/parametrized.py
@@ -13,11 +13,12 @@
 # limitations under the License.
 
 """This module contains the :class:`.Parametrized` class which acts as
-an abstract base class for all parametrized objects
+an abstract base class for all parametrized objects.
 """
 
-from mrmustard.types import Tensor, Dict, List, Trainable
+from mrmustard.types import Tensor, Dict, List, Tuple, Union
 from mrmustard.math import Math
+from mrmustard.utils.parameter import create_parameter
 
 math = Math()
 
@@ -31,73 +32,14 @@ class Parametrized:
     """
 
     def __init__(self, **kwargs):  # NOTE: only kwargs so that we can use the arg names
-        self._trainable_parameters = {"symplectic": [], "euclidean": [], "orthogonal": []}
-        self._constant_parameters = {"symplectic": [], "euclidean": [], "orthogonal": []}
-        self._param_names = []
+
         owner = f"{self.__class__.__qualname__}"
+
         for name, value in kwargs.items():
-            trainable = (name + "_trainable" in kwargs and kwargs[name + "_trainable"] is True) or (
-                math.from_backend(value) and math.is_trainable(value)
-            )
-            constant = (
-                name + "_trainable" in kwargs
-                and kwargs[name + "_trainable"] is False
-                or (math.from_backend(value) and not math.is_trainable(value))
-            )
-            param_type = (
-                "symplectic"
-                if name.startswith("symplectic")
-                else "orthogonal"
-                if name.startswith("orthogonal")
-                else "euclidean"
-            )
-            if trainable:
-                if not math.from_backend(value) or (
-                    math.from_backend(value) and not math.is_trainable(value)
-                ):
-                    value = math.new_variable(value, kwargs[name + "_bounds"], owner + ":" + name)
-                self._trainable_parameters[param_type].append(value)
-            elif constant:
-                if not math.from_backend(value):
-                    value = math.new_constant(value, owner + ":" + name)
-                self._constant_parameters[param_type].append(value)
-            else:
-                name = "_" + name
-            self.__dict__[name] = value
-            self._param_names += [] if name.startswith("_") else [name]
+            is_trainable = kwargs.get(f"{name}_trainable", False) or math.is_trainable(value)
+            if math.from_backend(value) or is_trainable:
+                bounds = kwargs.get(f"{name}_bounds", None)
+                value = create_parameter(name, value, is_trainable, bounds, owner)
 
-    @property
-    def trainable_parameters(self) -> Dict[str, List[Trainable]]:
-        r"""Returns the dictionary of trainable parameters, searching recursively in the object tree (for example, when in a Circuit)."""
-        if hasattr(self, "_ops"):
-            return {
-                "symplectic": math.unique_tensors(
-                    [p for op in self._ops for p in op.trainable_parameters["symplectic"]]
-                ),
-                "orthogonal": math.unique_tensors(
-                    [p for op in self._ops for p in op.trainable_parameters["orthogonal"]]
-                ),
-                "euclidean": math.unique_tensors(
-                    [p for op in self._ops for p in op.trainable_parameters["euclidean"]]
-                ),
-            }
-
-        return self._trainable_parameters  # default
-
-    @property
-    def constant_parameters(self) -> Dict[str, List[Tensor]]:
-        r"""Returns the dictionary of constant parameters, searching recursively in the object tree (for example, when in a Circuit)."""
-        if hasattr(self, "_ops"):
-            return {
-                "symplectic": math.unique_tensors(
-                    [p for op in self._ops for p in op.constant_parameters["symplectic"]]
-                ),
-                "orthogonal": math.unique_tensors(
-                    [p for op in self._ops for p in op.constant_parameters["orthogonal"]]
-                ),
-                "euclidean": math.unique_tensors(
-                    [p for op in self._ops for p in op.constant_parameters["euclidean"]]
-                ),
-            }
-
-        return self._constant_parameters  # default
+            # dynamically assign variable as attribute of the class
+            self.__dict__[f"_{name}"] = value
