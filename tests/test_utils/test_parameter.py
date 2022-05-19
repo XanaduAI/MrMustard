5885430718705f25c3d14bbfac62d0dcdd85d185
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2022-05-19 11:06:47 -0500

    test [parameter, parametrized]

---
+++ tests/test_utils/test_parameter.py
@@ -0,0 +1,67 @@
+# Copyright 2022 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import pytest
+
+from mrmustard.utils.parameter import (
+    create_parameter,
+    Constant,
+    Orthogonal,
+    Euclidian,
+    Symplectic,
+    Trainable,
+)
+from mrmustard.math import Math
+
+math = Math()
+
+
+@pytest.mark.parametrize("from_backend", [True, False])
+def test_create_constant(from_backend):
+    """Checks if the factory function `create_parameter`
+    returns an instance of the Constant class when args
+    are not trainable."""
+
+    value = np.random.rand(*np.random.randint(5, size=5))
+    name = "constant_tensor"
+    if from_backend:
+        value = math.new_constant(value, name)
+
+    param = create_parameter(value, name, is_trainable=False)
+
+    assert isinstance(param, Constant)
+    assert math.from_backend(param.value)
+    assert param.name == name
+
+
+@pytest.mark.parametrize("trainable_class", (Euclidian, Orthogonal, Symplectic))
+@pytest.mark.parametrize("from_backend", [True, False])
+@pytest.mark.parametrize("bounds", [None, (0, 10)])
+def test_create_trainable_euclidian(trainable_class, from_backend, bounds):
+    """Checks if the factory function `create_parameter`
+    returns an instance of the Euclidian/Orthogonal/Symplectic class when args
+    are trainable."""
+
+    value = 5
+    name = f"{trainable_class.__name__}_tensor".lower()
+    if from_backend:
+        value = math.new_variable(value, bounds, name)
+
+    param = create_parameter(value, name, is_trainable=True, bounds=bounds)
+
+    assert isinstance(param, trainable_class)
+    assert isinstance(param, Trainable)
+    assert math.from_backend(param.value)
+    assert param.name == name
---
+++ tests/test_utils/test_parametrized.py
@@ -0,0 +1,100 @@
+# Copyright 2022 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
+from hypothesis import given, strategies as st
+import pytest
+
+from mrmustard.utils.parametrized import Parametrized
+from mrmustard.math import Math
+from mrmustard.utils.parameter import Constant, Orthogonal, Euclidian, Symplectic, Trainable
+
+math = Math()
+
+
+def from_python_types():
+    relevant_python_types = [int, float, complex, list, tuple, dict, str, set]
+    chosen_type = random.choice(relevant_python_types)
+    return st.from_type(chosen_type)
+
+
+@given(kwargs=st.dictionaries(st.from_regex("[a-zA-Z]+"), from_python_types(), min_size=1))
+def test_attribute_assignment(kwargs):
+    """Test that arguments are assigned as attributes of the class."""
+    parametrized = Parametrized(**kwargs)
+    keys = kwargs.keys()
+
+    instance_attributes = parametrized.__dict__
+
+    # assert arguments are assigned as attributes of the class
+    expected_attribute_names = [f"_{key}" for key in keys]
+    instance_attribute_names = list(instance_attributes.keys())
+    assert all(attribute in instance_attribute_names for attribute in expected_attribute_names)
+
+    # assert attributes of the class are assigned the correct value
+    assert all(instance_attributes[f"_{key}"] == value for key, value in kwargs.items())
+
+
+@given(kwargs=st.dictionaries(st.from_regex("[a-zA-Z]+"), from_python_types(), min_size=1))
+def test_attribute_type_assignment(kwargs):
+    """Test that attributes of the class that do not belong
+    to the backend are assigned the correct type.
+    """
+    parametrized = Parametrized(**kwargs)
+
+    instance_attributes = parametrized.__dict__
+    assert all(type(instance_attributes[f"_{key}"]) is type(value) for key, value in kwargs.items())
+
+
+@pytest.mark.parametrize("trainable_class", (Euclidian, Orthogonal, Symplectic))
+@pytest.mark.parametrize("bounds", [None, (0, 10)])
+def test_attribute_from_backend_type_assignment(trainable_class, bounds):
+    """Test that arguments that are trainable get defined on the backend,
+    are assigned correctly as attributes of the Parametrized instance
+    and are the correct type of trainable instance.
+    """
+
+    name = f"{trainable_class.__name__}_tensor".lower()
+    value = 5
+    kwargs = {
+        name: value,
+        f"{name}_trainable": True,
+        f"{name}_bounds": bounds,
+    }
+
+    parametrized = Parametrized(**kwargs)
+    attrib = getattr(parametrized, f"_{name}")
+
+    assert isinstance(attrib, trainable_class)
+    assert isinstance(attrib, Trainable)
+    assert math.from_backend(attrib.value)
+    assert attrib.name == name
+
+
+def test_attribute_from_backend_constant_assignment():
+    """Test that arguments that are NOT trainable get defined on the backend,
+    are assigned correctly as attributes of the Parametrized instance
+    and are instances of :class:`Constant`.
+    """
+
+    name = "constant_tensor"
+    value = math.new_constant(5, name)
+    kwargs = {name: value, f"{name}_trainable": False}
+
+    parametrized = Parametrized(**kwargs)
+    attrib = getattr(parametrized, f"_{name}")
+
+    assert isinstance(attrib, Constant)
+    assert math.from_backend(attrib.value)
+    assert attrib.name == name
