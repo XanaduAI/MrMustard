8d10bae35620abc0ccff6bcc68df447826d1deb4
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2022-06-03 12:07:15 -0500

    refactor [training] - Optimizer: user grouped variables and update funcs

--- mrmustard/utils/training.py
+++ mrmustard/utils/training.py
@@ -16,16 +16,63 @@
 used within Mr Mustard.
 """
 
-import itertools
-from mrmustard.types import List, Callable, Sequence, Tensor
+from itertools import chain, groupby
+from nntplib import GroupInfo
+from mrmustard.types import List, Callable, Sequence, Tensor, Tuple
 from mrmustard.utils.parameter import Parameter, Trainable
 from mrmustard.utils import graphics
 from mrmustard.logger import create_logger
 from mrmustard.math import Math
-from mrmustard import settings
 from mrmustard.utils.parametrized import Parametrized
 
 math = Math()
+
+
+def update_symplectic(grads_and_vars: Sequence[Tuple[Tensor, Trainable]], symplectic_lr: float):
+
+    r"""Updates the symplectic parameters using the given symplectic gradients.
+    Implemented from:
+        Wang J, Sun H, Fiori S. A Riemannian-steepest-descent approach
+        for optimization on the real symplectic group.
+        Mathematical Methods in the Applied Sciences. 2018 Jul 30;41(11):4273-86.
+    """
+    for dS_euclidean, S in grads_and_vars:
+        Y = math.euclidean_to_symplectic(S, dS_euclidean)
+        YT = math.transpose(Y)
+        new_value = math.matmul(
+            S, math.expm(-symplectic_lr * YT) @ math.expm(-symplectic_lr * (Y - YT))
+        )
+        math.assign(S, new_value)
+
+
+def update_orthogonal(grads_and_vars: Sequence[Tuple[Tensor, Trainable]], orthogonal_lr: float):
+    r"""Updates the orthogonal parameters using the given orthogonal gradients.
+    Implemented from:
+        Fiori S, Bengio Y. Quasi-Geodesic Neural Learning Algorithms
+        Over the Orthogonal Group: A Tutorial.
+        Journal of Machine Learning Research. 2005 May 1;6(5).
+    """
+    for O, dO_euclidean in grads_and_vars:
+        dO_orthogonal = 0.5 * (
+            dO_euclidean - math.matmul(math.matmul(O, math.transpose(dO_euclidean)), O)
+        )
+        new_value = math.matmul(
+            O, math.expm(orthogonal_lr * math.matmul(math.transpose(dO_orthogonal), O))
+        )
+        math.assign(O, new_value)
+
+
+def update_euclidean(grads_and_vars: Sequence[Tuple[Tensor, Trainable]], euclidean_lr: float):
+    """Updates the parameters using the euclidian gradients."""
+    math.euclidean_opt.lr = euclidean_lr
+    math.euclidean_opt.apply_gradients(grads_and_vars)
+
+
+update_method_dict = {
+    "euclidean": update_euclidean,
+    "symplectic": update_symplectic,
+    "orthogonal": update_orthogonal,
+}
 
 # pylint: disable=disallowed-name
 class Optimizer:
@@ -42,7 +89,7 @@ class Optimizer:
         self, symplectic_lr: float = 0.1, orthogonal_lr: float = 0.1, euclidean_lr: float = 0.001
     ):
         self.learning_rate = {
-            "euclidian": euclidean_lr,
+            "euclidean": euclidean_lr,
             "symplectic": symplectic_lr,
             "orthogonal": orthogonal_lr,
         }
@@ -65,7 +112,7 @@ class Optimizer:
         try:
             # finding out which parameters are trainable from the ops
             trainable_params = list(
-                itertools.chain(
+                chain(
                     *[
                         item.trainable_parameters
                         for item in by_optimizing
@@ -79,9 +126,7 @@ class Optimizer:
 
                 while not self.should_stop(max_steps):
                     cost, grads = loss_and_gradients(cost_fn, trainable_params)
-                    for param, grad in zip(trainable_params, grads):
-                        param_lr = self.learning_rate[param.type]
-                        param.update(grad, param_lr)
+                    self.update_params(trainable_params, grads)
 
                     self.opt_history.append(cost)
                     bar.step(math.asnumpy(cost))
@@ -89,6 +134,31 @@ class Optimizer:
         except KeyboardInterrupt:  # graceful exit
             self.log.info("Optimizer execution halted due to keyboard interruption.")
             raise self.OptimizerInterruptedError() from None
+
+    def update_params(self, trainable_params, grads):
+
+        # group grads and vars by type
+        grouped_vars_and_grads = self._group_vars_and_grads_by_type(trainable_params, grads)
+
+        for param_type, grads_vars in grouped_vars_and_grads.items():
+            param_lr = self.learning_rate[param_type]
+            grads_and_vars = [(grad, p.value) for grad, p in grads_vars]
+            update_method = update_method_dict.get(param_type)
+            update_method(grads_and_vars, param_lr)
+
+    @staticmethod
+    def _group_vars_and_grads_by_type(trainable_params, grads):
+        sorted_grads_and_vars = sorted(
+            zip(grads, trainable_params), key=lambda grads_vars: grads_vars[1].type
+        )
+        grouped = {
+            key: list(result)
+            for key, result in groupby(
+                sorted_grads_and_vars, key=lambda grads_vars: grads_vars[1].type
+            )
+        }
+
+        return grouped
 
     def should_stop(self, max_steps: int) -> bool:
         r"""Returns ``True`` if the optimization should stop (either because the loss is stable or because the maximum number of steps is reached)."""
