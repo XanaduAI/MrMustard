919db568540a2cacf7dec282f1ef56ecc13db565
ziofil <miatto@gmail.com>
2022-09-01 18:28:08 -0400

    making codefactor happy

--- tests/test_lab/test_gates_fock.py
+++ tests/test_lab/test_gates_fock.py
@@ -35,6 +35,7 @@ def test_Dgate_1mode(state, xy):
 
 
 def test_attenuator_on_fock():
+    'tests that attenuating a fock state makes it mixed'
     assert (Fock(10) >> Attenuator(0.5)).is_pure == False
 
 
