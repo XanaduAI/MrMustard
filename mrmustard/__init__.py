3fd12f5293ab7728b8c0d7ea948d6eb936a0cf0c
Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>
2022-09-29 11:36:05 -0400

    fix [coverage] - __init__: remove testing from pythorch version

--- mrmustard/__init__.py
+++ mrmustard/__init__.py
@@ -106,11 +106,10 @@ def about():
     print("The Walrus version:        {}".format(thewalrus.__version__))
     print("TensorFlow version:        {}".format(tensorflow.__version__))
 
-    try:
+    try:  # pragma: no cover
         import torch
 
         torch_version = torch.__version__
+        print("Torch version:             {}".format(torch_version))
     except ImportError:
         torch_version = None
-
-    print("Torch version:             {}".format(torch_version))
