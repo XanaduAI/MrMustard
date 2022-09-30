7fa72f53df673e6889b19a0cb21d72e44a6e854c
Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>
2022-09-30 16:19:30 -0400

    format - black

--- tests/test_integration/test_settings.py
+++ tests/test_integration/test_settings.py
@@ -1,6 +1,7 @@
 from mrmustard import settings
 import pytest
 
+
 def test_backend_name():
     with pytest.raises(ValueError) as exception:
-        settings.backend = 'BUMBUM'
+        settings.backend = "BUMBUM"
