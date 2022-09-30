d5ce4455b1c63623c402837abcd8f1f4f07a057a
ziofil <ziofil@users.noreply.github.com>
2022-09-30 17:18:49 -0400

    adds other backends to the settings test

--- tests/test_integration/test_settings.py
+++ tests/test_integration/test_settings.py
@@ -3,5 +3,7 @@ import pytest
 
 
 def test_backend_name():
+    settings.backend = 'Tensorflow'
+    settings.backend = 'torch'
     with pytest.raises(ValueError) as exception:
         settings.backend = "BUMBUM"
