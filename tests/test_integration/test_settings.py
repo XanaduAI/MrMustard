da60ee9c1b4843da2a3c1a20f453a371d5a05e98
Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>
2022-10-14 16:43:53 -0400

    remove uneeded test

--- tests/test_integration/test_settings.py
+++
@@ -1,9 +0,0 @@
-from mrmustard import settings
-import pytest
-
-
-def test_backend_name():
-    settings.backend = 'Tensorflow'
-    settings.backend = 'torch'
-    with pytest.raises(ValueError) as exception:
-        settings.backend = "BUMBUM"
