9292bde23d9905748634d5ec37ed79e7cf4a1c11
ziofil <miatto@gmail.com>
2022-12-14 14:39:19 -0800

    pleasing codefactor

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Tests for the Settings class.
+"""
+
 from mrmustard import Settings
 
 
