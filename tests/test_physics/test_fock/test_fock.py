e52749e62f5cd9150e78bd14ddfe3f27ef20d5b4
ziofil <miatto@gmail.com>
2023-01-09 13:49:13 -0800

    adds test

--- tests/test_physics/test_fock/test_fock.py
+++ tests/test_physics/test_fock/test_fock.py
@@ -211,3 +211,9 @@ def test_fock_trace_function():
     dm = state.dm([10, 10])
     dm_traced = trace(dm, keep=[0])
     assert np.allclose(dm_traced, State(dm=dm).get_modes(0).dm(), atol=1e-5)
+
+
+def test_choi_application():
+    s = Attenuator(1.0) << State(dm=SqueezedVacuum(1.0, np.pi/2).dm([40]))
+    assert np.allclose(s.dm([10])[:10,:10], SqueezedVacuum(1.0, np.pi/2).dm([10]))
+    # NOTE: the [:10,:10] part is not necessary once PR #184 is merged
