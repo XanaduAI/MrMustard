9557e76f10a4e4d2c25e963b42c08b3ab34695f8
sylviemonet <sylvieyao502@gmail.com>
2023-02-15 14:41:55 +0100

    add REALINTF

--- mrmustard/lab/gates.py
+++ mrmustard/lab/gates.py
@@ -452,9 +452,9 @@ class Interferometer(Parametrized, Transformation):
         ):
             raise ValueError("Invalid number of modes and the mode list here!")
         if unitary is None:
-            unitary = math.random_unitary(num_modes)
-#            U = math.random_unitary(num_modes)
-#            unitary = math.block([[math.real(U), -math.imag(U)], [math.imag(U), math.real(U)]])
+#            unitary = math.random_unitary(num_modes)
+            U = math.random_unitary(num_modes)
+            unitary = math.block([[math.real(U), -math.imag(U)], [math.imag(U), math.real(U)]])
         super().__init__(
             unitary=unitary,
             unitary_trainable=unitary_trainable,
@@ -464,7 +464,8 @@ class Interferometer(Parametrized, Transformation):
 
     @property
     def X_matrix(self):
-        return math.block([[math.real(self.unitary.value), -math.imag(self.unitary.value)], [math.imag(self.unitary.value), math.real(self.unitary.value)]])
+        return self.unitary.value
+#        return math.block([[math.real(self.unitary.value), -math.imag(self.unitary.value)], [math.imag(self.unitary.value), math.real(self.unitary.value)]])
 
     def _validate_modes(self, modes):
         if len(modes) != self.unitary.value.shape[-1] // 2:
@@ -498,7 +499,8 @@ class RealInterferometer(Parametrized, Transformation):
             unitary = math.random_orthogonal(num_modes)
 #            unitary = math.block([[O, -math.zeros_like(O)], [math.zeros_like(O), O]])
         super().__init__(unitary=unitary, unitary_trainable=unitary_trainable)
-        self._modes = modes or list(range(num_modes))
+#        self._modes = modes or list(range(num_modes))
+        self._modes = list(range(num_modes))
         self._is_gaussian = True
 
     @property
