861977f9db1f42a8822737ee8a1eb841f2992431
ziofil <miatto@gmail.com>
2023-02-21 00:37:10 -0800

    adds import

--- mrmustard/lab/abstract/transformation.py
+++ mrmustard/lab/abstract/transformation.py
@@ -32,7 +32,7 @@ from mrmustard import settings
 from mrmustard.math import Math
 from mrmustard.physics import fock, gaussian
 from mrmustard.training.parameter import Parameter
-from mrmustard.typing import RealMatrix
+from mrmustard.typing import RealMatrix, RealVector
 
 from .state import State
 
