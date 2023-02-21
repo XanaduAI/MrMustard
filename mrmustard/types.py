35c577948e6297eab14fb694cf64a02fde1b017a
ziofil <miatto@gmail.com>
2023-02-20 22:53:31 -0800

    fixed type system

--- mrmustard/types.py
+++ mrmustard/types.py
@@ -13,24 +13,45 @@
 # limitations under the License.
 
 """A module containing all base type annotations."""
+from typing import Tuple, TypeVar, Protocol, Iterator
 
-# pylint: disable=unused-wildcard-import,wildcard-import
+import numpy as np
 
-from numbers import Number
-from typing import TypeVar
+R = TypeVar("R", np.float16, np.float32, np.float64)
+C = TypeVar("C", np.complex64, np.complex128)
+Z = TypeVar("Z", np.int16, np.int32, np.int64)
+N = TypeVar("N", np.uint16, np.uint32, np.uint64)
 
-from numpy.typing import ArrayLike
+RealVector = np.ndarray[Tuple[int], R]
+ComplexVector = np.ndarray[Tuple[int], C]
+IntVector = np.ndarray[Tuple[int], Z]
+UIntVector = np.ndarray[Tuple[int], N]
 
-# NOTE: when type-annotating with typevars, objects with the same typevars must have the same type
-# E.g. in `def f(x: Vector, y: Vector) -> Tensor: ...`
-# the type of `x` and the type of `y` are assumed to be the same, even though "Vector" can mean different things.
+RealMatrix = np.ndarray[Tuple[int, int], R]
+ComplexMatrix = np.ndarray[Tuple[int, int], C]
+IntMatrix = np.ndarray[Tuple[int, int], Z]
+UIntMatrix = np.ndarray[Tuple[int, int], N]
 
-Scalar = Number  # 0D Tensor
-Vector = ArrayLike  # 1D Tensor
-Matrix = ArrayLike  # 2D Tensor
-Tensor = ArrayLike  # ND Tensor including 0,1,2D Tensors
+RealTensor = np.ndarray[Tuple[int, ...], R]
+ComplexTensor = np.ndarray[Tuple[int, ...], C]
+IntTensor = np.ndarray[Tuple[int, ...], Z]
+UIntTensor = np.ndarray[Tuple[int, ...], N]
 
-Int1D = ArrayLike[int]
-Int2D = ArrayLike[int]
+T = TypeVar("T", covariant=True)
 
-Trainable = TypeVar("Trainable")
+
+class Batch(Protocol[T]):
+    def __iter__(self) -> Iterator[T]:
+        ...
+
+
+# minitest
+x: ComplexVector = np.array([1.0 + 1.0j, 2.0 + 2.0j, 3.0 + 3.0j])
+y: RealMatrix = np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+z: IntTensor = np.array([[[1, 2, -3], [4, 5, -6]], [[7, 8, 9], [10, 11, 12]]])
+w: UIntVector = np.array([1, 2, 3])
+
+
+# minitest
+X: Batch[RealVector] = np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+Y: Batch[IntVector] = [np.array([1, 2, 3]), np.array([2, -3, -3])]
