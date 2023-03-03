624d68041e5ac5f158da1fa1efcf111b6015057d
Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>
2023-03-03 17:05:22 +0000

    impl [devcontainer] - add pylint

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -4,4 +4,5 @@ pip install --no-cache-dir -r requirements.txt
 pip install --no-cache-dir -r requirements-dev.txt
 pip install --no-cache-dir -r doc/requirements.txt
 pip install ray
-pip install --no-cache-dir -e .
+pip install pylint
+pip install -e .
