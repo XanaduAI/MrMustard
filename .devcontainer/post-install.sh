6a204df235bab800f44a83e560f1ad0498537277
Sebastián Duque Mesa <675763+sduquemesa@users.noreply.github.com>
2023-03-03 16:33:39 +0000

    fix [post-install] - typo

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,7 +1,7 @@
 #! /bin/zsh
 
 pip install --no-cache-dir -r requirements.txt
-pip install --no-cache-dir-r requirements-dev.txt
+pip install --no-cache-dir -r requirements-dev.txt
 pip install --no-cache-dir -r doc/requirements.txt
 pip install ray
 pip install --no-cache-dir -e .
