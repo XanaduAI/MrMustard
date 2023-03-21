fix sign

mrmustard/physics/fock.py
@@ -670,7 +670,7 @@ def oscillator_eigenstate(q: Vector, cutoff: int) -> Tensor:
     prefactor = (omega_over_hbar / np.pi) ** (1 / 4) * math.sqrt(2 ** (-math.arange(0, cutoff)))
 
     # Renormalized physicist hermite polys: Hn / sqrt(n!)
-    R = np.array([[2 + 0j]])  # to get the physicist polys
+    R = -np.array([[2 + 0j]])  # to get the physicist polys
 
     def f_hermite_polys(xi):
         poly = math.hermite_renormalized(