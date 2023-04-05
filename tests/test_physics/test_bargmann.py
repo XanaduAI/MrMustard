fa47e43e9c038fd5312e136f029ec9063daa06a1
ziofil <miatto@gmail.com>
2023-04-05 06:42:44 +0000

    docstring

--- tests/test_physics/test_bargmann.py
+++ tests/test_physics/test_bargmann.py
@@ -5,16 +5,16 @@ from mrmustard.physics.bargmann import wigner_to_bargmann_psi, wigner_to_bargman
 
 
 def test_wigner_to_bargmann_psi():
+    """Test that the Bargmann representation of a ket is correct"""
     G = Gaussian(2) >> Dgate(0.1, 0.2)
 
-    # Test that the Bargmann representation of a state is correct
     for x, y in zip(G.bargmann(), wigner_to_bargmann_psi(G.cov, G.means)):
         assert np.allclose(x, y)
 
 
 def test_wigner_to_bargmann_rho():
+    """Test that the Bargmann representation of a dm is correct"""
     G = Gaussian(2) >> Dgate(0.1, 0.2) >> Attenuator(0.9)
 
-    # Test that the Bargmann representation of a state is correct
     for x, y in zip(G.bargmann(), wigner_to_bargmann_rho(G.cov, G.means)):
         assert np.allclose(x, y)
