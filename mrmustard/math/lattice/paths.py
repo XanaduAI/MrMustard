abb958814f553b9b99e3c7707ff76f2070c56852
ziofil <miatto@gmail.com>
2023-04-05 01:10:14 +0000

    module docstring

--- mrmustard/math/lattice/paths.py
+++ mrmustard/math/lattice/paths.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Path functions"""
+
 from numba import njit, typed, typeof, types
 from numba.cpython.unsafe.tuple import tuple_setitem
 
