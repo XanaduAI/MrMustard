edb12d58ef997c7586a5eb01f3a3d4f98aed9da4
ziofil <miatto@gmail.com>
2023-04-05 06:12:14 +0000

    add test

---
+++ tests/test_math/test_lattice.py
@@ -0,0 +1,28 @@
+# Copyright 2023 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for the lattice module"""
+
+import numpy as np
+
+from mrmustard.lab import Gaussian
+
+
+def test_vanilla_vs_binomial():
+    G = Gaussian(2)
+
+    ket_vanilla = G.ket(cutoffs=[10, 10])[:5, :5]
+    ket_binomial = G.ket(max_photons=10)[:5, :5]
+
+    assert np.allclose(ket_vanilla, ket_binomial)
