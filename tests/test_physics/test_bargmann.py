fd66ca698092a40475477cb9d908b2d51a272f94
ziofil <miatto@gmail.com>
2023-05-05 18:24:12 +0000

    fixed tests

--- tests/test_physics/test_bargmann.py
+++ tests/test_physics/test_bargmann.py
@@ -28,7 +28,7 @@ def test_wigner_to_bargmann_rho():
 def test_wigner_to_bargmann_U():
     """Test that the Bargmann representation of a unitary is correct"""
     G = Ggate(2) >> Dgate(0.1, 0.2)
-    X, _, d = G.XYd(allow_none=True)
+    X, _, d = G.XYd(allow_none=False)
     for x, y in zip(G.bargmann(), wigner_to_bargmann_U(X, d)):
         assert np.allclose(x, y)
 
@@ -36,6 +36,6 @@ def test_wigner_to_bargmann_U():
 def test_wigner_to_bargmann_choi():
     """Test that the Bargmann representation of a Choi matrix is correct"""
     G = Ggate(2) >> Dgate(0.1, 0.2) >> Attenuator([0.9, 1.0])
-    X, Y, d = G.XYd()
+    X, Y, d = G.XYd(allow_none=False)
     for x, y in zip(G.bargmann(), wigner_to_bargmann_Choi(X, Y, d)):
         assert np.allclose(x, y)
