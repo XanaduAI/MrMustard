15a6543c25c9bc9b273ebd801251989f5c9bf2e8
ziofil <miatto@gmail.com>
2023-05-29 22:17:26 +0000

    adds test

--- tests/test_physics/test_gaussian/test_symplectics.py
+++ tests/test_physics/test_gaussian/test_symplectics.py
@@ -21,6 +21,7 @@ from mrmustard.lab.gates import (
     Amplifier,
     Attenuator,
     BSgate,
+    Coherent,
     CXgate,
     CZgate,
     Dgate,
@@ -205,3 +206,9 @@ def test_amplifier_attenuator_on_coherent_coherent(eta, x, y):
     assert Vacuum(1) >> Dgate(x, y) >> Amplifier(1 / eta) >> Attenuator(eta) == Thermal(
         ((1 / eta) - 1) * eta
     ) >> Dgate(x, y)
+
+
+@given(x=st.floats(-2, 2), y=st.floats(-2, 2))
+def test_number_means(x, y):
+    """Tests that the number means of a displaced state are correct"""
+    assert np.allclose(Coherent(x, y).number_means, x * x + y * y)
