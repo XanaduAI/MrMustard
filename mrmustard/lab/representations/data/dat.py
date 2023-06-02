e8baf90d6ea1c6ad02a836c17391941f2520e971
Richard Wolf <47638463+ryk-wolf@users.noreply.github.com>
2023-06-02 13:37:58 +0000

    PEP8-compliance file renaming

--- mrmustard/lab/representations/data/dat.py
+++ mrmustard/lab/representations/data/dat.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from abc import ABC 
+from abc import ABC
 
-class Data(ABC):
+class Representation(ABC):
 
     def __init__(self) -> None:
         super().__init__()
