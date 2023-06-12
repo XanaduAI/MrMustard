331c7992ad670b7be5d0ac2806bb9a54ab7a923c
Richard Wolf <47638463+ryk-wolf@users.noreply.github.com>
2023-06-12 20:16:00 +0000

    Change Converter to support photons and probability for FockKet

--- mrmustard/lab/representations/converter.py
+++ mrmustard/lab/representations/converter.py
@@ -155,17 +155,16 @@ class Converter():
         Returns:
             The target representation
         """
-        max_prob = kwargs.get('max_prob', 0.0)
-        max_photon = kwargs.get('max-photon', 0)
-        if False: #todo implement
-            pass
-
-
         try:
             s_name = source.__class__.__name__
             d_name = self._find_target_node_name(source=source, destination=destination)
             f = self.g[s_name][d_name]
-            return f(source)
+            if d_name == "FockKet":
+                max_prob = kwargs.get('max_prob', 0.0)
+                max_photon = kwargs.get('max-photon', 0)
+                return f(source, max_prob=max_prob, max_photon=max_photon)
+            else:
+                return f(source)
         
         except KeyError as e:
             raise ValueError(f"{destination} is not a valid target name") from e
