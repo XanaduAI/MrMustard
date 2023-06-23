ced8f424abfa0cda4c8bb15eb32badb2addf686c
Richard Wolf <47638463+ryk-wolf@users.noreply.github.com>
2023-06-23 16:55:30 +0000

    Reorder tests in Data

--- tests/test_lab/test_representations/test_data/test_data.py
+++ tests/test_lab/test_representations/test_data/test_data.py
@@ -15,6 +15,7 @@
 import numpy as np
 import operator as op
 import pytest
+from copy import deepcopy
 from hypothesis import assume, given
 from hypothesis import strategies as st
 from hypothesis.extra.numpy import arrays
@@ -28,6 +29,23 @@ DATA = MockData()
 class TestData():
 
     #########   Common to different methods  #########
+    @pytest.mark.parametrize("operator", [op.neg])
+    def test_data_object_is_left_untouched_after_applying_negation(self, operator):
+        pre_op_data = deepcopy(DATA)
+        _ = operator(pre_op_data)
+        assert DATA == pre_op_data
+
+
+    @given(other = st.from_type(MockData))
+    @pytest.mark.parametrize("operator", [op.add, op.sub, op.mul, op.truediv, op.eq, op.and_])
+    def test_data_object_is_left_untouched_after_applying_operation_of_arity_two(self, 
+                                                                                 other, 
+                                                                                 operator):
+        pre_op_data = deepcopy(DATA)
+        _ = operator(pre_op_data, other)
+        assert DATA == DATA
+
+
     @given(other = everything_except( (int, float, complex) ))
     def test_truediv_raises_TypeError_if_divisor_is_not_scalar(self, other):
         with pytest.raises(TypeError):
@@ -42,26 +60,9 @@ class TestData():
         with pytest.raises(TypeError):
             operator(DATA, other)
 
-
-    @given(other = st.from_type(MockData))
-    @pytest.mark.parametrize("operator", [op.add, op.sub, op.mul, op.truediv, op.eq, op.and_])
-    def test_data_object_is_left_untouched_after_applying_operation_of_arity_two(self, 
-                                                                                 other, 
-                                                                                 operator):
-        
-        _ = operator(DATA, other)
-        assert DATA == DATA
-
-    
-    @pytest.mark.parametrize("operator", [op.neg])
-    def test_data_object_is_left_untouched_after_applying_negation(self, operator):
-        _ = operator(DATA)
-        assert DATA == DATA
-
-
     @given(other = st.from_type(MockData))
     @pytest.mark.parametrize("operator", [op.add, op.sub, op.mul, op.truediv])
-    def test_new_object_created_by_method_has_same_attribute_shapes_as_old_object(self,
+    def test_new_object_created_by_arity2_operation_has_same_attribute_shapes_as_old_object(self,
                                                                                   other,
                                                                                   operator):
         # NOTE: are we ok with try/except blocks in tests?
@@ -71,14 +72,27 @@ class TestData():
             try: # works for all numpy array attributes
                 assert getattr(DATA, k).shape == getattr(new_data, k).shape
             except AttributeError: # works for scalar attributes
-                assert getattr(DATA, k) == getattr(new_data, k)           
+                assert getattr(DATA, k) == getattr(new_data, k)
+
+
+    @given(other = st.from_type(MockData))
+    @pytest.mark.parametrize("operator", [op.add, op.sub, op.mul, op.truediv])
+    def test_new_object_created_by_negation_has_same_attribute_shapes_as_old_object(self,
+                                                                                    operator):
+        # NOTE: are we ok with try/except blocks in tests?
+        # NOTE: are we ok with for loops in tests?
+        for k in DATA.__dict__.keys():
+            new_data = operator(DATA)
+            try: # works for all numpy array attributes
+                assert getattr(DATA, k).shape == getattr(new_data, k).shape
+            except AttributeError: # works for scalar attributes
+                assert getattr(DATA, k) == getattr(new_data, k)               
 
     
-
-
     ####################  Init  ######################
 
     def test_when_arguments_given_attribute_values_match_them(self):
+        #new_data = 
         pass
 
 
