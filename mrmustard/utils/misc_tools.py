420f4bcd14ef26e8017e53c543b6650a8e21b4ae
Ryk <a.richard.wolf@gmail.com>
2023-06-30 14:29:46 +0000

    Add factory method to utils and document the  module

--- mrmustard/utils/misc_tools.py
+++ mrmustard/utils/misc_tools.py
@@ -17,23 +17,23 @@ They can be used as is in any of the classes.
 """
 
 def duck_type_checker(obj_a:object, obj_b:object) -> bool:
-    r""" If it walks like a duck and it quacks like a duck, then it must be a duck.
+    r""" *If it walks like a duck and it quacks like a duck, then it must be a duck.*
 
     This function performs type-checking from a duck-type perspective, testing whether both given
-    objects share the same set of attributes. If they do, no matter their 'type' -as obtained by
-    a call to `isinstance`-, they will be considered to be of the same duck-type.
+    objects share the same set of attributes. If they do, no matter their *type* -as obtained by
+    a call to ``isinstance``-, they will be considered to be of the same duck-type.
 
     Note that we here look at the symmetric difference, hence the exact question we ask is better
-    phrased as 'Are both objects of the same type?' than as 'Is object a of the same type as object
-    b?'.
+    phrased as "*Are both objects of the same type?*" than as "*Is object a of the same type as 
+    object b?*".
 
-    Note also that this checks for the exact match between types -via attributes-, this system will
-    likely not work with subclasses since there is a high probability that they might have 
-    different attributes
+    Note also that this checks for the exact match between types -via attributes-, this system is 
+    **not** meant to work with subclasses since there is a high probability that they might have 
+    different attributes.
 
     Args:
-        obj_a: one of the objects to be checked
-        obj_b: the other object to be checked
+        obj_a:  one of the objects to be checked
+        obj_b:  the other object to be checked
 
     Returns:
         True if both objects have exactly the same attributes, False otherwise.
@@ -41,3 +41,17 @@ def duck_type_checker(obj_a:object, obj_b:object) -> bool:
     set_a = set(obj_a.__dict__.keys())
     set_b = set(obj_b.__dict__.keys())
     return set_a.symmetric_difference(set_b) == set()
+
+
+
+def general_factory(cls, *args, **kwargs) -> object:
+    r""" Factory method which generates an instance of cls parameterized by the given arguments.
+
+    Args:
+        args:   non-keyword ordered arguments used to parameterize the class instance
+        kwargs: keyword arguments used to parameterize the class instance
+        
+    Returns:
+        An instance of the class cls parametrized by args and kwargs
+    """
+    return cls(*args, **kwargs)
