We now check whether lower neighbour is at the border of G. Also added small fix for jacobian path.

mrmustard/math/lattice/strategies/vanilla.py
@@ -68,7 +68,7 @@ def vanilla_jacobian(
     dGdc = G / c
 
     # initialize path iterator
-    path = paths.ndindex_path(G.shape)
+    path = np.ndindex(G.shape)
 
     # skip first index
     next(path)