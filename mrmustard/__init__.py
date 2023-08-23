c74f8d526184543e14715d5a111e74b9d565f2b4
ziofil <miatto@gmail.com>
2023-08-23 23:20:40 +0000

    add TN_BOND_DIM to settings

--- mrmustard/__init__.py
+++ mrmustard/__init__.py
@@ -51,6 +51,7 @@ class Settings:
         self._seed = np.random.randint(0, 2**31 - 1)
         self.rng = np.random.default_rng(self._seed)
         self.DEFAULT_BS_METHOD = "vanilla"  # can be 'vanilla' or 'schwinger'
+        self.TN_BOND_DIM = 50  # for wires of tensor network that don't have a dimension
 
     @property
     def SEED(self):
