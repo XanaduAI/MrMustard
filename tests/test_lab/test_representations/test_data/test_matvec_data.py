f5551ad9c6f3db9efbba991a290a0c87f015e7c2
ziofil <miatto@gmail.com>
2023-08-23 08:32:30 +0000

    simplify test fixture, reformat

--- tests/test_lab/test_representations/test_data/test_matvec_data.py
+++ tests/test_lab/test_representations/test_data/test_matvec_data.py
@@ -35,10 +35,11 @@ from tests.test_lab.test_representations.test_data.tools_for_tests import (
 )
 
 from mrmustard import settings
+
 settings.SEED = 42
 
-D = 10 #dimension, matrix will be DxD while means will be D
-N = 3 #number of elements in the batch
+D = 10  # dimension, matrix will be DxD while means will be D
+N = 3  # number of elements in the batch
 
 
 ##################   FIXTURES  ###################
@@ -47,37 +48,42 @@ def D():
     """The dimension: matrices will be DxD and vectors will be D."""
     return 5
 
+
 @pytest.fixture
 def N():
     """The number of elements in the batch."""
     return 3
+
 
 @pytest.fixture
 def TYPE():
     r"""Type of the object under test."""
     return MatVecData
 
+
 @pytest.fixture
-def MAT(D,N) -> Batch[Matrix]:
+def MAT(D, N) -> Batch[Matrix]:
     r"""Some batch of matrices for the object's parameterization."""
     mats = []
     for _ in range(N):
-        m = settings.rng.normal(size=(D,D)) + 1j*settings.rng.normal(size=(D,D))
+        m = settings.rng.normal(size=(D, D)) + 1j * settings.rng.normal(size=(D, D))
         m = m + m.T  # symmetrize A
         mats.append(m)
     return np.array(mats)
 
 
 @pytest.fixture
-def VEC(D,N) -> Batch[Vector]:
+def VEC(D, N) -> Batch[Vector]:
     r"""Some batch of vectors for the object's parameterization."""
-    return np.array([settings.rng.normal(size=D) + 1j*settings.rng.normal(size=D) for _ in range(N)])
+    return np.array(
+        [settings.rng.normal(size=D) + 1j * settings.rng.normal(size=D) for _ in range(N)]
+    )
 
 
 @pytest.fixture
 def COEFFS(N) -> Batch[Scalar]:
     r"""Some batch of scalars for the object's parameterization."""
-    return np.array([settings.rng.normal() + 1j*settings.rng.normal() for _ in range(N)])
+    return np.array([settings.rng.normal() + 1j * settings.rng.normal() for _ in range(N)])
 
 
 @pytest.fixture
@@ -99,7 +105,7 @@ def OTHER(DATA) -> MatVecData:
     return deepcopy(DATA)
 
 
-class TestMatVecData(TestData): #TestData
+class TestMatVecData(TestData):  # TestData
     ####################  Init  ######################
     def if_coeffs_not_given_they_are_equal_to_1(self, TYPE, PARAMS):
         params_without_coeffs = deepcopy(PARAMS)
@@ -113,7 +119,7 @@ class TestMatVecData(TestData): #TestData
         pre_op_data = deepcopy(DATA)
         neg_data = -DATA
         manual_neg_coeffs = [-c for c in pre_op_data.coeffs]
-        assert manual_neg_coeffs == neg_data.coeffs
+        assert np.allclose(manual_neg_coeffs, neg_data.coeffs)
         assert np.allclose(neg_data.mat, pre_op_data.mat)
         assert np.allclose(neg_data.vec, pre_op_data.vec)
 
@@ -125,46 +131,47 @@ class TestMatVecData(TestData): #TestData
     # testing that the 1st elt of 2nd object is at index len of 1st elt + 1
     # just this basic stuff
     @pytest.mark.parametrize("operator", [op.add, op.sub])
-    def test_length_of_added_subtracted_objects_is_same_as_previous_if_objects_are_same(self, DATA, operator):
+    def test_length_of_added_subtracted_objects_is_same_as_previous_if_objects_are_same(
+        self, DATA, operator
+    ):
         l0 = DATA.coeffs.shape[0]
         new_data = operator(DATA, DATA)
         assert l0 == new_data.coeffs.shape[0]
 
     @pytest.mark.parametrize("operator", [op.add, op.sub])
-    def test_length_of_added_subtracted_objects_is_sum_of_lengths_if_objects_are_diff(self, DATA, TYPE, operator):
+    def test_length_of_added_subtracted_objects_is_sum_of_lengths_if_objects_are_diff(
+        self, DATA, TYPE, operator
+    ):
         mat = DATA.mat
-        vec = DATA.vec +1
+        vec = DATA.vec + 1
         coeffs = DATA.coeffs + 2
         parameters = (mat, vec, coeffs)
         other_data = general_factory(TYPE, *parameters)
         l0 = DATA.coeffs.shape[0]
         l1 = other_data.coeffs.shape[0]
         new_data = operator(DATA, other_data)
-        total_length = l0+l1
+        total_length = l0 + l1
         assert total_length == new_data.coeffs.shape[0]
-
 
     #######  Scalar division / multiplication ########
     @pytest.mark.parametrize("operator", [op.truediv, op.mul])
     @pytest.mark.parametrize("x", [0.001, 7, 100])
-    def test_scalar_mul_or_div_if_mat_vec_same_dont_change_mat_vec_but_change_coeffs(self, 
-                                                                                     DATA, 
-                                                                                     operator, 
-                                                                                     x):
+    def test_scalar_mul_or_div_if_mat_vec_same_dont_change_mat_vec_but_change_coeffs(
+        self, DATA, operator, x
+    ):
         precision = 3
         preop_data = deepcopy(DATA)
         postop_data = operator(DATA, x)
-        f = lambda x : np.linalg.norm(x)
-        norms_of_mats_preop = set( np.around( [f(m) for m in preop_data.mat] , precision) )
-        norms_of_mats_postop = set( np.around( [f(m) for m in postop_data.mat] , precision) )
-        norms_of_vecs_preop = set( np.around( [f(v) for v in preop_data.mat] , precision) )
-        norms_of_vecs_postop = set( np.around( [f(v) for v in postop_data.mat] , precision) )
-        coeffs_preop = set( np.around(preop_data.coeffs, precision) )
-        coeffs_postop = set( np.around(postop_data.coeffs, precision) )
+        f = lambda x: np.linalg.norm(x)
+        norms_of_mats_preop = set(np.around([f(m) for m in preop_data.mat], precision))
+        norms_of_mats_postop = set(np.around([f(m) for m in postop_data.mat], precision))
+        norms_of_vecs_preop = set(np.around([f(v) for v in preop_data.mat], precision))
+        norms_of_vecs_postop = set(np.around([f(v) for v in postop_data.mat], precision))
+        coeffs_preop = set(np.around(preop_data.coeffs, precision))
+        coeffs_postop = set(np.around(postop_data.coeffs, precision))
         assert norms_of_mats_preop.symmetric_difference(norms_of_mats_postop) == set()
         assert norms_of_vecs_preop.symmetric_difference(norms_of_vecs_postop) == set()
         assert coeffs_preop.symmetric_difference(coeffs_postop) != set()
-        
 
     # ###############  Multiplication  ##################
     # NOTE: tested in children
