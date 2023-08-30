096d90fe7e961124383ba976750bf1967a8c34ea
Yuan Yao <yuan.yao@xanadu-Yuan-YAO.local>
2023-08-30 10:59:40 -0400

    add more tests for fock classes

--- tests/test_lab/test_representations/test_fockdm.py
+++ tests/test_lab/test_representations/test_fockdm.py
@@ -23,35 +23,70 @@ from mrmustard.lab import Coherent
 from mrmustard.lab.representations.fock_dm import FockDM
 
 
-def test_purity_of_fock_dm_state():
-    array = np.array([[0.5, 0.2], [-0.1, 1.5]])
-    fockdm = FockDM(array=array)
-    assert np.allclose(fockdm.purity, 0.615)
+class TestFockDMInit:
+    """Test class for the initialization of the FockDM class."""
+
+    def test_init_fock_dm_with_all_arguments(self):
+        "Test that the initialization of the fock dm class with all arguments."
+        FockDM(array=np.random.uniform((3, 3)))
+
+    def test_init_fock_dm_with_non_physical_state(self):
+        "Test that the initialization of the fock dm class with non physical state."
+        with self.assertRaises(ValueError):
+            FockDM(array=np.array([[100, 100, 100], [100, 100, 100], [100, 100, 100]]))
 
 
-def test_norm_of_fock_dm_state():
-    array = np.array([[0.5, 0.2], [-0.1, 1.5]])
-    fockdm = FockDM(array=array)
-    assert np.allclose(fockdm.norm, 2.0)
+class TestFockDMProperties:
+    """Test class for each properties returns the correct values."""
+
+    def test_purity_of_fock_dm_state(self):
+        """Test that the purity of a fock density matrix is correct."""
+        array = np.array([[0.5, 0.2], [-0.1, 1.5]])
+        fockdm = FockDM(array=array)
+        assert np.allclose(fockdm.purity, 0.615)
+
+    def test_norm_of_fock_dm_state(self):
+        """Test that the norm of the fock dm class is correct."""
+        array = np.array([[0.5, 0.2], [-0.1, 1.5]])
+        fockdm = FockDM(array=array)
+        assert np.allclose(fockdm.norm, 2.0)
+
+    @given(x=st.floats(-1, 1), y=st.floats(-1, 1))
+    def test_number_means_function_of_fock_dm_state_from_coherent_state(self, x, y):
+        """Test that the number means is correct for a coherent state."""
+        dm = Coherent(x, y).dm([80]).dm([80])
+        fockdm = FockDM(array=dm)
+        expected = x**2 + y**2
+        assert np.allclose(fockdm.number_means(), expected)
+
+    @given(x=st.floats(-1, 1), y=st.floats(-1, 1))
+    def test_number_variance_function_of_fock_dm_state_from_coherent_state(self, x, y):
+        """Test that the number variance is correct for a coherent state."""
+        dm = Coherent(x, y).dm([80])
+        fockdm = FockDM(array=dm)
+        expected = x**2 + y**2
+        assert np.allclose(fockdm.number_variances()[0], expected)
+
+    def test_number_variances_function_of_fock_dm_state_from_fock_state(self):
+        """Test that the number variance is correct for a fock state."""
+        dm = np.array([0, 1, 0, 0, 0])
+        fockdm = FockDM(array=dm)
+        expected = 0
+        assert np.allclose(fockdm.number_variances(), expected)
+
+    @given(array=xy_arrays())
+    def test_probabilities_of_fock_dm_state(self, array):
+        """Test that the probability of the fock dm class is the diagonal elements of the fock array."""
+        fockdm = FockDM(array=array)
+        assert np.allclose(fockdm.probability, np.diag(array))
 
 
-@given(x=st.floats(-1, 1), y=st.floats(-1, 1))
-def test_number_means_function_of_fock_dm_state_from_coherent_state(x, y):
-    dm = Coherent(x, y).ket([80]).dm([80])
-    fockdm = FockDM(array=dm)
-    expected = x**2 + y**2
-    assert np.allclose(fockdm.number_means, expected)
+class TestFockDMThrowErrors:
+    """Test class for all non-implement properties or methods of the class."""
 
+    fockdm = FockDM(array=np.random.uniform(3))
 
-@given(x=st.floats(-1, 1), y=st.floats(-1, 1))
-def test_number_means_function_of_fock_dm_state_from_coherent_state(x, y):
-    dm = Coherent(x, y).ket([80]).dm([80])
-    fockdm = FockDM(array=dm)
-    expected = x**2 + y**2
-    assert np.allclose(fockdm.number_variances[0], expected)
-
-
-@given(array=xy_arrays())
-def test_probabilities_of_fock_dm_state(array):
-    fockdm = FockDM(array=array)
-    assert np.allclose(fockdm.probability, np.diag(array))
+    def test_number_cov_with_error(self):
+        """Test that number covariance method is not implement for FockDM and returns an error."""
+        with self.assertRaises(NotImplementedError):
+            self.fockdm.number_cov()
--- tests/test_lab/test_representations/test_fockket.py
+++ tests/test_lab/test_representations/test_fockket.py
@@ -23,42 +23,70 @@ from mrmustard.lab import Coherent
 from mrmustard.lab.representations.fock_ket import FockKet
 
 
-@given(array=xy_arrays())
-def test_purity_of_fock_ket_state(array):
-    fockket = FockKet(array=array)
-    assert np.allclose(fockket.purity, 1.0)
+class TestFockKetInit:
+    """Test class for the initialization of the FockKet class."""
+
+    def test_init_fock_ket_with_all_arguments(self):
+        "Test that the initialization of the fock ket class with all arguments."
+        FockKet(array=np.random.uniform(3))
+
+    def test_init_fock_ket_with_non_physical_state(self):
+        "Test that the initialization of the fock ket class with non physical state."
+        with self.assertRaises(ValueError):
+            FockKet(array=np.array([100, 100, 100]))
 
 
-@given(array=xy_arrays())
-def test_norm_of_fock_ket_state(array):
-    fockket = FockKet(array=array)
-    assert np.allclose(fockket.norm, np.abs(np.norm(array)))
+class TestFockKetProperties:
+    """Test class for each properties returns the correct values."""
+
+    @given(array=xy_arrays())
+    def test_purity_of_fock_ket_state(self, array):
+        """Test that the purity of any fock ket class state is 1.0."""
+        fockket = FockKet(array=array)
+        assert np.allclose(fockket.purity, 1.0)
+
+    @given(array=xy_arrays())
+    def test_norm_of_fock_ket_state(self, array):
+        """Test that the norm of the fock ket class is correct."""
+        fockket = FockKet(array=array)
+        assert np.allclose(fockket.norm, np.abs(np.norm(array)))
+
+    @given(x=st.floats(-1, 1), y=st.floats(-1, 1))
+    def test_number_means_function_of_fock_ket_state_from_coherent_state(self, x, y):
+        """Test that the number means is correct for a coherent state."""
+        ket = Coherent(x, y).ket([80])
+        fockket = FockKet(array=ket)
+        expected = x**2 + y**2
+        assert np.allclose(fockket.number_means(), expected)
+
+    @given(x=st.floats(-1, 1), y=st.floats(-1, 1))
+    def test_number_variance_function_of_fock_ket_state_from_coherent_state(self, x, y):
+        """Test that the number variance is correct for a coherent state."""
+        ket = Coherent(x, y).ket([80])
+        fockket = FockKet(array=ket)
+        expected = x**2 + y**2
+        assert np.allclose(fockket.number_variances()[0], expected)
+
+    def test_number_variances_function_of_fock_ket_state_from_fock_state(self):
+        """Test that the number variance is correct for a fock state."""
+        ket = np.array([0, 1, 0, 0, 0])
+        fockket = FockKet(array=ket)
+        expected = 0
+        assert np.allclose(fockket.number_variances(), expected)
+
+    @given(array=xy_arrays())
+    def test_probabilities_of_fock_ket_state(self, array):
+        """Test that the probability of the wavefunction ket class is the absolute value of the fock array."""
+        fockket = FockKet(array=array)
+        assert np.allclose(fockket.probability, np.abs(array))
 
 
-@given(x=st.floats(-1, 1), y=st.floats(-1, 1))
-def test_number_means_function_of_fock_ket_state_from_coherent_state(x, y):
-    ket = ket = Coherent(x, y).ket([80])
-    fockket = FockKet(array=ket)
-    expected = x**2 + y**2
-    assert np.allclose(fockket.number_means, expected)
+class TestFockKetThrowErrors:
+    """Test class for all non-implement properties or methods of the class."""
 
+    fockket = FockKet(array=np.random.uniform(3))
 
-@given(x=st.floats(-1, 1), y=st.floats(-1, 1))
-def test_number_variance_function_of_fock_ket_state_from_coherent_state(x, y):
-    ket = Coherent(x, y).ket([80])
-    fockket = FockKet(array=ket)
-    expected = x**2 + y**2
-    assert np.allclose(fockket.number_variances[0], expected)
-
-
-def test_number_variances_function_of_fock_ket_state_from_fock_state():
-    ket = np.array([0, 1, 0, 0, 0])
-    fockket = FockKet(array=ket)
-    expected = 0
-    assert np.allclose(fockket.number_variances, expected)
-
-
-@given(array=xy_arrays())
-def test_probabilities_of_fock_ket_state(array):
-    fockket = FockKet(array=array)
-    assert np.allclose(fockket.probability, np.abs(array))
+    def test_number_cov_with_error(self):
+        """Test that number covariance method is not implement for FockKet and returns an error."""
+        with self.assertRaises(NotImplementedError):
+            self.fockket.number_cov()
