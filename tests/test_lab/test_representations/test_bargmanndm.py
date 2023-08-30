59356f2f6687d904271186e011a388d32bc149c9
Yuan Yao <yuan.yao@xanadu-Yuan-YAO.local>
2023-08-30 10:26:54 -0400

    add more tests for bargmann classes

--- tests/test_lab/test_representations/test_bargmanndm.py
+++ tests/test_lab/test_representations/test_bargmanndm.py
@@ -18,9 +18,57 @@ import pytest
 from mrmustard.lab.representations.bargmann_dm import BargmannDM
 
 
-class TestBargmannKetThrowErrors:
+class TestBargmannDMInit:
+    """Test class for the initialization of the BargmannDM class."""
+
+    def test_init_bargmann_dm_with_2dA(self):
+        "Test that the initialization of the BargmannDM class works with 2d A matrix."
+        bargmanndm = BargmannDM(A=np.random.random((3, 3)), b=np.random.random(3), c=1.0)
+        assert bargmanndm.data.A.shape == 3
+        assert bargmanndm.data.A.shape[0] == 1
+        assert bargmanndm.data.b.shape == 2
+        assert bargmanndm.data.b.shape[0] == 1
+
+    def test_init_bargmann_dm_with_3dA(self):
+        "Test that the initialization of the BargmannDM class works with 3d A matrix."
+        bargmanndm = BargmannDM(A=np.random.random((2, 3, 3)), b=np.random.random(2, 3), c=1.0)
+        assert bargmanndm.data.A.shape == 3
+        assert bargmanndm.data.A.shape[0] == 2
+        assert bargmanndm.data.b.shape == 2
+        assert bargmanndm.data.b.shape[0] == 2
+
+
+class TestBargmannDMThrowErrors:
+    """Test class for all non-implement properties or methods of the class."""
+
     bargmanndm = BargmannDM(A=np.random.random((3, 3)), b=np.random.random(3), c=1.0)
 
     def test_purity_with_error(self):
+        """Test that the purity is not implement for BargmannDM and returns an error."""
         with self.assertRaises(NotImplementedError):
-            self.bargmanndm.purity
+            self.bargmanndm.purity()
+
+    def test_norm_with_error(self):
+        """Test that the norm is not implement for BargmannDM and returns an error."""
+        with self.assertRaises(NotImplementedError):
+            self.bargmanndm.norm()
+
+    def test_number_means_with_error(self):
+        """Test that number means method is not implement for BargmannDM and returns an error."""
+        with self.assertRaises(NotImplementedError):
+            self.bargmanndm.number_means()
+
+    def test_number_cov_with_error(self):
+        """Test that number covariance method is not implement for BargmannDM and returns an error."""
+        with self.assertRaises(NotImplementedError):
+            self.bargmanndm.number_cov()
+
+    def test_number_variance_with_error(self):
+        """Test that number variance method is not implement for BargmannDM and returns an error."""
+        with self.assertRaises(NotImplementedError):
+            self.bargmanndm.number_variances()
+
+    def test_probability_with_error(self):
+        """Test that the probability method is not implement for BargmannDM and returns an error."""
+        with self.assertRaises(NotImplementedError):
+            self.bargmanndm.probability()
--- tests/test_lab/test_representations/test_bargmannket.py
+++ tests/test_lab/test_representations/test_bargmannket.py
@@ -18,6 +18,61 @@ import pytest
 from mrmustard.lab.representations.bargmann_ket import BargmannKet
 
 
-def test_purity_of_bargmann_ket():
+class TestBargmannKetInit:
+    """Test class for the initialization of the BargmannKet class."""
+
+    def test_init_bargmann_ket_with_2dA(self):
+        "Test that the initialization of the WignerKet class works with 2d A matrix."
+        bargmannket = BargmannKet(A=np.random.random((3, 3)), b=np.random.random(3), c=1.0)
+        assert bargmannket.data.A.shape == 3
+        assert bargmannket.data.A.shape[0] == 1
+        assert bargmannket.data.b.shape == 2
+        assert bargmannket.data.b.shape[0] == 1
+
+    def test_init_bargmann_ket_with_3dA(self):
+        "Test that the initialization of the BargmannKet class works with 3d A matrix."
+        bargmannket = BargmannKet(A=np.random.random((2, 3, 3)), b=np.random.random(2, 3), c=1.0)
+        assert bargmannket.data.A.shape == 3
+        assert bargmannket.data.A.shape[0] == 2
+        assert bargmannket.data.b.shape == 2
+        assert bargmannket.data.b.shape[0] == 2
+
+
+class TestWignerKetProperties:
+    """Test class for each properties returns the correct values."""
+
+    def test_purity_of_bargmann_ket(self):
+        """Test that the purity of any bargmann ket class state is 1.0."""
+        bargmannket = BargmannKet(A=np.random.random((3, 3)), b=np.random.random(3), c=1.0)
+        assert bargmannket.purity, 1.0
+
+
+class TestBargmannKetThrowErrors:
+    """Test class for all non-implement properties or methods of the class."""
+
     bargmannket = BargmannKet(A=np.random.random((3, 3)), b=np.random.random(3), c=1.0)
-    assert (bargmannket.purity, 1.0)
+
+    def test_norm_with_error(self):
+        """Test that the norm is not implement for BargmannKet and returns an error."""
+        with self.assertRaises(NotImplementedError):
+            self.bargmannket.norm()
+
+    def test_number_means_with_error(self):
+        """Test that number means method is not implement for BargmannKet and returns an error."""
+        with self.assertRaises(NotImplementedError):
+            self.bargmannket.number_means()
+
+    def test_number_cov_with_error(self):
+        """Test that number covariance method is not implement for BargmannKet and returns an error."""
+        with self.assertRaises(NotImplementedError):
+            self.bargmannket.number_cov()
+
+    def test_number_variance_with_error(self):
+        """Test that number variance method is not implement for BargmannKet and returns an error."""
+        with self.assertRaises(NotImplementedError):
+            self.bargmannket.number_variances()
+
+    def test_probability_with_error(self):
+        """Test that the probability method is not implement for BargmannKet and returns an error."""
+        with self.assertRaises(NotImplementedError):
+            self.bargmannket.probability()
