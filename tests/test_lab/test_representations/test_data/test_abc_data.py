bc47d85228b14c24f13d95c850abee680e921aad
ziofil <miatto@gmail.com>
2023-08-31 17:26:57 +0000

    new tests

--- tests/test_lab/test_representations/test_data/test_abc_data.py
+++ tests/test_lab/test_representations/test_data/test_abc_data.py
@@ -20,11 +20,14 @@ Check parents test classe-s for more details on the rationale.
 The fixtures must correspond to the concrete class being tested, here ABCData.
 """
 
-import numpy as np
-import pytest
 from copy import deepcopy
 
+import numpy as np
+import pytest
+
+from mrmustard.lab import Attenuator, Dgate, Gaussian, Ggate
 from mrmustard.lab.representations.data.abc_data import ABCData
+from mrmustard.physics import bargmann, gaussian
 from mrmustard.typing import Batch, Matrix, Scalar, Vector
 from mrmustard.utils.misc_tools import general_factory
 from tests.test_lab.test_representations.test_data.test_matvec_data import TestMatVecData
@@ -55,7 +58,7 @@ def TYPE():
 def A(D, N) -> Batch[Matrix]:
     r"""Some batch of matrices for the object's parameterization."""
     A = np.random.normal(size=(N, D, D)) + 1j * np.random.normal(size=(N, D, D))
-    return A + A.transpose((0, 2, 1))  # symmetrize As
+    return A + A.transpose((0, 2, 1))  # symmetrize
 
 
 @pytest.fixture
@@ -97,10 +100,10 @@ def X(D) -> ABCData:
 
 class TestABCData(TestMatVecData):  # TODO re-add inheritance
     ####################  Init  ######################
-    def test_non_symmetric_matrix_raises_ValueError(self, B, C):
+    def test_non_symmetric_matrix_raises_AssertionError(self, B, C):
         non_symmetric_mat = np.eye(10)  # TODO factory method for this
         non_symmetric_mat[0] += np.array(range(10))
-        with pytest.raises(ValueError):
+        with pytest.raises(AssertionError):
             ABCData([non_symmetric_mat], B, C)
 
     ##################  Negative  ####################
@@ -130,3 +133,63 @@ class TestABCData(TestMatVecData):  # TODO re-add inheritance
         our_operation = (DATA * OTHER).value(X)
         manual_operation = DATA.value(X) * OTHER.value(X)
         assert np.isclose(our_operation, manual_operation)
+
+    def test_abc_contraction_ket_into_unitary_1mode(self, DATA, OTHER, X):
+        ket = Gaussian(1) >> Dgate(*np.random.normal(size=2))
+        unitary = Ggate(1) >> Dgate(*np.random.normal(size=2))
+        KET = ABCData(*ket.bargmann())
+        UNITARY = ABCData(*unitary.bargmann())
+        ours = UNITARY[1] @ KET[0]
+        other = (ket >> unitary).bargmann()
+        assert np.allclose(ours.A[0], other[0])
+        assert np.allclose(ours.b[0], other[1])
+        assert np.isclose(
+            np.abs(ours.c[0]), np.abs(other[2])
+        )  # note we can't check global phase :)
+
+    def test_abc_contraction_ket_into_unitary_2mode(self, DATA, OTHER, X):
+        ket = Gaussian(2) >> Dgate(*np.random.normal(size=4))
+        unitary = Ggate(2) >> Dgate(*np.random.normal(size=4))
+        KET = ABCData(*ket.bargmann())
+        UNITARY = ABCData(*unitary.bargmann())
+        ours = UNITARY[2, 3] @ KET[0, 1]
+        other = (ket >> unitary).bargmann()
+        assert np.allclose(ours.A[0], other[0])
+        assert np.allclose(ours.b[0], other[1])
+        assert np.isclose(
+            np.abs(ours.c[0]), np.abs(other[2])
+        )  # note we can't check global phase :)
+
+    def test_abc_contraction_dm_into_channel_1mode(self, DATA, OTHER, X):
+        # old method
+        dm = Gaussian(1) >> Dgate(*np.random.normal(size=2)) >> Attenuator(0.4)
+        channel = Ggate(1) >> Dgate(*np.random.normal(size=2)) >> Attenuator(0.5)
+        other = (dm >> channel).bargmann()
+
+        # new method
+        DM = ABCData(*dm.bargmann())
+        CHOI = ABCData(*channel.bargmann())
+        ours = DM[0, 1] @ CHOI[1, 3]  # channel is out, in, out, in
+
+        # compare
+        assert np.allclose(ours.A[0], other[0])
+        assert np.allclose(ours.b[0], other[1])
+        assert np.isclose(np.abs(ours.c[0]), np.abs(other[2]))
+
+    def test_abc_contraction_generaldyne_1mode(self, DATA, OTHER, X):
+        # old method
+        ket = Gaussian(2) >> Dgate(*np.random.normal(size=4))
+        proj = Gaussian(1) >> Dgate(*np.random.normal(size=2))
+        _, prob, cov, means = gaussian.general_dyne(ket.cov, ket.means, proj.cov, proj.means, [0])
+        A, b, c = bargmann.wigner_to_bargmann_psi(cov, means)
+        c *= np.sqrt(prob)
+
+        # new method
+        KET = ABCData(*ket.bargmann())
+        PROJ = ABCData(*proj.bargmann())
+        ours = KET[0] @ PROJ[0]
+
+        # compare
+        assert np.allclose(ours.A[0], A)
+        assert np.allclose(ours.b[0], b)
+        assert np.isclose(np.abs(ours.c[0]), np.abs(c))
