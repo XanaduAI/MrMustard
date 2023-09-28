069d360882409fc0548c6f594ab4d58d563dc9f6
ziofil <miatto@gmail.com>
2023-09-28 15:15:54 +0000

    add/improve methods to work with matrix diagonals

--- mrmustard/math/tensorflow.py
+++ mrmustard/math/tensorflow.py
@@ -133,8 +133,8 @@ class TFMath(MathInterface):
     def diag(self, array: tf.Tensor, k: int = 0) -> tf.Tensor:
         return tf.linalg.diag(array, k=k)
 
-    def diag_part(self, array: tf.Tensor) -> tf.Tensor:
-        return tf.linalg.diag_part(array)
+    def diag_part(self, array: tf.Tensor, k: int) -> tf.Tensor:
+        return tf.linalg.diag_part(array, k=k)
 
     def einsum(self, string: str, *tensors) -> tf.Tensor:
         if type(string) is str:
@@ -259,6 +259,9 @@ class TFMath(MathInterface):
 
     def reshape(self, array: tf.Tensor, shape: Sequence[int]) -> tf.Tensor:
         return tf.reshape(array, shape)
+
+    def set_diag(self, array: tf.Tensor, diag: tf.Tensor, k: int) -> tf.Tensor:
+        return tf.linalg.set_diag(array, diag, k=k)
 
     def sin(self, array: tf.Tensor) -> tf.Tensor:
         return tf.math.sin(array)
