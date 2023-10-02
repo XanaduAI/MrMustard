fe3379765436baab4910cd12f7a7ab784e4611ef
Robbe De Prins (UGent-imec) <Robbe.DePrins@UGent.be>
2023-10-02 17:27:08 +0200

    no relative import for MathInterface

--- mrmustard/math/tensorflow.py
+++ mrmustard/math/tensorflow.py
@@ -36,7 +36,8 @@ from julia import (
     Main as Main_julia,
 )  # must be imported after running "jl = Julia(compiled_modules=False)", which is done in math/__init__.py
 
-from .math_interface import MathInterface
+# from .math_interface import MathInterface
+from mrmustard.math.math_interface import MathInterface
 
 # import Julia functions
 math_directory = os.path.dirname(__file__)
