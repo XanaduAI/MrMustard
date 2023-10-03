409e2fb06de018af7e326e5c830a517df37d8175
SamFerracin <samuele.ferracin@gmail.com>
2023-10-03 17:39:54 -0400

    46 testsgaf

--- mrmustard/lab/circuit.py
+++ mrmustard/lab/circuit.py
@@ -39,7 +39,7 @@ class Circuit(Transformation, Parametrized):
 
     def __init__(self, ops: Optional[List] = None):
         self._ops = list(ops) if ops is not None else []
-        Transformation.__init__(self)
+        super().__init__(name="Circuit")
         self.reset()
 
     def reset(self):
