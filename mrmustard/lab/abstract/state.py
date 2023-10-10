f9e896aa755c38d7a2abbd4e29aaf9f83f6be7e2
Robbe De Prins (UGent-imec) <Robbe.DePrins@UGent.be>
2023-10-10 10:47:10 +0200

    added julia packages to readthedocs.yml (to solve that docs weren't building)

--- mrmustard/lab/abstract/state.py
+++ mrmustard/lab/abstract/state.py
@@ -325,12 +325,7 @@ class State:  # pylint: disable=too-many-public-methods
                 return fock.ket_to_dm(ket)
         else:
             if self.is_gaussian:
-                self._dm = fock.wigner_to_fock_state(
-                    self.cov,
-                    self.means,
-                    shape=cutoffs + cutoffs,
-                    return_dm=True,
-                )
+                self._dm = fock.wigner_to_fock_state(self.cov, self.means, shape=cutoffs + cutoffs, return_dm=True)
             elif cutoffs != (current_cutoffs := list(self._dm.shape[: self.num_modes])):
                 paddings = [(0, max(0, new - old)) for new, old in zip(cutoffs, current_cutoffs)]
                 if any(p != (0, 0) for p in paddings):
