8a38a56575713004eb75d824a40538a5cd5c1654
Robbe De Prins (UGent-imec) <Robbe.DePrins@UGent.be>
2023-10-17 16:48:32 +0200

    improved tests thanks to Sam

--- tests/test_math/test_lattice.py
+++ tests/test_math/test_lattice.py
@@ -14,15 +14,20 @@
 
 """Tests for the lattice module"""
 
+import pytest
 import numpy as np
 
 from mrmustard.lab import Gaussian
 from mrmustard import settings
 
+p0 = settings.PRECISION_BITS_HERMITE_POLY
 
-def test_vanillaNumba_vs_binomial():
-    """Test that the vanilla method (Numba with precision of complex128) and the binomial method give the same result"""
-    settings.PRECISION_BITS_HERMITE_POLY = 128
+
+@pytest.mark.parametrize("precision", ([128, 512]))
+def test_vanillaNumba_vs_binomial(precision):
+    """Test that the vanilla method and the binomial method give the same result.
+    Test is repeated for all possible values of PRECISION_BITS_HERMITE_POLY."""
+    settings.PRECISION_BITS_HERMITE_POLY = precision
     G = Gaussian(2)
 
     ket_vanilla = G.ket(cutoffs=[10, 10])[:5, :5]
@@ -30,13 +35,4 @@ def test_vanillaNumba_vs_binomial():
 
     assert np.allclose(ket_vanilla, ket_binomial)
 
-
-def test_vanillaJulia_vs_binomial():
-    """Test that the vanilla method (Julia with precision of complex512) and the binomial method give the same result"""
-    settings.PRECISION_BITS_HERMITE_POLY = 512
-    G = Gaussian(2)
-
-    ket_vanilla = G.ket(cutoffs=[10, 10])[:5, :5]
-    ket_binomial = G.ket(max_photons=10)[:5, :5]
-
-    assert np.allclose(ket_vanilla, ket_binomial)
+    settings.PRECISION_BITS_HERMITE_POLY = p0  # put original value back
