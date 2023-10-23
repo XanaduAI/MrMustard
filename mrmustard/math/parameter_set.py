1e9b8a38555e810d7d3255b3b7f7b07519ae7006
SamFerracin <samuele.ferracin@gmail.com>
2023-10-23 11:35:50 -0400

    added classes and funcs

---
+++ mrmustard/math/parameter_set.py
@@ -0,0 +1,70 @@
+# Copyright 2023 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This module contains the classes to describe sets of parameters."""
+
+from typing import Union
+
+from .parameters import Constant, Variable
+
+__all__ = ["ParameterSet",]
+
+
+class ParameterSet:
+    r"""
+    A set of parameters.
+    """
+
+    def __init__(self):
+        self._constants: dict[str, Constant] = {}
+        self._variables: dict[str, Variable] = {}
+
+    @property
+    def constants(self) -> dict[str, Constant]:
+        r"""
+        The constant parameters in this parameter set.
+        """
+        return self._constants
+
+    @property
+    def variables(self) -> dict[str, Variable]:
+        r"""
+        The variable parameters in this parameter set.
+        """
+        return self._variables
+
+    def add_parameter(self, parameter: Union[Constant, Variable]) -> None:
+        r"""
+        Adds a parameter to this parameter set.
+
+        Args:
+            parameter: A constant or variable parameter.
+
+        Raises:
+            ValueError: If this parameter set already contains a parameter with the same
+                name as that of the given parameter.
+        """
+        name = parameter.name
+
+        if name in self.constants or name in self.variables:
+            msg = f"A parameter with name ``{name}`` is already part of this parameter set."
+            raise ValueError(msg)
+
+        # updates dictionary and dynamically creates an attribute
+        if isinstance(parameter, Constant):
+            self.constants[name] = parameter
+            self.__dict__[name] = self.constants[name]
+        elif isinstance(parameter, Variable):
+            self.variables[parameter.name] = parameter
+            self.__dict__[name] = self.variables[name]
---
+++ mrmustard/math/parameters.py
@@ -0,0 +1,191 @@
+# Copyright 2023 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This module contains the classes to describe constant and variable parameters used in Mr Mustard."""
+
+from typing import Callable, Optional, Tuple, Sequence
+
+from mrmustard.math import Math
+from mrmustard.utils.typing import Tensor
+
+math = Math()
+
+
+__all__ = ["Constant", "Variable"]
+
+
+# ~~~~~~~
+# Classes
+# ~~~~~~~
+
+
+class Constant:
+    r"""
+    A parameter with a constant, immutable value.
+
+    Args:
+        value: The value of this constant.
+        name: The name of this constant.
+        dtype: The type of this constant.
+    """
+
+    def __init__(self, value: any, name: str, dtype: any = math.float64):
+        self._value = value
+        self._name = name
+        self._dtype = dtype
+
+    @property
+    def dtype(self) -> str:
+        r"""
+        The type of this constant.
+        """
+        return self._dtype
+
+    @property
+    def name(self) -> str:
+        r"""
+        The name of this constant.
+        """
+        return self._name
+
+    @property
+    def value(self) -> any:
+        r"""
+        The value of this constant.
+        """
+        return self._value
+
+
+class Variable:
+    r"""
+    A parameter whose value can change.
+
+    Args:
+        value: The value of this variable.
+        name: The name of this variable.
+        bounds: The numerical bounds of this variable.
+        update_fn (optional): The function used to update this variable during
+            training. If ``None``, training defaults to ``update_euclidean``.
+    """
+
+    def __init__(
+        self,
+        value: any,
+        name: str,
+        dtype: any = math.float64,
+        bounds: Tuple[Optional[float], Optional[float]] = (None, None),
+        update_fn: Optional[Callable] = None,
+    ):
+        self._value = value
+        self._name = name
+        self._bounds = bounds
+        self._update_fn = update_fn
+        self._dtype = dtype
+
+    @property
+    def bounds(self) -> Tuple[Optional[float], Optional[float]]:
+        r"""
+        The numerical bounds of this variable.
+        """
+        return self._bounds
+
+    @property
+    def dtype(self) -> str:
+        r"""
+        The type of this variable.
+        """
+        return self._dtype
+
+    @property
+    def name(self) -> str:
+        r"""
+        The name of this variable.
+        """
+        return self._name
+
+    @property
+    def update_fn(self) -> Optional[Callable]:
+        r"""
+        The function used to update this variable during training.
+        """
+        return self._update_fn
+
+    @property
+    def value(self) -> any:
+        r"""
+        The value of this variable.
+        """
+        return self._value
+
+    @bounds.setter
+    def bounds(self, value):
+        self._bounds = value
+
+    @update_fn.setter
+    def update_fn(self, value):
+        self._update_fn = value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+# ~~~~~~~~~
+# Functions
+# ~~~~~~~~~
+
+def update_symplectic(grads_and_vars: Sequence[Tuple[Tensor, Variable]], symplectic_lr: float):
+    r"""
+    Updates the symplectic parameters using the given symplectic gradients.
+    
+    Implemented from:
+        Wang J, Sun H, Fiori S. A Riemannian-steepest-descent approach
+        for optimization on the real symplectic group.
+        Mathematical Methods in the Applied Sciences. 2018 Jul 30;41(11):4273-86.
+    """
+    for dS_euclidean, S in grads_and_vars:
+        Y = math.euclidean_to_symplectic(S, dS_euclidean)
+        YT = math.transpose(Y)
+        new_value = math.matmul(
+            S, math.expm(-symplectic_lr * YT) @ math.expm(-symplectic_lr * (Y - YT))
+        )
+        math.assign(S, new_value)
+        
+def update_orthogonal(grads_and_vars: Sequence[Tuple[Tensor, Variable]], orthogonal_lr: float):
+    r"""Updates the orthogonal parameters using the given orthogonal gradients.
+
+    Implemented from:
+        Y Yao, F Miatto, N Quesada - arXiv preprint arXiv:2209.06069, 2022.
+    """
+    for dO_euclidean, O in grads_and_vars:
+        Y = math.euclidean_to_unitary(O, math.real(dO_euclidean))
+        new_value = math.matmul(O, math.expm(-orthogonal_lr * Y))
+        math.assign(O, new_value)
+
+
+def update_unitary(grads_and_vars: Sequence[Tuple[Tensor, Variable]], unitary_lr: float):
+    r"""Updates the unitary parameters using the given unitary gradients.
+
+    Implemented from:
+        Y Yao, F Miatto, N Quesada - arXiv preprint arXiv:2209.06069, 2022.
+    """
+    for dU_euclidean, U in grads_and_vars:
+        Y = math.euclidean_to_unitary(U, dU_euclidean)
+        new_value = math.matmul(U, math.expm(-unitary_lr * Y))
+        math.assign(U, new_value)
+
+
+def update_euclidean(grads_and_vars: Sequence[Tuple[Tensor, Variable]], euclidean_lr: float):
+    """Updates the parameters using the euclidian gradients."""
+    math.euclidean_opt.lr = euclidean_lr
+    math.euclidean_opt.apply_gradients(grads_and_vars)
