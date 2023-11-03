802cc15876fe8e111af00f46dc09ab119538ca10
ziofil <miatto@gmail.com>
2023-11-03 20:19:23 +0000

    representation base

---
+++ mrmustard/lab/abstract/representation.py
@@ -0,0 +1,46 @@
+    # Copyright 2021 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""This module contains the interface for Representations."""
+
+from __future__ import annotations
+from abc import ABC, abstractmethod, abstractproperty
+
+class Representation(ABC):
+
+    name: str = None
+
+    @abstractproperty
+    def dimension(self):
+        r"""Returns the dimension of the representation."""
+
+    def __getattr__(self, name):
+        r"""Returns the attribute of the data object and wraps it in a function if it is callable.
+        In particular this will work with any method the State or Transformation class calls on the
+        Representation object, which will be passed through to the data object if not implemented
+        on the Representation object.
+        """
+        attr = getattr(self.data, name)
+        if callable(attr):
+            def wrapper(*args, **kwargs):
+                return attr(*args, **kwargs)
+            return wrapper
+        return attr
+
+
+
+class FockArray(Representation):
+    def __init__(self, fock_array):
+        self.data = fock_array
+
