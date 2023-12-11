fe32e161ee44c258903983100d6e2e6b725d39d4
ziofil <miatto@gmail.com>
2023-12-11 21:13:34 +0000

    Refactor update_tensor and update_add_tensor methods

--- mrmustard/math/backend_numpy.py
+++ mrmustard/math/backend_numpy.py
@@ -329,14 +329,16 @@ class BackendNumpy(BackendBase):  # pragma: no cover
 
     @Autocast()
     def update_tensor(self, tensor: np.ndarray, indices: np.ndarray, values: np.ndarray) -> np.ndarray:
+        indices = self.atleast_2d(indices)
         for i, v in zip(indices, values):
-            tensor[i] = v
+            tensor[tuple(i)] = v
         return tensor
 
     @Autocast()
     def update_add_tensor(self, tensor: np.ndarray, indices: np.ndarray, values: np.ndarray) -> np.ndarray:
+        indices = self.atleast_2d(indices)
         for i, v in zip(indices, values):
-            tensor[i] += v
+            tensor[tuple(i)] += v
         return tensor
 
     def zeros(self, shape: Sequence[int], dtype=np.float64) -> np.ndarray:
