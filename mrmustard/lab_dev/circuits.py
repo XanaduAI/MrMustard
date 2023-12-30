be98f07a8dd205faac26153e3727ed8a30273ae6
ziofil <miatto@gmail.com>
2023-12-30 10:20:03 +0000

    Update Circuit class with new connection logic and contraction method

--- mrmustard/lab_dev/circuits.py
+++ mrmustard/lab_dev/circuits.py
@@ -19,7 +19,8 @@ A class to quantum circuits.
 from __future__ import annotations
 
 from typing import Sequence
-
+from functools import reduce
+from operator import add
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
@@ -63,57 +64,93 @@ class Circuit:
         circ.draw();
     """
 
-    def __init__(self, components: Sequence[CircuitComponent] = [], connections: dict[int,int] = {}) -> None:
+    def __init__(self, components: Sequence[CircuitComponent] = []) -> None:
         self.components = list(components)
-        self.components_by_id = {id:i for i,c in enumerate(self.components) for id in c.wires.ids}
-        self.connections = self._connect(self.components, connections)
+        self._connect(self.components)
 
     @staticmethod
-    def _connect(components: Sequence[CircuitComponent], connections: dict[int,int]) -> dict[int,int]:
-        r"""Connects all components (sets their wires.connection[id] value) and returns the updated dictionary of connections.
+    def _connect(components: Sequence[CircuitComponent]):
+        r"""Connects all components (sets the same id of connected wire pairs).
         Supports mode reinitialization."""
         for i,c in enumerate(components):
-            ket_modes = set(m for m in c.wires.output.ket.modes if c.wires.output.ket[m].ids[0] not in connections)
-            bra_modes = set(m for m in c.wires.output.bra.modes if c.wires.output.bra[m].ids[0] not in connections)
+            ket_modes = set(c.wires.output.ket.modes)
+            bra_modes = set(c.wires.output.bra.modes)
             for c_ in components[i+1:]:
-                common_ket = ket_modes & c_.wires.input.ket.modes
-                common_bra = bra_modes & c_.wires.input.bra.modes
-                for m in common_ket:
-                    id = c.wires[m].output.ket.ids[0]
-                    if id not in connections:
-                        c.wires.connections[id] = connections[id] = c_.wires[m].input.ket.ids[0]
-                for m in common_bra:
-                    id = c.wires[m].output.bra.ids[0]
-                    if id not in connections:
-                        c.wires.connections[id] = connections[id] = c_.wires[m].input.bra.ids[0]
+                common_ket = ket_modes.intersection(c_.wires.input.ket.modes)
+                common_bra = bra_modes.intersection(c_.wires.input.bra.modes)
+                c.wires[common_ket].output.ket.ids = c_.wires[common_ket].input.ket.ids
+                c.wires[common_bra].output.bra.ids = c_.wires[common_bra].input.bra.ids
                 ket_modes -= common_ket
                 bra_modes -= common_bra
                 if not ket_modes and not bra_modes:
                     break
-        return connections
+    
+    @staticmethod
+    def _create_einsum_dict(components: Sequence[CircuitComponent]) -> dict[str, Representation]:
+        r"""Creates the einsum dict from the components."""
+        einsum: dict[str, Representation] = {}  
+        ids = {id:None for c in components for id in c.wires.ids}
+        ids_map = {id:i for i,id in enumerate(ids.keys())}  # remap to 0,1,2...
+        strings_ints = [[ids_map[id] for id in c.wires.ids] for c in components]
+        strings = ["".join([chr(i+97) for i in s]) for s in strings_ints]
+        for s,c in zip(strings, components):
+            einsum[s] = c.representation
+        return einsum
 
-    def _contract_CV(self):
+    def _contract_bargmann(self, components: Sequence[CircuitComponent]):
         r"""Contracts the circuit assuming it is made of CV (Bargmann) stuff."""
-        r = self.components[0].representation
-        order = []
-        for c in self.components[1:]:
-            r = r & c.representation  # tensor product 
-            order += [c.wires.index(id) for id in self.connections if id in c.wires.ids]
+        # 0. create the einsum dict and the char-id and id-char dicts
+        einsum_dict = self._create_einsum_dict(self.components)
+        char_id_dict = {char:self.components[i].wires.ids[j] for i,string in enumerate(einsum_dict) for j,char in enumerate(string)}
+        id_char_dict = {id:char for char,id in char_id_dict.items()}
 
-        
+        # 1. create the einsum string for the whole contraction
+        all_chars = [char for s in einsum_dict for char in s]
+        ids = [char_id_dict[char] for char in set(all_chars) if all_chars.count(char) == 1]
+        out_bra = reduce(add, [c.wires.bra.output.subset(ids) for c in components])
+        in_bra = reduce(add, [c.wires.bra.input.subset(ids) for c in components])
+        out_ket = reduce(add, [c.wires.ket.output.subset(ids) for c in components])
+        in_ket = reduce(add, [c.wires.ket.input.subset(ids) for c in components])
+        out_string = "".join([id_char_dict[id] for id in out_bra.ids + in_bra.ids + out_ket.ids + in_ket.ids])
+        einsum_string = ",".join(einsum_dict.keys()) + "->" + out_string
+
+        # 2. use einsum string in path_info to get pair-wise contractions
+        import opt_einsum as oe
+        shapes = [(2,)*len(s) for s in self.einsum_dict.keys()]
+        path_info = oe.contract_path(einsum_string, *shapes, shapes=True, optimize='auto')
+
+        # 3. update the dict as path_info indicates until the whole circuit is contracted
+        for _, _, current, _, _ in path_info[1].contraction_list:
+            idx_A, idx_B, idx_reorder = self.parse_einsum(current)
+            A, (B, out) = current.split(",")[0], current.split(",")[1].split("->")
+            new_r = einsum_dict[A][idx_A] @ einsum_dict[B][idx_B]
+            new_r = new_r.reorder(idx_reorder)
+            einsum_dict[out] = new_r
+
+        # 4. return the result
+        return CircuitComponent("", einsum_dict[out_string], modes_out_bra=out_bra.modes, modes_in_bra=in_bra.modes, modes_out_ket=out_ket.modes, modes_in_ket=in_ket.modes)
+
+    @staticmethod
+    def parse_einsum(string: str):
+        parts, result = string.split('->')
+        parts = parts.split(',')
+        repeated = set(parts[0]).intersection(parts[1])
+        remaining = set(parts[0]).union(parts[1]) - repeated
+        return [parts[0].index(i) for i in repeated], [parts[1].index(i) for i in repeated], [result.index(i) for i in remaining]
+
 
     def __rshift__(self, other: Circuit | CircuitComponent) -> Circuit:
         r"""
         Returns a ``Circuit`` that contains all the components of ``self`` and ``other``.
         """
         other = Circuit([other]) if isinstance(other, CircuitComponent) else other
-        return Circuit(self.components + other.components, self.connections | other.connections)
+        return Circuit(self.components + other.components)
 
-    def __getitem__(self, idx: int) -> CircuitComponent:
+    def __getitem__(self, i: int) -> CircuitComponent:
         r"""
-        The component in position ``idx`` of this circuit's components.
+        The component in position ``i`` of this circuit's components.
         """
-        return self.components[idx]
+        return self.components[i]
 
     def draw(self, layout: str = "spring_layout", figsize: tuple[int, int] = (10, 6)):
         r"""Draws the components in this circuit in the style of a tensor network.
@@ -125,7 +162,7 @@ class Circuit:
         Returns:
             A figure showing the tensor network.
         """
-        components = connect(self.components)
+        self._connect(self.components)
         try:
             fn_layout = getattr(nx.drawing.layout, layout)
         except AttributeError:
@@ -144,26 +181,15 @@ class Circuit:
         arrows_in = nx.Graph()
         arrows_out = nx.Graph()
 
-        for idx, component in enumerate(components):
-            component_id = component.name + str(idx)
+        for i, component in enumerate(self.components):
+            component_id = component.name + str(i)
             graph.add_node(component_id)
             component_labels[component_id] = component.name
             mode_labels[component_id] = ""
             node_size.append(150)
             node_color.append("red")
-
-            wires_in = [
-                (m, w)
-                for m, w in list(component.wires.in_ket.items())
-                + list(component.wires.in_bra.items())
-                if w
-            ]
-            wires_out = [
-                (m, w)
-                for m, w in list(component.wires.out_ket.items())
-                + list(component.wires.out_bra.items())
-                if w
-            ]
+            wires_in = list(zip(component.wires.input.modes, component.wires.input.ids))
+            wires_out = list(zip(component.wires.output.modes, component.wires.output.ids))
             wires = wires_in + wires_out
             for mode, wire in wires:
                 if wire not in graph.nodes:
