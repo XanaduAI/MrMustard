ca77b9c17d72ef50f6b6ffff52faaaa45848376f
SamFerracin <samuele.ferracin@gmail.com>
2024-01-17 16:27:35 -0500

    fmt

--- tests/test_lab_dev/test_simulator.py
+++ tests/test_lab_dev/test_simulator.py
@@ -27,6 +27,7 @@ class TestSimulator:
     r"""
     Tests for the simulator class.
     """
+
     @pytest.mark.parametrize("modes", [[0], [1], [2]])
     @pytest.mark.parametrize("add_bras", [True, False])
     def test_simulate_one_mode_components_in_series(self, modes, add_bras):
@@ -35,14 +36,14 @@ class TestSimulator:
         """
         d1 = Dgate(1, modes=modes)
         d2 = Dgate(2, modes=modes)
-        
+
         circuit = Circuit([d1, d1, d2])
         result = Simulator().run(circuit, add_bras)
-        
+
         rep = result.representation
         A = [[0, 1], [1, 0]]
         b = [4, -4]
-        
+
         assert result.modes == modes
         assert result.name == ""
         assert (rep.A == A).all()
@@ -53,18 +54,38 @@ class TestSimulator:
         r"""
         Simulates a circuit with one-mode Dgates applied in parallel.
         """
-        d1 = Dgate(1, modes=[1,])
-        d2 = Dgate(2, modes=[2,])
-        d3 = Dgate(3, modes=[3,])
-        d4 = Dgate(4, modes=[4,])
-        
+        d1 = Dgate(
+            1,
+            modes=[
+                1,
+            ],
+        )
+        d2 = Dgate(
+            2,
+            modes=[
+                2,
+            ],
+        )
+        d3 = Dgate(
+            3,
+            modes=[
+                3,
+            ],
+        )
+        d4 = Dgate(
+            4,
+            modes=[
+                4,
+            ],
+        )
+
         circuit = Circuit([d3, d1, d4, d2])
         result = Simulator().run(circuit, add_bras)
 
         rep = result.representation
         A = np.kron(np.eye(4), [[0, 1], [1, 0]])
         b = [1, -1, 2, -2, 3, -3, 4, -4]
-        
+
         assert result.modes == [1, 2, 3, 4]
         assert result.name == ""
         assert (rep.A == A).all()
@@ -75,18 +96,38 @@ class TestSimulator:
         r"""
         Simulates a circuit with one-mode Dgates applied in parallel and in series.
         """
-        d1 = Dgate(1, modes=[1,])
-        d2 = Dgate(2, modes=[2,])
-        d3 = Dgate(3, modes=[3,])
-        d4 = Dgate(4, modes=[4,])
-        
+        d1 = Dgate(
+            1,
+            modes=[
+                1,
+            ],
+        )
+        d2 = Dgate(
+            2,
+            modes=[
+                2,
+            ],
+        )
+        d3 = Dgate(
+            3,
+            modes=[
+                3,
+            ],
+        )
+        d4 = Dgate(
+            4,
+            modes=[
+                4,
+            ],
+        )
+
         circuit = Circuit([d1, d1, d2, d1, d3, d1, d4, d2, d1])
         result = Simulator().run(circuit, add_bras)
 
         rep = result.representation
         A = np.kron(np.eye(4), [[0, 1], [1, 0]])
         b = [5, -5, 4, -4, 3, -3, 4, -4]
-        
+
         assert result.modes == [1, 2, 3, 4]
         assert result.name == ""
         assert (rep.A == A).all()
@@ -97,7 +138,13 @@ class TestSimulatorBroken:
     r"""
     Tests for the simulator class that currently do not pass.
     """
-    @pytest.mark.parametrize("modes", [[0, 1],])
+
+    @pytest.mark.parametrize(
+        "modes",
+        [
+            [0, 1],
+        ],
+    )
     def test_simulate_multi_mode_components_in_series(self, modes):
         r"""
         Simulates a circuit with multi-mode Dgates applied in series.
@@ -110,7 +157,7 @@ class TestSimulatorBroken:
         rep = result.representation
         A = [[0, 1], [1, 0]]
         b = [4, -4]
-        
+
         assert result.modes == modes
         assert result.name == ""
         assert (rep.A == A).all()
