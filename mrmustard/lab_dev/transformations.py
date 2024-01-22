76698df96b297e20ce1d5d004d466d055d1d15fc
SamFerracin <samuele.ferracin@gmail.com>
2024-01-22 17:15:54 -0500

    UNDO: dgate

--- mrmustard/lab_dev/transformations.py
+++ mrmustard/lab_dev/transformations.py
@@ -99,8 +99,8 @@ class Dgate(Unitary):
         if len(ys) == 1:
             ys = np.array([ys[0] for _ in range(num_modes)])
 
-        A = np.kron(math.eye(num_modes), np.array([[0, 1], [1, 0]]))
-        B = math.concat([[x + 1j * y, -x + 1j * y] for x, y in zip(xs, ys)], axis=0)
+        A = np.kron(np.array([[0, 1], [1, 0]]), math.eye(num_modes))
+        B = math.concat([xs + 1j * ys, -xs + 1j * ys], axis=0)
         C = np.prod([np.exp(-abs(x + 1j * y) ** 2 / 2) for x, y in zip(xs, ys)])
 
         return Bargmann(A, B, C)
