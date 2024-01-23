b54d23b2dbd59c002399b40971f0403ae040b687
Yuan YAO <sylvieyao502@gmail.com>
2024-01-23 21:56:01 +0000

    add more assertations in the tests for fock repr

--- tests/test_physics/test_fock/test_fock_repr.py
+++ tests/test_physics/test_fock/test_fock_repr.py
@@ -67,6 +67,10 @@ class TestFockRepresentation:
         fock2 = Fock(array2, batch_flag=True)
         fock1_mul_fock2 = fock1 * fock2
         assert fock1_mul_fock2.array.shape == (5, 5, 7, 8)
+        assert np.allclose(
+            math.reshape(fock1_mul_fock2.array, -1),
+            math.reshape(np.einsum("bcde, pcde -> bpcde", array1, array2), -1),
+        )
 
     def test_divide_on_a_scaler(self):
         r"""Tests the divide on a scaler."""
@@ -83,6 +87,10 @@ class TestFockRepresentation:
         fock2 = Fock(array2, batch_flag=True)
         fock1_mul_fock2 = fock1 / fock2
         assert fock1_mul_fock2.array.shape == (5, 5, 7, 8)
+        assert np.allclose(
+            math.reshape(fock1_mul_fock2.array, -1),
+            math.reshape(np.einsum("bcde, pcde -> bpcde", array1, 1/array2), -1),
+        )
 
     def test_conj(self):
         r"""Tests the conjugate of a Fock reprsentation has the correct array."""
@@ -112,6 +120,9 @@ class TestFockRepresentation:
         fock2 = Fock(array2, batch_flag=True)
         fock1_add_fock2 = fock1 + fock2
         assert fock1_add_fock2.array.shape == (10, 5, 7, 8)
+        assert np.allclose(fock1_add_fock2.array[0], array1[0]+array2[0])
+        assert np.allclose(fock1_add_fock2.array[4], array1[0]+array2[4])
+        assert np.allclose(fock1_add_fock2.array[5], array1[1]+array2[0])
 
     def test_sub(self):
         r"""Tests the subtraction function can return the correct array."""
@@ -119,15 +130,19 @@ class TestFockRepresentation:
         array2 = math.astensor(np.random.random((5, 5, 7, 8)))
         fock1 = Fock(array1, batch_flag=True)
         fock2 = Fock(array2, batch_flag=True)
-        fock1_add_fock2 = fock1 - fock2
-        assert fock1_add_fock2.array.shape == (10, 5, 7, 8)
+        fock1_sub_fock2 = fock1 - fock2
+        assert fock1_sub_fock2.array.shape == (10, 5, 7, 8)
+        assert np.allclose(fock1_sub_fock2.array[0], array1[0]-array2[0])
+        assert np.allclose(fock1_sub_fock2.array[4], array1[0]-array2[4])
+        assert np.allclose(fock1_sub_fock2.array[9], array1[1]-array2[4])
 
     def test_trace(self):
         r"""Tests the traceo of given indices."""
-        array1 = math.astensor(np.random.random((2, 6, 5, 8, 7, 6, 8, 7, 9)))
+        array1 = math.astensor(np.random.random((2, 5, 5, 8, 7, 6, 8, 7, 9)))
         fock1 = Fock(array1, batch_flag=True)
         fock2 = fock1.trace(idxs1=[2, 4], idxs2=[1, 7])
         assert fock2.array.shape == (2, 8, 6, 8, 9)
+        assert np.allclose(fock2.array, np.einsum("bccefghfj -> beghj", array1))
 
     def test_reorder(self):
         r"""Tests the reorder of the array."""
