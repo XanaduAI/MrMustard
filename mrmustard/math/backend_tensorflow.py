7fce1060f93dd3fa9f78efcad73dc23c2100d62a
Yuan <16817699+sylviemonet@users.noreply.github.com>
2024-01-24 17:42:58 -0500

    Update mrmustard/math/backend_tensorflow.py
    Co-authored-by: SamFerracin <samuele.ferracin@xanadu.ai>

--- mrmustard/math/backend_tensorflow.py
+++ mrmustard/math/backend_tensorflow.py
@@ -542,7 +542,7 @@ class BackendTensorflow(BackendBase):  # pragma: no cover
             # The following import must come after running "jl = Julia(compiled_modules=False)" in settings.py
             from julia import Main as Main_julia  # pylint: disable=import-outside-toplevel
 
-            (poly0, poly2, poly1010, poly1001, poly1,) = Main_julia.DiagonalAmps.fock_diagonal_amps(
+            (poly0, poly2, poly1010, poly1001, poly1) = Main_julia.DiagonalAmps.fock_diagonal_amps(
                 A, B, C.item(), tuple(cutoffs), precision_bits
             )
 
