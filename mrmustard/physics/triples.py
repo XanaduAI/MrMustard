f0d8e4413804b1a246dc70fa20d33aa8bd0c6f33
Yuan YAO <sylvieyao502@gmail.com>
2024-02-02 19:01:15 +0000

    add more triples and blacked

--- mrmustard/physics/triples.py
+++ mrmustard/physics/triples.py
@@ -20,6 +20,12 @@ from typing import Union
 from mrmustard import math
 from mrmustard.utils.typing import Matrix, Vector, Scalar
 
+
+#  ~~~~~~~~~~~~
+#  Pure States
+#  ~~~~~~~~~~~~
+
+
 def vacuum_ABC_triples(num_modes: int) -> Union[Matrix, Vector, Scalar]:
     r"""Returns the ABC triples of the pure vacuum state.
 
@@ -29,14 +35,21 @@ def vacuum_ABC_triples(num_modes: int) -> Union[Matrix, Vector, Scalar]:
     Returns:
         (Matrix, Vector, Scalar): A matrix, B vector and C scalar of the pure vacuum state.
     """
-    return math.zeros((num_modes, num_modes)), math.zeros(num_modes), 1
+    return vacuum_A_matrix(num_modes), vacuum_B_vector(num_modes), 1
 
-def coherent_ABC_triples(x: Union[Scalar, Vector], y: Union[Scalar, Vector]) -> Union[Matrix, Vector, Scalar]:
+
+def coherent_ABC_triples(
+    x: Union[Scalar, Vector], y: Union[Scalar, Vector]
+) -> Union[Matrix, Vector, Scalar]:
     r"""Returns the ABC triples of the pure coherent state.
+
+    The dimension depends on the dimensions of ``x`` and ``y``. If one of them has dimension one, we will repete it
+    to have the same dimension as the other one. For example, if ``x = [1,2,3], y = [1]``, we will fill it automatically
+    like ``x = [1,2,3], y = [1,1,1]``.
 
     Args:
         x (scalar or vector): real part of displacement (in units of :math:`\sqrt{\hbar}`)
-        y (scalar or vector): imaginary part of displacement (in units of :math:`\sqrt{\hbar}`)  
+        y (scalar or vector): imaginary part of displacement (in units of :math:`\sqrt{\hbar}`)
 
     Returns:
         (Matrix, Vector, Scalar): A matrix, B vector and C scalar of the pure coherent state.
@@ -48,4 +61,101 @@ def coherent_ABC_triples(x: Union[Scalar, Vector], y: Union[Scalar, Vector]) -> 
     if y.shape[-1] == 1:
         y = math.tile(y, x.shape)
     num_modes = x.shape
-    return math.zeros((num_modes, num_modes)), x+1j*y, math.exp(-0.5*(x**2 + y**2))
+    return vacuum_A_matrix(num_modes), x + 1j * y, math.exp(-0.5 * (x**2 + y**2))
+
+
+def squeezed_vacuum_ABC_triples(
+    r: Union[Scalar, Vector], phi: Union[Scalar, Vector]
+) -> Union[Matrix, Vector, Scalar]:
+    r"""Returns the ABC triples of a squeezed vacuum state.
+
+    The dimension depends on the dimensions of ``r`` and ``phi``. If one of them has dimension one, we will repete it
+    to have the same dimension as the other one. For example, if ``r = [1,2,3], phi = [1]``, we will fill it automatically
+    like ``r = [1,2,3], phi = [1,1,1]``.
+
+    Args:
+        r (scalar or vector): squeezing magnitude
+        phi (scalar or vector): squeezing angle
+
+    Returns:
+        (Matrix, Vector, Scalar): A matrix, B vector and C scalar of the squeezed vacuum state.
+    """
+    r = math.atleast_1d(r, math.float64)
+    phi = math.atleast_1d(phi, math.float64)
+    if r.shape[-1] == 1:
+        r = math.tile(r, phi.shape)
+    if phi.shape[-1] == 1:
+        phi = math.tile(phi, r.shape)
+    num_modes = phi.shape[-1]
+    return squeezed_vacuum_A_matrix(r, phi), vacuum_B_vector(num_modes), 1 / math.sqrt(math.cosh(r))
+
+
+def displaced_squeezed_vacuum__ABC_triples(
+    x: Union[Scalar, Vector],
+    y: Union[Scalar, Vector],
+    r: Union[Scalar, Vector],
+    phi: Union[Scalar, Vector],
+) -> Union[Matrix, Vector, Scalar]:
+    r"""Returns the ABC triples of a displaced squeezed vacuum state.
+
+    ValueError will be raise if the dimensions of ``x``, ``y``, ``r`` and ``phi`` are different.
+
+    Args:
+        r (scalar or vector): squeezing magnitude
+        phi (scalar or vector): squeezing angle
+        x (scalar or vector): real part of displacement (in units of :math:`\sqrt{\hbar}`)
+        y (scalar or vector): imaginary part of displacement (in units of :math:`\sqrt{\hbar}`)
+
+    Returns:
+        (Matrix, Vector, Scalar): A matrix, B vector and C scalar of the squeezed vacuum state.
+    """
+    r = math.atleast_1d(r, math.float64)
+    phi = math.atleast_1d(phi, math.float64)
+    x = math.atleast_1d(x, math.float64)
+    y = math.atleast_1d(y, math.float64)
+    if not (r.shape[-1] == phi.shape[-1] & x.shape[-1] == y.shape[-1] & r.shape[-1] == x.shape[-1]):
+        raise ValueError("The shape of them must be the same.")
+    return (
+        squeezed_vacuum_A_matrix(r, phi),
+        displaced_squeezed_vacuum_B_vector(x, y, r, phi),
+        math.exp(
+            -0.5 * (x**2 + y**2)
+            - 0.5 * (x - 1j * y) ** 2 * math.sinh(r) / math.cosh(r) * math.exp(1j * phi)
+        )
+        / math.sqrt(math.cosh(r)),
+    )
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+#  Unitary transformations
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#  ~~~~~~~~~~~~
+#  Utilities
+#  ~~~~~~~~~~~~
+
+
+def vacuum_A_matrix(num_modes: int) -> Matrix:
+    r"""Returns the A matrix of a vacuum state."""
+    return math.zeros((num_modes, num_modes))
+
+
+def vacuum_B_vector(num_modes: int) -> Vector:
+    r"""Returns the B vector of a vacuum state."""
+    return math.zeros(num_modes)
+
+
+def squeezed_vacuum_A_matrix(r: Union[Scalar, Vector], phi: Union[Scalar, Vector]) -> Matrix:
+    r"""Returns the A matrix of a squeezed vacuum state."""
+    return math.diag(-math.sinh(r) / math.cosh(r) * math.exp(1j * phi))
+
+
+def displaced_squeezed_vacuum_B_vector(
+    x: Union[Scalar, Vector],
+    y: Union[Scalar, Vector],
+    r: Union[Scalar, Vector],
+    phi: Union[Scalar, Vector],
+) -> Vector:
+    r"""Returns the B vector of a displaced squeezed vacuum state."""
+    return (x + 1j * y) + (x - 1j * y) * math.sinh(r) / math.cosh(r) * math.exp(1j * phi)
