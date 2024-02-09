1bca63bad67a5c14c1fc260f31cd39217632cbee
SamFerracin <samuele.ferracin@gmail.com>
2024-02-09 15:24:18 -0500

    code factor

--- tests/test_physics/test_triples.py
+++ tests/test_physics/test_triples.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Tests the Bargmann triples."""
+
+
 import numpy as np
 import pytest
 
