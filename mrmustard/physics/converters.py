d095ba8738ee2b231798140aa1b03e47d03876ed
Yuan YAO <sylvieyao502@gmail.com>
2024-02-27 16:31:55 +0000

    add to_fock converters

---
+++ mrmustard/physics/converters.py
@@ -0,0 +1,58 @@
+# Copyright 2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This module contains the converter functions between different representations.
+"""
+from typing import Iterable, Union
+from mrmustard.physics.representations import Representation, Bargmann, Fock
+from mrmustard import math, settings
+from mrmustard.utils.typing import Matrix, Vector, Scalar
+import numpy as np
+
+
+import numpy as np
+import warnings
+
+
+def to_fock(rep: Representation, cutoffs: Union[int, Iterable[int]] = None):
+    r"""This functions takes a Representaion and returns a new Fock Representation object."""
+    if isinstance(rep, Bargmann):
+        shape_of_cutoffs_without_batch = len(rep.b[0])
+        if not cutoffs:
+            cutoffs = (settings.AUTOCUTOFF_MAX_CUTOFF,) * shape_of_cutoffs_without_batch
+        elif isinstance(cutoffs, int):
+            cutoffs = (cutoffs,) * shape_of_cutoffs_without_batch
+        else:
+            if len(cutoffs) != shape_of_cutoffs_without_batch:
+                raise ValueError(
+                    "The shape of the given cutoffs ",
+                    cutoffs,
+                    "is not compatiable with the object's representation, it should be ",
+                    shape_of_cutoffs_without_batch,
+                    ".",
+                )
+        return Fock(
+            math.astensor(
+                [
+                    math.hermite_renormalized(A, b, c, cutoffs)
+                    for A, b, c in zip(rep.A, rep.b, rep.c)
+                ]
+            ),
+            batched=True,
+        )
+    elif isinstance(rep, Fock):
+        if not cutoffs:
+            warnings.warn("The cutoffs here hasn't been used!")
+        return rep
---
+++ tests/test_physics/test_converters.py
@@ -0,0 +1,63 @@
+# Copyright 2024 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests the converters between representations."""
+
+import numpy as np
+import pytest
+
+from mrmustard.physics.representations import Representation, Bargmann, Fock
+from mrmustard.physics.converters import to_fock
+from mrmustard.physics.triples import vacuum_state_Abc, coherent_state_Abc
+from mrmustard import settings
+
+
+class TestToFock:
+    def test_tofock_from_a_fock(self):
+        r"""Tests that the to_fock function works for Fock representation."""
+        fock_original = Fock(np.arange(16).reshape((4, 4)), batched=False)
+        fock_after = to_fock(fock_original)
+        assert fock_original == fock_after
+
+        fock_after = to_fock(fock_original, cutoffs=50)
+        assert fock_original == fock_after
+
+    def test_tofock_from_a_bargmann_vacuum_state(self):
+        r"""Tests that the to_fock function works for a vacuum state in Bargmann representation."""
+        vacuum_bargmann = Bargmann(*vacuum_state_Abc(n_modes=2))
+        vacuum_fock_no_cutoffs = to_fock(vacuum_bargmann)
+        assert vacuum_fock_no_cutoffs.array[0, 0, 0] == 1
+        assert vacuum_fock_no_cutoffs.array.shape[-1] == settings.AUTOCUTOFF_MAX_CUTOFF
+        assert vacuum_fock_no_cutoffs.array.shape == (1, 100, 100)
+
+        vacuum_fock_with_int_cutoffs = to_fock(vacuum_bargmann, cutoffs=80)
+        assert vacuum_fock_with_int_cutoffs.array[0, 0, 0] == 1
+        assert vacuum_fock_with_int_cutoffs.array.shape[-1] == 80
+        assert vacuum_fock_with_int_cutoffs.array.shape == (1, 80, 80)
+
+        with pytest.raises(ValueError):
+            to_fock(vacuum_bargmann, cutoffs=[50])
+
+    def test_tofock_from_a_bargmann_coherent_state(self):
+        r"""Tests that the to_fock function works for a coherent state in Bargmann representation."""
+        coherent_bargmann = Bargmann(*coherent_state_Abc(x=[0.3, 0.2], y=[0.1]))
+        coherent_fock_no_cutoffs = to_fock(coherent_bargmann)
+        assert coherent_fock_no_cutoffs.array[0, 0, 0] == np.exp(
+            -0.5 * (0.3**2 + 0.2**2 + 2 * 0.1**2)
+        )
+        assert coherent_fock_no_cutoffs.array[0, 1, 0] == (0.3 + 1j * 0.1) * np.exp(
+            -0.5 * (0.3**2 + 0.2**2 + 2 * 0.1**2)
+        )
+        assert coherent_fock_no_cutoffs.array.shape[-1] == settings.AUTOCUTOFF_MAX_CUTOFF
+        assert coherent_fock_no_cutoffs.array.shape == (1, 100, 100)
