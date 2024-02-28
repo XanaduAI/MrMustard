0af5454386c8e81a82c1be4db45a255fc2aa7a09
SamFerracin <samuele.ferracin@gmail.com>
2024-02-28 15:30:45 -0500

    codefactor

--- mrmustard/lab_dev/__init__.py
+++ mrmustard/lab_dev/__init__.py
@@ -12,5 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+r"""
+The lab module in development.
+"""
+
 from .circuit_components import *
 from .wires import Wires
