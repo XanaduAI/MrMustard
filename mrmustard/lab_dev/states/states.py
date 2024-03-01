7f02d97184e4a63b9d0c9345d421a5506c83eef5
SamFerracin <samuele.ferracin@gmail.com>
2024-03-01 10:45:26 -0500

    oops

--- mrmustard/lab_dev/states/states.py
+++ mrmustard/lab_dev/states/states.py
@@ -94,7 +94,7 @@ class Number(Ket):
         >>> from mrmustard.lab_dev import Number
 
         >>> state = Number(modes=[0, 1], n=[10, 20], cutoff=20)
-        >>> assert state.modes == [0, 1, 2]
+        >>> assert state.modes == [0, 1]
     
     Args:
         modes: The modes of the number state.
