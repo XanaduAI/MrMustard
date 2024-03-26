a8b59322b4b1c2da851f0bad2dcb5e35731e43bf
Yuan <sylvieyao502@gmail.com>
2024-03-26 08:50:17 +0000

    fix bugs in tests

--- tests/test_physics/test_utils.py
+++ tests/test_physics/test_utils.py
@@ -15,27 +15,28 @@
 """Tests for the helper functions in physics."""
 
 import numpy as np
+from mrmustard import math
 from mrmustard.physics.utils import real_gaussian_integral
 
 
 def test_real_gaussian_integral():
     """Tests the ``real_gaussian_integral`` method with a hard-coded A matric from a Gaussian(3) state."""
-    A = np.array(
+    A = math.astensor(np.array(
         [
             [0.35307718 - 0.09738001j, -0.01297994 + 0.26050244j, 0.05349344 - 0.13728068j],
             [-0.01297994 + 0.26050244j, 0.05696707 - 0.2351408j, 0.18954838 - 0.42959383j],
             [0.05349344 - 0.13728068j, 0.18954838 - 0.42959383j, -0.16931712 - 0.09205837j],
         ]
-    )
-    b = np.arange(3)
-    c = 1.0
+    ))
+    b = math.astensor(np.arange(3)+0j)
+    c = 1.0 + 0j
     res = real_gaussian_integral((A, b, c), idx=[0, 1])
-    assert np.allclose(res[0], A[2, 2] - A[2:, :2] @ np.linalg.inv(A[:2, :2]) @ A[:2, 2:])
-    assert np.allclose(res[1], b[2] - A[2:, :2] @ np.linalg.inv(A[:2, :2]) @ b[:2])
+    assert np.allclose(res[0], A[2, 2] - A[2:, :2] @ math.inv(A[:2, :2]) @ A[:2, 2:])
+    assert np.allclose(res[1], b[2] - math.sum(A[2:, :2] * math.matvec(math.inv(A[:2, :2]), b[:2])))
     assert np.allclose(
         res[2],
         c
         * (2 * np.pi)
-        / np.sqrt(np.linalg.det(A[:2, :2]))
-        * np.exp(-0.5 * b[:2] @ np.linalg.inv(A[:2, :2]) @ b[:2]),
+        / math.sqrt(math.det(A[:2, :2]))
+        * math.exp(-0.5 * math.sum(b[:2] * math.matvec(math.inv(A[:2, :2]), b[:2]))),
     )
