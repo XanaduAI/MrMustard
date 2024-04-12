850cfbcafe537bbf04f2caa6e0b49b7e01b9666d
Filippo Miatto <ziofil@users.noreply.github.com>
2024-04-12 10:42:09 -0700

    removed test

--- tests/test_math/test_backend_manager.py
+++ tests/test_math/test_backend_manager.py
@@ -298,15 +298,6 @@ class TestBackendManager:
         arr = np.array([[1.0, 2.0], [3.0, 4.0]])
         assert np.allclose(math.det(arr), -2.0)
 
-    def test_det_warning(self):
-        r"""
-        if numpy stops raising this error remove the filter
-        in the det method in the numpy backend
-        """
-        with pytest.warns(RuntimeWarning):
-            matrix = np.random.normal(size=(2, 2)) + 0.0j  # needs to be complex
-            np.linalg.det(matrix)  # pylint: disable=pointless-statement
-
     def test_diag(self):
         r"""
         Tests the ``diag`` method.
