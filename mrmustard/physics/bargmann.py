387c0c3e1be8b3a2c998cfae0a39759e8ca04bcd
Yuan YAO <sylvieyao502@gmail.com>
2024-05-07 18:21:58 +0000

    try to fix errors with math

--- mrmustard/physics/bargmann.py
+++ mrmustard/physics/bargmann.py
@@ -12,14 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""
+r"""
 This module contains functions for performing calculations on objects in the Bargmann representations.
 
 The Bargmann representation of the state can be obtained as projecting the state on to the Bargmann basis.
 
 The Bargmann basis :math:`|z\rangle_b` can be defined from the coherent state basis :math:`|z\rangle_c` 
 
-.. math :: 
+.. math:: 
     |z\rangle_b = e^\frac{|z|^2}{2}|z\rangle_c = \sum_n \frac{z^n}{\sqrt{n!}}|n\rangle_f.
 
 Any Gaussian objects :math:`O` can be written in the Bargmann basis as a Gaussian exponential function, parametrized by a matrix :math:`A`, a vector :math:`b` and a scalar :math:`c`, which is called ``triples`` through all the documentations in MM.
@@ -27,7 +27,7 @@ The objects in Bargmann representation uses the :class:`~mrmustard.physics.ansat
 
 A ``n``-mode pure Gaussian state :math:`|\psi\rangle` can be defined as
 
-.. math ::
+.. math::
     \langle \alpha | \psi \rangle = 
 """
 import numpy as np
