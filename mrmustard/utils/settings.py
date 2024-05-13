4c2b33b6166cd2d7aa6f775130108bea5424248f
Filippo Miatto <ziofil@users.noreply.github.com>
2024-05-13 08:57:31 -0700

    Batched methods + simplified settings (#391)
    **Description of the Change:**
    - L2_norm, purity, probability can now work element-wise along the batch
    dimension or globally. This enables using the batch dimension more
    freely.
    
    - `settings.UNSAFE_ZIP_BATCH` is now available to enable computing complex integrals where the A,b,c triples are taken by zipping the batch dimension of the two representations. It's marked UNSAFE because it's a hack that we nevertheless want available.
    
    - simplified docstrings in Settings class
    
    - adds setting `CIRCUIT_DRAW_PARAMS` to turn on/off the component parameters in the circuit drawer.
    
    ---------
    
    Co-authored-by: Yuan YAO <16817699+sylviemonet@users.noreply.github.com>

--- mrmustard/utils/settings.py
+++ mrmustard/utils/settings.py
@@ -12,51 +12,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""A module containing the settings.
-"""
+"""A module containing global settings."""
 
-from typing import Any
 import os
 from rich import print
 import rich.table
 import numpy as np
 
-from mrmustard.utils.filters import add_complex_warning_filter, remove_complex_warning_filter
+from mrmustard.utils.filters import (
+    add_complex_warning_filter,
+    remove_complex_warning_filter,
+)
 
-__all__ = ["Settings", "settings"]
-
-
-class ImmutableSetting:
-    r"""A setting that becomes immutable after the first time its value is queried.
-
-    Args:
-        value (any): the default value of this setting
-        name (str): the name of this setting
-    """
-
-    def __init__(self, value: Any, name: str) -> None:
-        self._value = value
-        self._name = name
-        self._is_immutable = False
-
-    @property
-    def name(self):
-        r"""The name of this setting."""
-        return self._name
-
-    @property
-    def value(self):
-        r"""The value of this setting."""
-        self._is_immutable = True
-        return self._value
-
-    @value.setter
-    def value(self, value):
-        if np.isclose(value, self._value):
-            return
-        if self._is_immutable:
-            raise ValueError(f"Cannot change the value of `settings.{self.name}`.")
-        self._value = value
+__all__ = ["settings"]
 
 
 # pylint: disable=too-many-instance-attributes
@@ -85,75 +53,71 @@ class Settings:
         return cls.instance
 
     def __init__(self):
-        self._hbar = ImmutableSetting(2.0, "HBAR")
-        self._debug = False
-        self._autocutoff_probability = 0.999  # capture at least 99.9% of the probability
-        self._autocutoff_max_cutoff = 100
-        self._autocutoff_min_cutoff = 1
-        self._circuit_decimals = 3
-        self._discretization_method = "iterative"
-        # use cutoff=5 for each mode when determining if two transformations in fock repr are equal
-        # 3 is enough to include a full step of the rec relations
-        self._eq_transformation_cutoff = 3
-        self._eq_transformation_rtol_fock = 1e-3
-        self._eq_transformation_rtol_gauss = 1e-6
-        # for the detectors
-        self._pnr_internal_cutoff = 50
-        self._homodyne_squeezing = 10.0
-        # misc
-        self._progressbar = True
+        self._hbar = 2.0
+        self._hbar_locked = False
         self._seed = np.random.randint(0, 2**31 - 1)
         self.rng = np.random.default_rng(self._seed)
-        self._default_bs_method = "vanilla"  # can be 'vanilla' or 'schwinger'
-        self._precision_bits_hermite_poly = 128
         self._julia_initialized = (
             False  # set to True when Julia is initialized (cf. PRECISION_BITS_HERMITE_POLY.setter)
         )
-        self._complex_warning = False
 
-    def _force_hbar(self, value):
-        r"can set the value of HBAR at any time. use with caution."
-        self._hbar._value = value
+        self.UNSAFE_ZIP_BATCH = False
+        "Whether to operate element-wise within a batch of Ansatze. If True, the length of the batch dimension of two circuit components must be the same. Default is False."
 
-    @property
-    def AUTOCUTOFF_MAX_CUTOFF(self):
+        self.DEBUG = False
+        "Whether or not to print the vector of means and the covariance matrix alongside the html representation of a state. Default is False."
+
+        self.AUTOCUTOFF_PROBABILITY = 0.999
+        "The minimum l2_norm to reach before automatically stopping the Bargmann-to-Fock conversion. Default is 0.999."
+
+        self.AUTOCUTOFF_MAX_CUTOFF = 100
         r"""The maximum value for autocutoff. Default is ``100``."""
-        return self._autocutoff_max_cutoff
 
-    @AUTOCUTOFF_MAX_CUTOFF.setter
-    def AUTOCUTOFF_MAX_CUTOFF(self, value: int):
-        self._autocutoff_max_cutoff = value
-
-    @property
-    def AUTOCUTOFF_MIN_CUTOFF(self):
+        self.AUTOCUTOFF_MIN_CUTOFF = 1
         r"""The minimum value for autocutoff. Default is ``1``."""
-        return self._autocutoff_min_cutoff
 
-    @AUTOCUTOFF_MIN_CUTOFF.setter
-    def AUTOCUTOFF_MIN_CUTOFF(self, value: int):
-        self._autocutoff_min_cutoff = value
+        self.CIRCUIT_DRAW_PARAMS = True
+        "Whether or not to draw the parameters of a circuit."
 
-    @property
-    def AUTOCUTOFF_PROBABILITY(self):
-        r"""The autocutoff probability. Default is ``0.999``."""
-        return self._autocutoff_probability
+        self.CIRCUIT_DECIMALS = 3
+        "The number of decimal places to display when drawing a circuit."
 
-    @AUTOCUTOFF_PROBABILITY.setter
-    def AUTOCUTOFF_PROBABILITY(self, value: float):
-        self._autocutoff_probability = value
+        self.DISCRETIZATION_METHOD = "iterative"
+        "The method used to discretize the Wigner function. Default is ``iterative``."
 
-    @property
-    def CIRCUIT_DECIMALS(self):
-        r"""The number of decimals displayed when drawing a circuit with parameters. Default is ``3``."""
-        return self._circuit_decimals
+        self.EQ_TRANSFORMATION_CUTOFF = 3  # enough for a full step of rec relations
+        "The cutoff used when comparing two transformations via the Choi–Jamiolkowski isomorphism. Default is 3."
 
-    @CIRCUIT_DECIMALS.setter
-    def CIRCUIT_DECIMALS(self, value: int):
-        self._circuit_decimals = value
+        self.EQ_TRANSFORMATION_RTOL_FOCK = 1e-3
+        "The relative tolerance used when comparing two transformations via the Choi–Jamiolkowski isomorphism. Default is 1e-3."
+
+        self.EQ_TRANSFORMATION_RTOL_GAUSS = 1e-6
+        "The relative tolerance used when comparing two transformations on Gaussian states. Default is 1e-6."
+
+        self.PRN_INTERNAL_CUTOFF = 50
+        "The cutoff used when computing the output of a PNR detection. Default is 50."
+
+        self.HOMODYNE_SQUEEZING = 10.0
+        "The value of squeezing for homodyne measurements. Default is 10.0."
+
+        self.PROGRESSBAR = True
+        "Whether or not to display the progress bar when performing training. Default is True."
+
+        self.PNR_INTERNAL_CUTOFF = 50
+        "The cutoff used when computing the output of a PNR detection. Default is 50."
+
+        self.BS_FOCK_METHOD = "vanilla"  # can be 'vanilla' or 'schwinger'
+        "The method for computing a beam splitter in the Fock basis . Default is ``vanilla``."
+
+        self.PRECISION_BITS_HERMITE_POLY = 128
+        "The number of bits used to represent a single Fock amplitude when calculating Hermite polynomials. Default is 128."
+
+        self.COMPLEX_WARNING = False
+        "Whether tensorflow's ComplexWarning should be raised when a complex is cast to a float. Default is False."
 
     @property
     def COMPLEX_WARNING(self):
-        r"""Whether tensorflow's ``ComplexWarning``s should be raised when a complex is casted to a float. Default is ``False``."""
+        r"""Whether tensorflow's ``ComplexWarning``s should be raised when a complex is cast to a float. Default is ``False``."""
         return self._complex_warning
 
     @COMPLEX_WARNING.setter
@@ -165,109 +129,19 @@ class Settings:
             add_complex_warning_filter()
 
     @property
-    def DEBUG(self):
-        r"""Whether or not to print the vector of means and the covariance matrix alongside the
-        html representation of a state. Default is ``False``.
-        """
-        return self._debug
-
-    @DEBUG.setter
-    def DEBUG(self, value: bool):
-        self._debug = value
-
-    @property
-    def DISCRETIZATION_METHOD(self):
-        r"""The method used to discretize the Wigner function. Default is ``iterative``.
-
-        Can be either ``'iterative'`` or ``'clenshaw'``.
-        """
-        return self._discretization_method
-
-    @DISCRETIZATION_METHOD.setter
-    def DISCRETIZATION_METHOD(self, value: str):
-        self._discretization_method = value
-
-    @property
-    def DEFAULT_BS_METHOD(self):
-        r"""The default method for computing the transformation operated by a beam splitter in
-         the Fock basis . Default is ``vanilla``.
-
-        Can be either ``'vanilla'`` or ``'schwinger'``.
-        """
-        return self._default_bs_method
-
-    @DEFAULT_BS_METHOD.setter
-    def DEFAULT_BS_METHOD(self, value: str):
-        self._default_bs_method = value
-
-    @property
-    def EQ_TRANSFORMATION_CUTOFF(self):
-        r"""The cutoff used when comparing two transformations via the Choi–Jamiolkowski
-        isomorphism. Default is ``3``."""
-        return self._eq_transformation_cutoff
-
-    @EQ_TRANSFORMATION_CUTOFF.setter
-    def EQ_TRANSFORMATION_CUTOFF(self, value: int):
-        self._eq_transformation_cutoff = value
-
-    @property
-    def EQ_TRANSFORMATION_RTOL_FOCK(self):
-        r"""The relative tolerance used when comparing two transformations via the Choi–Jamiolkowski
-        isomorphism. Default is ``1e-3``."""
-        return self._eq_transformation_rtol_fock
-
-    @EQ_TRANSFORMATION_RTOL_FOCK.setter
-    def EQ_TRANSFORMATION_RTOL_FOCK(self, value: float):
-        self._eq_transformation_rtol_fock = value
-
-    @property
-    def EQ_TRANSFORMATION_RTOL_GAUSS(self):
-        r"""The relative tolerance used when comparing two transformations on Gaussian states.
-        Default is ``1e-6``."""
-        return self._eq_transformation_rtol_gauss
-
-    @EQ_TRANSFORMATION_RTOL_GAUSS.setter
-    def EQ_TRANSFORMATION_RTOL_GAUSS(self, value: float):
-        self._eq_transformation_rtol_gauss = value
-
-    @property
     def HBAR(self):
         r"""The value of the Planck constant. Default is ``2``.
 
         Cannot be changed after its value is queried for the first time.
         """
-        return self._hbar.value
+        self._hbar_locked = True
+        return self._hbar
 
     @HBAR.setter
     def HBAR(self, value: float):
-        self._hbar.value = value
-
-    @property
-    def HOMODYNE_SQUEEZING(self):
-        r"""The value of squeezing for homodyne measurements. Default is ``10``."""
-        return self._homodyne_squeezing
-
-    @HOMODYNE_SQUEEZING.setter
-    def HOMODYNE_SQUEEZING(self, value: float):
-        self._homodyne_squeezing = value
-
-    @property
-    def PNR_INTERNAL_CUTOFF(self):
-        r"""The cutoff used when computing the output of a PNR detection. Default is ``50``."""
-        return self._pnr_internal_cutoff
-
-    @PNR_INTERNAL_CUTOFF.setter
-    def PNR_INTERNAL_CUTOFF(self, value: int):
-        self._pnr_internal_cutoff = value
-
-    @property
-    def PROGRESSBAR(self):
-        r"""Whether or not to display the progress bar when performing training. Default is ``True``."""
-        return self._progressbar
-
-    @PROGRESSBAR.setter
-    def PROGRESSBAR(self, value: bool):
-        self._progressbar = value
+        if value != self._hbar and self._hbar_locked:
+            raise ValueError("Cannot change the value of `settings.HBAR`.")
+        self._hbar = value
 
     @property
     def SEED(self):
@@ -340,10 +214,10 @@ class Settings:
         table.add_column("Value")
 
         for key, val in self.__dict__.items():
-            if key in not_displayed:
+            if key in not_displayed or key.startswith("_"):
                 continue
-            key = key.upper()[1:]
-            value = str(val._value) if isinstance(val, ImmutableSetting) else str(val)
+            key = key.upper()
+            value = str(val)
             table.add_row(key, value)
 
         print(table)
