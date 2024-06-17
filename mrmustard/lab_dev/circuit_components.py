4c72acfcefd79cd98eac96ac4449cf463cf686cb
Filippo Miatto <ziofil@users.noreply.github.com>
2024-06-17 10:56:39 -0700

    Update mrmustard/lab_dev/circuit_components.py

--- mrmustard/lab_dev/circuit_components.py
+++ mrmustard/lab_dev/circuit_components.py
@@ -491,6 +491,7 @@ class CircuitComponent:
         the wires of the components. For example this allows ``Ket``s to be right-shifted
         into ``Channel``s and automatically the result is a ``DM``. If the result has
         no wires left, it returns the (batched) scalar value of the representation.
+        Note that a ``CircuitComponent`` is allowed to right-shift into scalars because the scalar part may result from an automated contraction subroutine that involves several components).
 
         .. code-block::
             >>> from mrmustard.lab_dev import Coherent, Attenuator, Ket, DM, Channel
