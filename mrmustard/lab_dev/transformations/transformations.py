0d2fe46201ec54c13591e122d0f1cf4bf81a0fbd
Kasper Nielsen <74625466+kaspernielsen96@users.noreply.github.com>
2024-06-18 10:32:28 -0400

    Adding amplification channel (#402)
    **Context:**
    Adding amplification channel
    **Description of the Change:**
    Adding the amplification channel and tests of the channel
    **Benefits:**
    More (louder) channels
    **Possible Drawbacks:**
    
    **Related GitHub Issues:**
    
    ---------
    
    Co-authored-by: Filippo Miatto <ziofil@users.noreply.github.com>

--- mrmustard/lab_dev/transformations/transformations.py
+++ mrmustard/lab_dev/transformations/transformations.py
@@ -25,7 +25,7 @@ from ...physics.representations import Bargmann
 from ...physics import triples
 from ..utils import make_parameter, reshape_params
 
-__all__ = ["Attenuator", "BSgate", "Dgate", "Rgate", "Sgate", "Identity", "S2gate"]
+__all__ = ["Amplifier", "Attenuator", "BSgate", "Dgate", "Rgate", "Sgate", "S2gate", "Identity"]
 
 
 class BSgate(Unitary):
@@ -387,6 +387,77 @@ class S2gate(Unitary):
     @property
     def representation(self) -> Bargmann:
         return Bargmann(*triples.twomode_squeezing_gate_Abc(self.r.value, self.phi.value))
+
+
+class Amplifier(Channel):
+    r"""The noisy amplifier channel.
+
+    If ``gain`` is an iterable, its length must be equal to `1` or `N`. If it length is equal to `1`,
+    all the modes share the same gain.
+
+    .. code-block ::
+
+        >>> import numpy as np
+        >>> from mrmustard.lab_dev import Amplifier, Coherent
+
+        >>> amp = Amplifier([0], gain=4)
+        >>> coh = Coherent([0], x=1.0, y=2.0)  # units of hbar=2 (default) 
+        >>> _, mu, _ = (coh >> amp).phase_space(0)
+        >>> assert np.allclose(mu[0], np.array([4.0, 8.0]))
+
+    Args:
+        modes: The modes this gate is applied to.
+        gain: The gain.
+        gain_trainable: Whether the gain is a trainable variable.
+        gain_bounds: The bounds for the gain.
+
+    .. details::
+
+        The :math:`N`-mode attenuator is defined as
+
+        .. math::
+            X = /sqrt{/bar{g}}I_{2N} \text{ , }
+            Y = (/bar{g}-1)I_{2N} \text{ , and }
+            d = O_{4N}\:,
+
+        where :math:`/bar{g}` is the gain and
+        :math:`\text{diag}_N(\bar{g})` is the :math:`N\text{x}N` matrix with diagonal :math:`\bar{g}`.
+
+        Its ``(A,b,c)`` triple is given by 
+
+        .. math::
+            A &= \begin{bmatrix}
+                    O_N & \text{diag}_N(1/(\sqrt{\bar{g}}) & \text{diag}_N(1-1/\bar{g}) & O_N \\
+                    \text{diag}_N(1/(\sqrt{\bar{g}}) & O_N & O_N & O_N \\
+                    \text{diag}_N(1-1/\bar{g})  & O_N & O_N &  \text{diag}_N(1/(\bar{g})\\
+                    O_N & O_N &  \text{diag}_N(1/(\sqrt{\bar{g}}) & O_N
+                \end{bmatrix} \\ \\
+            b &= O_{4N} \\ \\
+            c &= 1//bar{g}\:.
+    """
+
+    def __init__(
+        self,
+        modes: Sequence[int],
+        gain: Union[Optional[float], Optional[list[float]]] = 1.0,
+        gain_trainable: bool = False,
+        gain_bounds: Tuple[Optional[float], Optional[float]] = (1.0, None),
+    ):
+        super().__init__(modes_out=modes, modes_in=modes, name="Amp")
+        self._add_parameter(
+            make_parameter(
+                gain_trainable,
+                gain,
+                "gain",
+                gain_bounds,
+                None,
+            )
+        )
+
+    @property
+    def representation(self) -> Bargmann:
+        g = list(reshape_params(len(self.modes), g=self.gain.value))[0]
+        return Bargmann(*triples.amplifier_Abc(g))
 
 
 class Attenuator(Channel):
