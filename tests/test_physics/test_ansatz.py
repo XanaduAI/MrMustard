33d2079b9439af831f09aefe998f91b6fc74adb0
kaspernielsen96 <kaw.nielsen@gmail.com>
2024-07-12 12:09:59 -0400

    format

--- tests/test_physics/test_ansatz.py
+++ tests/test_physics/test_ansatz.py
@@ -413,7 +413,6 @@ class TestDiffOpPolyExpAnsatz:
         assert np.allclose(ansatz2.vec[0], b)
         assert np.allclose(ansatz2.array[0], d * c)
 
-
     def test_truediv(self):
         A1, b1, c1 = Abc_triple(5)
         A2, b2, c2 = Abc_triple(5)
@@ -537,20 +536,20 @@ class TestDiffOpPolyExpAnsatz:
 
     def test_decompose_ansatz(self):
         A, b, _ = Abc_triple(4)
-        c = np.random.uniform(-10,10,size=(1,3,3,3))
+        c = np.random.uniform(-10, 10, size=(1, 3, 3, 3))
         ansatz = DiffOpPolyExpAnsatz(A, b, c)
         decomp_ansatz = ansatz.decompose_ansatz()
-        z = np.random.uniform(-10,10,size=(1,1))
-        assert np.allclose(ansatz(z),decomp_ansatz(z))
-        assert np.allclose(decomp_ansatz.A.shape, (1,2,2))
+        z = np.random.uniform(-10, 10, size=(1, 1))
+        assert np.allclose(ansatz(z), decomp_ansatz(z))
+        assert np.allclose(decomp_ansatz.A.shape, (1, 2, 2))
 
     def test_decompose_ansatz_batch(self):
         A1, b1, _ = Abc_triple(4)
-        c1 = np.random.uniform(-10,10,size=(3,3,3))
+        c1 = np.random.uniform(-10, 10, size=(3, 3, 3))
         A2, b2, _ = Abc_triple(4)
-        c2 = np.random.uniform(-10,10,size=(3,3,3))
-        ansatz = DiffOpPolyExpAnsatz([A1,A2], [b1,b2], [c1,c2])
+        c2 = np.random.uniform(-10, 10, size=(3, 3, 3))
+        ansatz = DiffOpPolyExpAnsatz([A1, A2], [b1, b2], [c1, c2])
         decomp_ansatz = ansatz.decompose_ansatz()
-        z = np.random.uniform(-10,10,size=(1,1))
-        assert np.allclose(ansatz(z),decomp_ansatz(z))
-        assert np.allclose(decomp_ansatz.A.shape, (2,2,2))
+        z = np.random.uniform(-10, 10, size=(1, 1))
+        assert np.allclose(ansatz(z), decomp_ansatz(z))
+        assert np.allclose(decomp_ansatz.A.shape, (2, 2, 2))
