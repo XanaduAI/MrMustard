8149ab376ba7fe0f1e871a0b4ba63752c2a77f5a
Arsalan Motamedi <motamedi17@gmail.com>
2024-07-12 14:06:20 -0400

    formatting corrected

--- mrmustard/physics/bargmann.py
+++ mrmustard/physics/bargmann.py
@@ -161,8 +161,8 @@ def Symplectic2Au(S):
     A_1 = S_2 @ math.conj(math.inv(S_1))  # use solve for inverse
     A_2 = math.conj(math.inv(S_1.T))
     A_3 = A_2.T
-    A_4 = -math.conj(math.solve(S_1,S_2))
-    #-np.conjugate(np.linalg.pinv(S_1)) @ np.conjugate(S_2)
+    A_4 = -math.conj(math.solve(S_1, S_2))
+    # -np.conjugate(np.linalg.pinv(S_1)) @ np.conjugate(S_2)
 
     A = math.block([[A_1, A_2], [A_3, A_4]])
 
