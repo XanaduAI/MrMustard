c221a017e5dd3274e204f3d03f6b281065b74e79
Arsalan Motamedi <motamedi17@gmail.com>
2024-07-18 11:46:53 -0400

    removed random methods

--- tests/test_lab_dev/test_states.py
+++ tests/test_lab_dev/test_states.py
@@ -358,12 +358,12 @@ class TestKet:  # pylint: disable=too-many-public-methods
         settings.UNSAFE_ZIP_BATCH = False
         assert better_cat == Coherent([0], x=2.0) + Coherent([0], x=-2.0)
 
-    @pytest.mark.parametrize("max_sq", [1, 2, 3])
-    def test_random_states(self, max_sq):
-        psi = Ket.random([1, 22], max_sq)
-        A = psi.representation.A[0]
-        assert np.isclose(psi.probability, 1)  # checks if the state is normalized
-        assert np.allclose(A - np.transpose(A), np.zeros(2))  # checks if the A matrix is symmetric
+    # @pytest.mark.parametrize("max_sq", [1, 2, 3])
+    # def test_random_states(self, max_sq):
+    #     psi = Ket.random([1, 22], max_sq)
+    #     A = psi.representation.A[0]
+    #     assert np.isclose(psi.probability, 1)  # checks if the state is normalized
+    #     assert np.allclose(A - np.transpose(A), np.zeros(2))  # checks if the A matrix is symmetric
 
 
 class TestDM:
@@ -590,16 +590,16 @@ class TestDM:
         assert isinstance(dm >> Coherent([0], 1).dual, DM)
         assert isinstance(dm >> Coherent([0], 1).dm().dual, DM)
 
-    @pytest.mark.parametrize("modes", [[5], [1, 2]])
-    def test_random(self, modes):
+    # @pytest.mark.parametrize("modes", [[5], [1, 2]])
+    # def test_random(self, modes):
 
-        m = len(modes)
-        dm = DM.random(modes)
-        A = dm.representation.A[0]
-        Gamma = A[:m, m:]
-        assert np.all(
-            np.linalg.eigvals(Gamma) >= 0
-        )  # checks if the off-diagonal block of dm is PSD
+    #     m = len(modes)
+    #     dm = DM.random(modes)
+    #     A = dm.representation.A[0]
+    #     Gamma = A[:m, m:]
+    #     assert np.all(
+    #         np.linalg.eigvals(Gamma) >= 0
+    #     )  # checks if the off-diagonal block of dm is PSD
 
 
 class TestCoherent:
