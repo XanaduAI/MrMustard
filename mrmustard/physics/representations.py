9e1ccaba996c1687b38ca52431092f0ec9199392
kaspernielsen96 <kaw.nielsen@gmail.com>
2024-07-22 16:14:22 -0400

    import new contract method

--- mrmustard/physics/representations.py
+++ mrmustard/physics/representations.py
@@ -32,6 +32,7 @@ from mako.template import Template
 from mrmustard import math, settings
 from mrmustard.physics.gaussian_integrals import (
     contract_two_Abc,
+    contract_two_Abc_poly,
     reorder_abc,
     complex_gaussian_integral,
 )
@@ -481,25 +482,35 @@ class Bargmann(Representation):
 
             return to_fock(self, shape=shape)[idx_s] @ other[idx_o]
 
-        if self.ansatz.degree > 0 or other.ansatz.degree > 0:
-            raise NotImplementedError(
-                "Inner product of ansatze is only supported for ansatze with polynomial of degree 0."
-            )
-
         Abc = []
-        if settings.UNSAFE_ZIP_BATCH:
-            if self.ansatz.batch_size != other.ansatz.batch_size:
-                raise ValueError(
-                    f"Batch size of the two ansatze must match since the settings.UNSAFE_ZIP_BATCH is {settings.UNSAFE_ZIP_BATCH}."
-                )
-            for (A1, b1, c1), (A2, b2, c2) in zip(
-                zip(self.A, self.b, self.c), zip(other.A, other.b, other.c)
-            ):
-                Abc.append(contract_two_Abc((A1, b1, c1), (A2, b2, c2), idx_s, idx_o))
+        if self.ansatz.degree > 0 or other.ansatz.degree > 0:
+            if settings.UNSAFE_ZIP_BATCH:
+                if self.ansatz.batch_size != other.ansatz.batch_size:
+                    raise ValueError(
+                        f"Batch size of the two ansatze must match since the settings.UNSAFE_ZIP_BATCH is {settings.UNSAFE_ZIP_BATCH}."
+                    )
+                for (A1, b1, c1), (A2, b2, c2) in zip(
+                    zip(self.A, self.b, self.c), zip(other.A, other.b, other.c)
+                ):
+                    Abc.append(contract_two_Abc_poly((A1, b1, c1), (A2, b2, c2), idx_s, idx_o))
+            else:
+                for A1, b1, c1 in zip(self.A, self.b, self.c):
+                    for A2, b2, c2 in zip(other.A, other.b, other.c):
+                        Abc.append(contract_two_Abc_poly((A1, b1, c1), (A2, b2, c2), idx_s, idx_o))
         else:
-            for A1, b1, c1 in zip(self.A, self.b, self.c):
-                for A2, b2, c2 in zip(other.A, other.b, other.c):
+            if settings.UNSAFE_ZIP_BATCH:
+                if self.ansatz.batch_size != other.ansatz.batch_size:
+                    raise ValueError(
+                        f"Batch size of the two ansatze must match since the settings.UNSAFE_ZIP_BATCH is {settings.UNSAFE_ZIP_BATCH}."
+                    )
+                for (A1, b1, c1), (A2, b2, c2) in zip(
+                    zip(self.A, self.b, self.c), zip(other.A, other.b, other.c)
+                ):
                     Abc.append(contract_two_Abc((A1, b1, c1), (A2, b2, c2), idx_s, idx_o))
+            else:
+                for A1, b1, c1 in zip(self.A, self.b, self.c):
+                    for A2, b2, c2 in zip(other.A, other.b, other.c):
+                        Abc.append(contract_two_Abc((A1, b1, c1), (A2, b2, c2), idx_s, idx_o))
 
         A, b, c = zip(*Abc)
         return Bargmann(A, b, c)
