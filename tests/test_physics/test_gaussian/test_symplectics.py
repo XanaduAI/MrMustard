84884710c1207e9829a822f88ee4206a1b4caf1a
Filippo Miatto <ziofil@users.noreply.github.com>
2024-08-17 14:23:43 -0700

    fixed old test

--- tests/test_physics/test_gaussian/test_symplectics.py
+++ tests/test_physics/test_gaussian/test_symplectics.py
@@ -42,12 +42,12 @@ from mrmustard.lab.states import TMSV, Thermal, Vacuum
 from mrmustard.physics.gaussian import controlled_X, controlled_Z
 
 
-@given(r=st.floats(0, 2))
-def test_two_mode_squeezing(r):
+@given(r=st.floats(0, 2), phi=st.floats(0, 2 * np.pi))
+def test_two_mode_squeezing(r, phi):
     """Tests that the two-mode squeezing operation is implemented correctly"""
-    cov = (Vacuum(num_modes=2) >> S2gate(r=r, phi=0.0)).cov / settings.HBAR
-    expected = two_mode_squeezing(2 * r, 0.0)
-    assert np.allclose(cov, expected, atol=1e-6)
+    cov = (Vacuum(num_modes=2) >> S2gate(r=r, phi=phi)).cov * 2 / settings.HBAR
+    S = two_mode_squeezing(r, phi)
+    assert np.allclose(cov, S @ S.T, atol=1e-6)
 
 
 @given(r=st.floats(0, 1), phi=st.floats(0, 1))
