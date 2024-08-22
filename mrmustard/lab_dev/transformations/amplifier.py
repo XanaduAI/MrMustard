e7ecbc90c212ae9844346bce27c3909f44dafa9a
Filippo Miatto <ziofil@users.noreply.github.com>
2024-08-22 09:00:32 -0700

    Update mrmustard/lab_dev/transformations/amplifier.py

--- mrmustard/lab_dev/transformations/amplifier.py
+++ mrmustard/lab_dev/transformations/amplifier.py
@@ -41,9 +41,9 @@ class Amplifier(Channel):
         >>> from mrmustard.lab_dev import Amplifier, Coherent
 
         >>> amp = Amplifier([0], gain=4)
-        >>> coh = Coherent([0], x=1.0 * np.sqrt(2), y=2.0 * np.sqrt(2))  # units of hbar=1 (default)
+        >>> coh = Coherent([0], x=1.0, y=2.0)
         >>> _, mu, _ = (coh >> amp).phase_space(0)
-        >>> assert np.allclose(mu[0], np.array([4.0, 8.0]))
+        >>> assert np.allclose(mu[0]*np.sqrt(2/settings.HBAR), np.array([4.0, 8.0]))
 
     Args:
         modes: The modes this gate is applied to.
