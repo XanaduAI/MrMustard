e1ec4223865e63416881062f61a9d5d80237f4cb
Matthew Silverman <matthews@xanadu.ai>
2024-08-30 17:16:53 +0000

    skip HBAR check

--- mrmustard/utils/settings.py
+++ mrmustard/utils/settings.py
@@ -128,7 +128,7 @@ class Settings:
 
     def __setattr__(self, name, value):
         """Once the class is initialized, do not allow the addition of new settings."""
-        if self._frozen and not hasattr(self, name):
+        if self._frozen and name != "HBAR" and not hasattr(self, name):
             raise AttributeError(f"unknown MrMustard setting: '{name}'")
         return super().__setattr__(name, value)
 
