2a1afc1ded8d887eaf7bdf11dd34da15b299c600
Arsalan Motamedi <motamedi17@gmail.com>
2024-09-04 15:35:19 -0400

    Formatting

--- mrmustard/lab_dev/transformations/cft.py
+++ mrmustard/lab_dev/transformations/cft.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 
-# http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,7 +27,7 @@ class CFT(Map):
     The main use is to convert between Characteristic functions and phase space functions.
 
     Args:
-    num_modes: number of modes of this channel.
+        num_modes: number of modes of this channel.
     """
 
     def __init__(
