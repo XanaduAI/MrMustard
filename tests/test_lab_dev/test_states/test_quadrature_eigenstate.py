6c0b6d202a41af3a61f882444b21975168790cc8
Matthew Silverman <matthews@xanadu.ai>
2024-09-10 18:21:40 +0000

    fix quad eig test, use hbar=2 in viz tests

--- tests/test_lab_dev/test_states/test_quadrature_eigenstate.py
+++ tests/test_lab_dev/test_states/test_quadrature_eigenstate.py
@@ -60,7 +60,6 @@ class TestQuadratureEigenstate:
 
     @pytest.mark.parametrize("hbar", hbar)
     def test_probability_hbar(self, hbar):
-
         with settings(HBAR=2.0):
             q0 = QuadratureEigenstate([0], x=0, phi=0).bargmann_triple()
 
@@ -89,5 +88,5 @@ class TestQuadratureEigenstate:
 
     def test_with_coherent(self):
         val0 = Coherent([0], 0, 0) >> QuadratureEigenstate([0], 0, 0).dual
-        val1 = Coherent([0], 1, 0) >> QuadratureEigenstate([0], 2, 0).dual
+        val1 = Coherent([0], 1, 0) >> QuadratureEigenstate([0], np.sqrt(2), 0).dual
         assert np.allclose(val0, val1)
