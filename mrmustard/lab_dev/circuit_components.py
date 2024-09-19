395a40293ea8e1c2542f64ae3bf76e7f718c701b
Anthony <anthonychytros@gmail.com>
2024-09-19 12:21:03 -0400

    codefactor

--- mrmustard/lab_dev/circuit_components.py
+++ mrmustard/lab_dev/circuit_components.py
@@ -499,12 +499,12 @@ class CircuitComponent:
                     )
                     for A, b, c in zip(As, bs, cs)
                 ]
-        except AttributeError:
+        except AttributeError as e:
             shape = shape or self.auto_shape()
             if len(shape) != num_vars:
                 raise ValueError(
                     f"Expected Fock shape of length {num_vars}, got length {len(shape)}"
-                )
+                ) from e
             arrays = self.representation.reduce(shape).array
         array = math.sum(arrays, axes=[0])
         arrays = math.expand_dims(array, 0) if batched else array
