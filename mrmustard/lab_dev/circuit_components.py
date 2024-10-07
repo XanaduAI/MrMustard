9d4195e71163dd5fef0610d7a47716db0382914d
Arsalan Motamedi <motamedi17@gmail.com>
2024-10-07 10:59:24 -0400

    conflict resolved

--- mrmustard/lab_dev/circuit_components.py
+++ mrmustard/lab_dev/circuit_components.py
@@ -783,7 +783,7 @@ class CircuitComponent:
         wires_result, perm = self.wires @ other.wires
         idx_z, idx_zconj = self._matmul_indices(other)
 
-        if type(self.representation) == type(other.representation) == Fock:
+        if type(self.representation) is type(other.representation) is Fock:
             self_rep = self.representation
             other_rep = other.representation
         else:
