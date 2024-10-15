c42e20fb9e0c25a68f820ec66c809393ad610814
Arsalan Motamedi <motamedi17@gmail.com>
2024-10-15 10:38:31 -0400

    just restarging

--- mrmustard/lab_dev/circuit_components.py
+++ mrmustard/lab_dev/circuit_components.py
@@ -812,7 +812,7 @@ class CircuitComponent:
 
         wires_result, perm = self.wires @ other.wires
         idx_z, idx_zconj = self._matmul_indices(other)
-
+ 
         if type(self.representation) is type(other.representation) is Fock:
             self_rep = self.representation
             other_rep = other.representation
