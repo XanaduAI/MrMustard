1f44cbc99dc9cd5dc8e7357df34423b1ba6f35f0
Arsalan Motamedi <motamedi17@gmail.com>
2024-10-17 15:57:08 -0400

    Gaussian random noise channel

---
+++ mrmustard/lab_dev/transformations/grn.py
@@ -0,0 +1,55 @@
+# Copyright 2023 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+The class representing a noisy attenuator channel.
+"""
+
+from __future__ import annotations
+
+from mrmustard.utils.typing import RealMatrix
+from typing import Sequence
+from .base import Channel
+from ...physics.representations import Bargmann
+from ...physics import triples
+from ..utils import make_parameter, reshape_params
+
+__all__ = ["Attenuator"]
+
+
+class GRN(Channel):
+    r"""
+    The Gaussian random noise channel.
+    """
+
+    short_name = "GRN"
+    def __init__(
+        self,
+        modes: Sequence[int],
+        Y: RealMatrix,
+        Y_train: bool = False,):
+
+        super().__init__(modes_out=modes, modes_in=modes, name="GRN")
+        self._add_parameter(
+            make_parameter(
+                Y_train,
+                value= Y,
+                name="Y",
+                bounds = (None, None)
+            )
+        )
+
+        self._representation = Bargmann.from_function(
+            fn=triples.gaussian_random_noise_Abc, Y = self.Y.value
+        )
--- mrmustard/physics/triples.py
+++ mrmustard/physics/triples.py
@@ -22,7 +22,7 @@ from typing import Generator, Iterable, Union
 import numpy as np
 
 from mrmustard import math, settings
-from mrmustard.utils.typing import Matrix, Vector, Scalar
+from mrmustard.utils.typing import Matrix, Vector, Scalar, RealMatrix
 from mrmustard.physics.gaussian_integrals import complex_gaussian_integral_2
 
 
@@ -604,6 +604,23 @@ def fock_damping_Abc(
 
     return A, b, c
 
+
+def gaussian_random_noise_Abc(Y: RealMatrix) -> Union[Matrix, Vector, Scalar]:
+    m = Y.shape[-1]//2
+    xi = math.eye(2*m) + Y/settings.HBAR
+    xi_inv = math.inv(xi)
+    temp = math.block([[math.eye(2*m) - xi_inv, xi_inv],
+                       [xi_inv, math.eye(2*m) - xi_inv]])
+    R = 1/np.sqrt(2) * math.block([[math.eye(m), 1j * math.eye(m), math.zeros((m,2*m))],
+                                   [math.zeros((m,2*m)), math.eye(m), -1j * math.eye(m)],
+                                   [math.eye(m), -1j * math.eye(m), math.zeros((m,2*m))],
+                                   [math.zeros((m,2*m)), math.eye(m), 1j * math.eye(m)]])
+    
+    A = math.Xmat(2*m) @ R @ temp @ math.conj(R).T
+    b = math.zeros(2*m)
+    c = 1/math.sqrt(math.det(xi))
+
+    return A,b,c
 
 def bargmann_to_quadrature_Abc(n_modes: int, phi: float) -> tuple[Matrix, Vector, Scalar]:
     r"""
