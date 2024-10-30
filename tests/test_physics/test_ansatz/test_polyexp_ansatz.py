a9dd3692d9cff3b4833d605972f89212a33069e9
Arsalan <94669474+arsalan-motamedi@users.noreply.github.com>
2024-10-30 16:20:23 -0400

    Channel from XY (#514)
    **Context:**
    We would like to have the ability to initialize a Channel from its X,Y
    matrices (and potentially a d vector).
    
    **Description of the Change:**
    Added the method `.from_XY` which allows this computation.
    
    **Benefits:**
    Provides a simpler way for a user to initialize their desired channel.
    
    **Possible Drawbacks:**
    None.
    
    **Related GitHub Issues:**
    None.

--- tests/test_physics/test_ansatz/test_polyexp_ansatz.py
+++ tests/test_physics/test_ansatz/test_polyexp_ansatz.py
@@ -19,17 +19,17 @@
 from unittest.mock import patch
 
 import numpy as np
-from ipywidgets import Box, VBox, HTML, IntText, Stack, IntSlider
-from plotly.graph_objs import FigureWidget
 import pytest
+from ipywidgets import HTML, Box, IntSlider, IntText, Stack, VBox
+from plotly.graph_objs import FigureWidget
 
 from mrmustard import math
+from mrmustard.physics.ansatz.array_ansatz import ArrayAnsatz
+from mrmustard.physics.ansatz.polyexp_ansatz import PolyExpAnsatz
 from mrmustard.physics.gaussian_integrals import (
     complex_gaussian_integral_1,
     complex_gaussian_integral_2,
 )
-from mrmustard.physics.ansatz.polyexp_ansatz import PolyExpAnsatz
-from mrmustard.physics.ansatz.array_ansatz import ArrayAnsatz
 
 from ...random import Abc_triple
 
