8e5b8c7b276dc11b6bdc55a6660ff0ebc1925f59
Filippo Miatto <ziofil@users.noreply.github.com>
2024-11-27 10:49:10 -0800

    np -> math (#527)

--- tests/test_lab_dev/test_transformations/test_phasenoise.py
+++ tests/test_lab_dev/test_transformations/test_phasenoise.py
@@ -66,7 +66,7 @@ class TestPhaseNoise:
         """
         psi = Number([0], 0) + Number([0], 1)
         phi = psi >> PhaseNoise([0], sigma)
-        assert phi.fock_array(2)[0, 1] == math.exp(-(complex(sigma) ** 2) / 2)
+        assert np.isclose(phi.fock_array(2)[0, 1], math.exp(-(complex(sigma) ** 2) / 2))
 
     def test_check_adding_adjoint(self):
         r"""
