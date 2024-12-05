2bd37f133c221f09e6d87c19627ecee233bade1d
Anthony <anthonychytros@gmail.com>
2024-12-05 10:00:46 -0500

    rem

--- mrmustard/math/backend_numpy.py
+++ mrmustard/math/backend_numpy.py
@@ -14,7 +14,7 @@
 
 """This module contains the numpy backend."""
 
-# pylint: disable = missing-function-docstring, missing-class-docstring, fixme, too-many-positional-arguments
+# pylint: disable = missing-function-docstring, missing-class-docstring, fixme
 
 from __future__ import annotations
 
