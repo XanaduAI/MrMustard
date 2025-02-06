1cdd906860f3cf8dd125f39858d06652a2baee55
hitarth <hitarthc64@gmail.com>
2025-02-06 18:11:49 +0000

    remvove additional dimension addition

--- mrmustard/physics/triples.py
+++ mrmustard/physics/triples.py
@@ -663,7 +663,7 @@ def fock_damping_Abc(
     n_modes = len(beta)
 
     O_n = math.zeros((n_modes, n_modes), math.complex128)
-    B_n = math.diag(math.astensor([math.exp(-beta)])).reshape((n_modes, n_modes))
+    B_n = math.diag(math.astensor(math.exp(-beta))).reshape((n_modes, n_modes))
 
     A = math.block([[O_n, B_n], [B_n, O_n]])
     b = _vacuum_B_vector(n_modes * 2)
