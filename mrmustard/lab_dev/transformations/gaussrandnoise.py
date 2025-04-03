0392e6717d2e75e52672589f193d01575482a2fa
Arsalan Motamedi <motamedi17@gmail.com>
2025-04-03 14:55:10 -0400

    some other transformations

--- mrmustard/lab_dev/transformations/gaussrandnoise.py
+++ mrmustard/lab_dev/transformations/gaussrandnoise.py
@@ -32,6 +32,12 @@ class GaussRandNoise(Channel):
     r"""
     The Gaussian random noise channel.
 
+
+    Args:
+        modes: The modes the channel is applied to. The number of modes must match half of the size of ``Y``.
+        Y: The Y matrix of the Gaussian random noise channel.
+        Y_trainable: Whether ``Y`` is trainable.
+
     .. code-block ::
 
         >>> import numpy as np
@@ -40,11 +46,6 @@ class GaussRandNoise(Channel):
         >>> channel = GaussRandNoise(modes=(1, 2), Y = 0.2 * np.eye(4))
         >>> assert channel.modes == (1, 2)
         >>> assert math.allclose(channel.parameters.Y.value, 0.2 * np.eye(4))
-
-    Args:
-        modes: The modes the channel is applied to. The number of modes must match half of the size of ``Y``.
-        Y: The Y matrix of the Gaussian random noise channel.
-        Y_trainable: Whether ``Y`` is trainable.
 
     Raises:
         ValueError: If the number of modes does not match half of the size of ``Y``.
