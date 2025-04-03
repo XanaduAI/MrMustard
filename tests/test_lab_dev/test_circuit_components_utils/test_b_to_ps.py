a2c9d9fd947521db6bf0a170b15ae0998e2fc5b5
Arsalan Motamedi <motamedi17@gmail.com>
2025-04-03 13:47:50 -0400

    simle fix

--- tests/test_lab_dev/test_circuit_components_utils/test_b_to_ps.py
+++ tests/test_lab_dev/test_circuit_components_utils/test_b_to_ps.py
@@ -44,7 +44,7 @@ class TestBtoPS:
         settings.HBAR = hbar
         state = Ket.random((0,)) >> Dgate(0, x=2, y=0.1)
 
-        dm = math.sum(state.to_fock(100).dm().ansatz.array, axis=0)
+        dm = state.to_fock(100).dm().ansatz.array
         vec = np.linspace(-5, 5, 100)
         wigner, _, _ = wigner_discretized(dm, vec, vec)
 
