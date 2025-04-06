425a23a92d9ca2197e1d558f05c93306de961f34
Filippo Miatto <miatto@gmail.com>
2025-04-06 09:30:15 -0700

    more functions in batch.py file

--- mrmustard/math/lattice/strategies/vanilla/batch.py
+++ mrmustard/math/lattice/strategies/vanilla/batch.py
@@ -20,13 +20,13 @@ from numba import njit, prange
 from mrmustard.math.lattice import steps
 from mrmustard.utils.typing import ComplexTensor
 
-from .core import vanilla, vanilla_stable
+from .core import vanilla, stable
 
 
 @njit(parallel=True)
-def vanilla_stable_b_batch(shape: tuple[int, ...], A, b, c) -> ComplexTensor:  # pragma: no cover
+def stable_b_batch(shape: tuple[int, ...], A, b, c) -> ComplexTensor:  # pragma: no cover
     r"""Batched version of the stable vanilla algorithm for calculating the fock representation of a Gaussian tensor.
-    See the documentation of ``vanilla_stable`` for more details about the non-batched version.
+    See the documentation of ``stable`` for more details about the non-batched version.
 
     Args:
         shape (tuple[int, ...]): shape of the output tensor excluding the batch dimension, which is inferred from ``b``
@@ -41,7 +41,7 @@ def vanilla_stable_b_batch(shape: tuple[int, ...], A, b, c) -> ComplexTensor:  #
     B = b.shape[0]
     G = np.zeros((B,) + shape, dtype=np.complex128)
     for k in prange(B):
-        G[k] = vanilla_stable(shape, A, b[k], c)
+        G[k] = stable(shape, A, b[k], c)
     return G
 
 
@@ -90,4 +90,27 @@ def vanilla_full_batch(shape: tuple[int, ...], A, b, c) -> ComplexTensor:  # pra
     G = np.zeros((batch_size,) + shape, dtype=np.complex128)
     for k in prange(batch_size):
         G[k] = vanilla(shape, A[k], b[k], c[k])
+    return G
+
+
+@njit(parallel=True)
+def stable_full_batch(shape: tuple[int, ...], A, b, c) -> ComplexTensor:  # pragma: no cover
+    r"""Batched version of the stable vanilla algorithm for calculating the fock representation of a
+    Gaussian tensor. This implementation assumes that the batch dimension is on the first
+    axis of A, b, and c and it's the same for all of them.
+    See the documentation ``stable`` for more details about the non-batched version.
+
+    Args:
+        shape (tuple[int, ...]): shape of the output tensor excluding the batch dimension
+        A (np.ndarray): batched A matrix of the Bargmann representation
+        b (np.ndarray): batched b vector of the Bargmann representation
+        c (complex): batched vacuum amplitudes
+
+    Returns:
+        np.ndarray: Fock representation of the Gaussian tensor with shape ``(batch,) + shape``
+    """
+    batch_size = b.shape[0]
+    G = np.zeros((batch_size,) + shape, dtype=np.complex128)
+    for k in prange(batch_size):
+        G[k] = stable(shape, A[k], b[k], c[k])
     return G
