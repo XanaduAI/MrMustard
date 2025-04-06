9fd8946d4d919f3180d1f1aa22778ae91a9e1ce5
Filippo Miatto <miatto@gmail.com>
2025-04-06 08:46:23 -0700

    added vanilla_full_batch_numba forward pass

--- mrmustard/math/lattice/strategies/vanilla.py
+++ mrmustard/math/lattice/strategies/vanilla.py
@@ -22,6 +22,7 @@ __all__ = [
     "vanilla",
     "vanilla_stable",
     "vanilla_stable_batch",
+    "vanilla_full_batch_numba",
     "vanilla_batch",
     "vanilla_jacobian",
     "vanilla_vjp",
@@ -258,6 +259,29 @@ def vanilla_batch(shape: tuple[int, ...], A, b, c) -> ComplexTensor:  # pragma: 
     for index in path:
         G[(slice(None),) + index] = steps.vanilla_step_batch(G, A, b, index)
 
+    return G
+
+
+@njit(parallel=True)
+def vanilla_full_batch_numba(shape: tuple[int, ...], A, b, c) -> ComplexTensor:  # pragma: no cover
+    r"""Batched version of the vanilla algorithm for calculating the fock representation of a
+    Gaussian tensor. This implementation assumes that the batch dimension is on the first
+    axis of A, b, and c and it's the same for all of them.
+    See the documentation of ``vanilla`` for more details about the non-batched version.
+
+    Args:
+        shape (tuple[int, ...]): shape of the output tensor excluding the batch dimension
+        A (np.ndarray): batched A matrix of the Bargmann representation
+        b (np.ndarray): batched b vector of the Bargmann representation
+        c (complex): batched vacuum amplitudes
+
+    Returns:
+        np.ndarray: Fock representation of the Gaussian tensor with shape ``(batch,) + shape``
+    """
+    batch_size = b.shape[0]
+    G = np.zeros((batch_size,) + shape, dtype=np.complex128)
+    for k in prange(batch_size):
+        G[k] = vanilla(shape, A[k], b[k], c[k])
     return G
 
 
