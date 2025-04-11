fac5febf15b6870bdd4521b3ed0c8f84933d941d
Filippo Miatto <miatto@gmail.com>
2025-04-11 16:10:43 -0700

    removed oe contract

--- mrmustard/math/backend_numpy.py
+++ mrmustard/math/backend_numpy.py
@@ -28,7 +28,6 @@ from scipy.linalg import expm as scipy_expm
 from scipy.linalg import sqrtm as scipy_sqrtm
 from scipy.special import xlogy as scipy_xlogy
 from scipy.stats import multivariate_normal
-from opt_einsum import contract
 from ..utils.settings import settings
 from .autocast import Autocast
 from .backend_base import BackendBase
@@ -228,7 +227,7 @@ class BackendNumpy(BackendBase):  # pragma: no cover
         return array
 
     def einsum(self, string: str, *tensors, optimize: bool | str = False) -> np.ndarray:
-        return contract(string, *tensors, optimize=optimize)
+        return np.einsum(string, *tensors, optimize=optimize)
 
     def exp(self, array: np.ndarray) -> np.ndarray:
         return np.exp(array)
