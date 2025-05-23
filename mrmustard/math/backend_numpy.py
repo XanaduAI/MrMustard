8658294bd7938f7d0249d80b94b6ade26568a453
Garett Brown <garett.brown@xanadu.ai>
2025-05-23 17:15:58 -0400

    Changed np.array -> np.asarray to reduce memory overhead.

--- mrmustard/math/backend_numpy.py
+++ mrmustard/math/backend_numpy.py
@@ -84,14 +84,14 @@ class BackendNumpy(BackendBase):
     def asnumpy(self, tensor: np.ndarray) -> np.ndarray:
         if isinstance(tensor, np.ndarray):
             return tensor
-        return np.array(tensor)
+        return np.asarray(tensor)
 
     def assign(self, tensor: np.ndarray, value: np.ndarray) -> np.ndarray:
         tensor = value
         return tensor
 
     def astensor(self, array: np.ndarray, dtype=None) -> np.ndarray:
-        array = np.array(array)
+        array = np.asarray(array)
         return self.cast(array, dtype=dtype or array.dtype)
 
     def atleast_nd(self, array: np.ndarray, n: int, dtype=None) -> np.ndarray:
@@ -107,14 +107,14 @@ class BackendNumpy(BackendBase):
         return sp.linalg.block_diag(*blocks)
 
     def boolean_mask(self, tensor: np.ndarray, mask: np.ndarray) -> np.ndarray:
-        return np.array([t for i, t in enumerate(tensor) if mask[i]])
+        return np.asarray([t for i, t in enumerate(tensor) if mask[i]])
 
     def cast(self, array: np.ndarray, dtype=None) -> np.ndarray:
         if dtype is None:
             return array
         if dtype not in [self.complex64, self.complex128, "complex64", "complex128"]:
             array = self.real(array)
-        return np.array(array, dtype=dtype)
+        return np.asarray(array, dtype=dtype)
 
     def clip(self, array, a_min, a_max) -> np.ndarray:
         return np.clip(array, a_min, a_max)
@@ -124,7 +124,7 @@ class BackendNumpy(BackendBase):
         try:
             return np.concatenate(values, axis)
         except ValueError:
-            return np.array(values)
+            return np.asarray(values)
 
     def conj(self, array: np.ndarray) -> np.ndarray:
         return np.conj(array)
@@ -207,7 +207,7 @@ class BackendNumpy(BackendBase):
             for line in array:
                 ret.append(np.diag(line, k))
 
-            ret = np.array(ret)
+            ret = np.asarray(ret)
             inner_shape = (
                 original_sh[-1] + abs(k),
                 original_sh[-1] + abs(k),
@@ -266,7 +266,7 @@ class BackendNumpy(BackendBase):
         return False
 
     def lgamma(self, x: np.ndarray) -> np.ndarray:
-        return np.array([mlgamma(v) for v in x])
+        return np.asarray([mlgamma(v) for v in x])
 
     def log(self, x: np.ndarray) -> np.ndarray:
         return np.log(x)
@@ -301,10 +301,10 @@ class BackendNumpy(BackendBase):
         name: str,
         dtype=np.float64,
     ):  # pylint: disable=unused-argument
-        return np.array(value, dtype=dtype)
+        return np.asarray(value, dtype=dtype)
 
     def new_constant(self, value, name: str, dtype=np.float64):  # pylint: disable=unused-argument
-        return np.array(value, dtype=dtype)
+        return np.asarray(value, dtype=dtype)
 
     def norm(self, array: np.ndarray) -> np.ndarray:
         return np.linalg.norm(array)
@@ -425,11 +425,11 @@ class BackendNumpy(BackendBase):
         return np.zeros(shape, dtype=dtype)
 
     def zeros_like(self, array: np.ndarray) -> np.ndarray:
-        return np.zeros(np.array(array).shape, dtype=array.dtype)
+        return np.zeros(np.asarray(array).shape, dtype=array.dtype)
 
     def map_fn(self, func, elements):
         # Is this done like this?
-        return np.array([func(e) for e in elements])
+        return np.asarray([func(e) for e in elements])
 
     def squeeze(self, tensor, axis=None):
         return np.squeeze(tensor, axis=axis)
