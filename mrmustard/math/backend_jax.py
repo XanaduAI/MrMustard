9854ce7e6ea01076f7959088a3ca53a969ab2d42
Anthony <anthonychytros@gmail.com>
2025-05-30 11:59:30 -0400

    update path

--- mrmustard/math/backend_jax.py
+++ mrmustard/math/backend_jax.py
@@ -37,7 +37,7 @@ from .lattice.strategies.compactFock.inputValidation import (  # pragma: no cove
 )
 
 jax.config.update("jax_enable_x64", True)
-jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
+jax.config.update("jax_compilation_cache_dir", "mrmustard/math/tmp/jax_cache")
 jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
 jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
 jax.config.update("jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir")
