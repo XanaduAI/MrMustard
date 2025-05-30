146e24a233f5718c4cfc11c09840396664c0fd0f
Anthony <anthonychytros@gmail.com>
2025-05-30 11:32:55 -0400

    jit cache

--- mrmustard/math/backend_jax.py
+++ mrmustard/math/backend_jax.py
@@ -36,7 +36,11 @@ from .lattice.strategies.compactFock.inputValidation import (  # pragma: no cove
     hermite_multidimensional_diagonal_batch,
 )
 
-jax.config.update("jax_enable_x64", True)  # pragma: no cover
+jax.config.update("jax_enable_x64", True)
+jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+jax.config.update("jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir")
 
 
 # pylint: disable=too-many-public-methods
