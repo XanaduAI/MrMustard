8606267982341303c1c3c9e1ae700feefb2c6761
Filippo Miatto <ziofil@users.noreply.github.com>
2025-06-05 11:48:34 -0700

    Update mrmustard/physics/ansatz/array_ansatz.py
    Co-authored-by: Anthony <125415978+apchytr@users.noreply.github.com>

--- mrmustard/physics/ansatz/array_ansatz.py
+++ mrmustard/physics/ansatz/array_ansatz.py
@@ -224,7 +224,7 @@ class ArrayAnsatz(Ansatz):
             >>> assert fock1 == fock2
 
             >>> fock3 = fock1.reduce((2, 2, 2))
-            >>> array3 = np.array([[[0, 1], [3, 4]], [[9, 10], [12, 13]]])
+            >>> array3 = math.astensor([[[0, 1], [3, 4]], [[9, 10], [12, 13]]])
             >>> assert fock3 == ArrayAnsatz(array3)
 
             >>> fock4 = fock1.reduce((1, 3, 1))
