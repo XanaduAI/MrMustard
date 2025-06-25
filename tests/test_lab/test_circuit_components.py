0f06aa9d934d1e52cb73f7260d97f8c05f962184
Luke Helt <luke.helt@gmail.com>
2025-06-25 10:53:06 -0400

    last one

--- tests/test_lab/test_circuit_components.py
+++ tests/test_lab/test_circuit_components.py
@@ -97,7 +97,7 @@ class TestCircuitComponent:
     def test_from_to_quadrature(self):
         c = Dgate(0, x=0.1, y=0.2) >> Sgate(0, r=1.0, phi=0.1)
         cc = CircuitComponent(ansatz=c.ansatz, wires=c.wires, name=c.name)
-        ccc = CircuitComponent.from_quadrature(tuple(), tuple(), (0,), (0,), cc.quadrature_triple())
+        ccc = CircuitComponent.from_quadrature((), (), (0,), (0,), cc.quadrature_triple())
         assert cc == ccc
 
     def test_adjoint(self):
