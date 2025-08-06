627f3a964c392a4edbe378ff42b9e68634e4d366
Anthony <anthonychytros@gmail.com>
2025-08-06 08:43:23 -0400

    prog

--- mrmustard/training/optimizer.py
+++ mrmustard/training/optimizer.py
@@ -101,7 +101,8 @@ class Optimizer:
         loss_value, grads = jax.value_and_grad(cost_fn, argnums=tuple(range(len(by_optimizing))))(
             *by_optimizing,
         )
-        conj_grads = jax.tree_util.tree_map(lambda x: jax.numpy.conj(x), grads)
+        jax.debug.print("grads: {grads}", grads=grads[0].parameters.symplectic.value)
+        conj_grads = jax.tree.map(lambda x: jax.numpy.conj(x), grads)
         updates, opt_state = optim.update(conj_grads, opt_state, by_optimizing)
         by_optimizing = eqx.apply_updates(by_optimizing, updates)
         return by_optimizing, opt_state, loss_value
